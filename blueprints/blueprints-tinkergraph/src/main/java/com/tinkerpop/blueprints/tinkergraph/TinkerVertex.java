14b233825ae593d94ff9663fcc6f251a9eba98e2
Marko A. Rodriguez <okrammarko@gmail.com>
2013-10-15 15:40:52 -0600

    added support for State.CENTRIC, State.ADJACENT so that TinkerVertex has endogenous concepts associated with VertexCentric computing. Added PageRankVertexProgram to Gremlin from Furnace2 for testing purposes.

--- blueprints/blueprints-tinkergraph/src/main/java/com/tinkerpop/blueprints/tinkergraph/TinkerVertex.java
+++ blueprints/blueprints-tinkergraph/src/main/java/com/tinkerpop/blueprints/tinkergraph/TinkerVertex.java
@@ -18,33 +18,83 @@ import java.util.Set;
  */
 class TinkerVertex extends TinkerElement implements Vertex, Serializable {
 
+    protected enum State {STANDARD, CENTRIC, ADJACENT}
+
+    protected TinkerVertexMemory vertexMemory = null;
+    protected final State state;
+    protected String centricId;
+
     protected Map<String, Set<Edge>> outEdges = new HashMap<>();
     protected Map<String, Set<Edge>> inEdges = new HashMap<>();
 
     protected TinkerVertex(final String id, final TinkerGraph graph) {
         super(id, graph);
+        this.state = State.STANDARD;
+        this.centricId = id;
+    }
+
+    protected TinkerVertex(final TinkerVertex vertex, final State state, final String centricId, final TinkerVertexMemory vertexMemory) {
+        super(vertex.id, vertex.graph);
+        this.state = state;
+        this.outEdges = vertex.outEdges;
+        this.inEdges = vertex.inEdges;
+        this.properties = vertex.properties;
+        this.vertexMemory = vertexMemory;
+        this.centricId = centricId;
     }
 
     public <T> Property<T, Vertex> getProperty(final String key) {
-        final Property<T, Vertex> t = this.properties.get(key);
-        return (null == t) ? Property.<T, Vertex>empty() : t;
+        if (State.STANDARD == this.state) {
+            final Property<T, Vertex> t = this.properties.get(key);
+            return (null == t) ? Property.empty() : t;
+        } else if (State.CENTRIC == this.state) {
+            if (this.vertexMemory.isComputeKey(key))
+                return this.vertexMemory.getProperty(this, key);
+            else {
+                final Property<T, Vertex> t = this.properties.get(key);
+                return (null == t) ? Property.empty() : t;
+            }
+        } else {
+            if (this.vertexMemory.isComputeKey(key))
+                return this.vertexMemory.getProperty(this, key);
+            else
+                throw new IllegalArgumentException("The provided key is not a compute key: " + key);
+        }
     }
 
     public <T> Property<T, Vertex> setProperty(final String key, final T value) {
-        ElementHelper.validateProperty(this, key, value);
-        final Property<T, Vertex> oldValue = this.properties.put(key, new TinkerProperty<>(key, value, this));
-        this.graph.vertexIndex.autoUpdate(key, value, oldValue, this);
-        return oldValue;
+        if (State.STANDARD == this.state) {
+            ElementHelper.validateProperty(this, key, value);
+            final Property<T, Vertex> oldValue = this.properties.put(key, new TinkerProperty<>(key, value, this));
+            this.graph.vertexIndex.autoUpdate(key, value, oldValue, this);
+            return oldValue;
+        } else if (State.CENTRIC == this.state) {
+            if (this.vertexMemory.isComputeKey(key))
+                return this.vertexMemory.setProperty(this, key, value);
+            else
+                throw new IllegalArgumentException("The provided key is not a compute key: " + key);
+        } else {
+            throw new UnsupportedOperationException();
+        }
     }
 
     public <T> Property<T, Vertex> removeProperty(final String key) {
-        final Property<T, Vertex> oldValue = this.properties.remove(key);
-        this.graph.vertexIndex.autoRemove(key, oldValue, this);
-        return oldValue;
+        if (State.STANDARD == this.state) {
+            final Property<T, Vertex> oldValue = this.properties.remove(key);
+            this.graph.vertexIndex.autoRemove(key, oldValue, this);
+            return oldValue;
+        } else if (State.CENTRIC == this.state) {
+            if (this.vertexMemory.isComputeKey(key))
+                return this.vertexMemory.removeProperty(this, key);
+            else
+                throw new IllegalArgumentException("The provided key is not a compute key: " + key);
+        } else {
+            throw new UnsupportedOperationException();
+        }
     }
 
     public VertexQuery query() {
-        return new TinkerVertexQuery(this);
+        return new TinkerVertexQuery(this, this.vertexMemory);
     }
 
     public String toString() {
@@ -59,5 +109,9 @@ class TinkerVertex extends TinkerElement implements Vertex, Serializable {
         this.query().direction(Direction.BOTH).edges().forEach(Edge::remove);
         graph.vertexIndex.removeElement(this);
         graph.vertices.remove(this.id);
+    }
+
+    public TinkerVertex createClone(final State state, final String centricId, final TinkerVertexMemory vertexMemory) {
+        return new TinkerVertex(this, state, centricId, vertexMemory);
     }
 }
--- blueprints/blueprints-tinkergraph/src/test/java/com/tinkerpop/blueprints/tinkergraph/TinkerGraphTest.java
+++ blueprints/blueprints-tinkergraph/src/test/java/com/tinkerpop/blueprints/tinkergraph/TinkerGraphTest.java
@@ -7,6 +7,7 @@ import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.computer.ComputeResult;
 import com.tinkerpop.blueprints.computer.VertexProgram;
 import com.tinkerpop.blueprints.computer.util.LambdaVertexProgram;
+import com.tinkerpop.blueprints.util.StreamFactory;
 import junit.framework.TestCase;
 
 import java.util.Random;
@@ -34,7 +35,7 @@ public class TinkerGraphTest extends TestCase {
     public void testLambdaProgram() {
         //TinkerGraph g = TinkerFactory.createClassic();
         TinkerGraph g = new TinkerGraph();
-        Stream.generate(g::addVertex).limit(500000).count();
+        Stream.generate(g::addVertex).limit(5000).count();
         ComputeResult result = g.compute().program(LambdaVertexProgram.create()
                 .setup(gm -> {
                 })
@@ -47,8 +48,8 @@ public class TinkerGraphTest extends TestCase {
                 .submit();
 
         System.out.println("Runtime: " + result.getGraphMemory().getRuntime());
-        /*StreamFactory.stream(g.query().vertices())
-                .forEach(v -> System.out.println(result.getVertexMemory().getProperty(v, "i").getValue()));*/
+        StreamFactory.stream(g.query().vertices())
+                .forEach(v -> System.out.println(result.getVertexMemory().getProperty(v, "i").getValue()));
     }
 
 }
