b1abbd8ea2f50eb160dc5fb35d18dbb3bc6f0dab
Stephen Mallette <spmva@genoprime.com>
2013-10-15 13:56:59 -0400

    Add server and graph configuration files.
    The yaml file represents server configuration. It will point to external graph properties files that can then be utilized natively to be passed into GraphFactory for creation.

--- gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/GremlinServer.java
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/GremlinServer.java
@@ -13,6 +13,8 @@ import io.netty.handler.codec.http.HttpServerCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 /**
  * Adapted from https://github.com/netty/netty/tree/netty-4.0.10.Final/example/src/main/java/io/netty/example/http/websocketx/server
  *
@@ -20,10 +22,10 @@ import org.slf4j.LoggerFactory;
  */
 public class GremlinServer {
     private static final Logger logger = LoggerFactory.getLogger(GremlinServer.class);
-    private final int port;
+    private final ServerSettings settings;
 
-    public GremlinServer(int port) {
-        this.port = port;
+    public GremlinServer(final ServerSettings settings) {
+        this.settings = settings;
     }
 
     public void run() throws Exception {
@@ -35,9 +37,9 @@ public class GremlinServer {
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new WebSocketServerInitializer());
 
-            final Channel ch = b.bind(port).sync().channel();
-            logger.info("Web socket server started at port {}.", port);
-            logger.info("Open your browser and navigate to http://localhost:{}/",port);
+            final Channel ch = b.bind(settings.port).sync().channel();
+            logger.info("Web socket server started at port {}.", settings.port);
+            logger.info("Open your browser and navigate to http://localhost:{}/", settings.port);
 
             ch.closeFuture().sync();
         } finally {
@@ -47,13 +49,27 @@ public class GremlinServer {
     }
 
     public static void main(final String[] args) throws Exception {
-        int port;
+        printHeader();
+        final String file;
         if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
+            file = args[0];
         } else {
-            port = 8182;
+            file = "config/gremlin-server.yaml";
         }
-        new GremlinServer(port).run();
+
+        final Optional<ServerSettings> settings = ServerSettings.read(file);
+        if (settings.isPresent()) {
+            logger.info("Configuring Gremlin Server from {}", file);
+            new GremlinServer(settings.get()).run();
+        } else
+            logger.error("Configuration file at {} could not be found or parsed properly.", file);
+    }
+
+    private static void printHeader() {
+        logger.info("");
+        logger.info("         \\,,,/");
+        logger.info("         (o o)");
+        logger.info("-----oOOo-(_)-oOOo-----");
     }
 
     private class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
