bece74e57252c8f443fd8b999863fe3d27a69f93
Stephen Mallette <spmva@genoprime.com>
2013-10-28 10:47:02 -0400

    Use tokens instead of hardcoded strings for ops.

--- gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/OpProcessor.java
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/OpProcessor.java
@@ -30,21 +30,21 @@ class OpProcessor {
     public Consumer<Context> select(final RequestMessage message) {
         final Consumer<Context> op;
         switch (message.op) {
-            case "version":
+            case ServerTokens.OPS_VERSION:
                 op = (message.optionalArgs("verbose").isPresent()) ?
                         text("Gremlin " + Tokens.VERSION + GremlinServer.getHeader()) :
                         text(Tokens.VERSION);
                 break;
-            case "eval":
+            case ServerTokens.OPS_EVAL:
                 op = validateEvalMessage(message).orElse(evalOp());
                 break;
-            case "import":
+            case ServerTokens.OPS_IMPORT:
                 op = validateImportMessage(message).orElse(importOp());
                 break;
-            case "use":
+            case ServerTokens.OPS_USE:
                 op = validateUseMessage(message).orElse(useOp());
                 break;
-            case "invalid":
+            case ServerTokens.OPS_INVALID:
                 op = error(String.format("Message could not be parsed.  Check the format of the request. [%s]", message));
                 break;
             default:
@@ -57,7 +57,7 @@ class OpProcessor {
 
     private static Optional<Consumer<Context>> validateEvalMessage(final RequestMessage message) {
         if (!message.optionalArgs("gremlin").isPresent())
-            return Optional.of(error("A message with an [eval] op code requires a [gremlin] argument."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires a [gremlin] argument.", ServerTokens.OPS_EVAL)));
         else
             return Optional.empty();
     }
@@ -65,25 +65,25 @@ class OpProcessor {
     private static Optional<Consumer<Context>> validateImportMessage(final RequestMessage message) {
         final Optional<List> l = message.optionalArgs("imports");
         if (!l.isPresent())
-            return Optional.of(error("A message with an [import] op code requires a [imports] argument."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires a [imports] argument.", ServerTokens.OPS_IMPORT)));
 
         if (l.orElse(new ArrayList()).size() == 0)
-            return Optional.of(error("A message with an [import] op code requires that the [imports] argument has at least one import string specified."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires that the [imports] argument has at least one import string specified.", ServerTokens.OPS_IMPORT)));
         else
             return Optional.empty();
     }
 
     private static Optional<Consumer<Context>> validateUseMessage(final RequestMessage message) {
-        final Optional<List> l = message.optionalArgs("use");
+        final Optional<List> l = message.optionalArgs("coordinates");
         if (!l.isPresent())
-            return Optional.of(error("A message with an [use] op code requires a [coordinates] argument."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires a [coordinates] argument.", ServerTokens.OPS_USE)));
 
         final List coordinates = l.orElse(new ArrayList());
         if (coordinates.size() == 0)
-            return Optional.of(error("A message with an [use] op code requires that the [coordinates] argument has at least one set of valid maven coordinates specified."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires that the [coordinates] argument has at least one set of valid maven coordinates specified.", ServerTokens.OPS_USE)));
 
         if (!coordinates.stream().allMatch(OpProcessor::validateCoordinates))
-            return Optional.of(error("A message with an [use] op code requires that all [coordinates] specified are valid maven coordinates with a group, artifact, and version."));
+            return Optional.of(error(String.format("A message with an [%s] op code requires that all [coordinates] specified are valid maven coordinates with a group, artifact, and version.", ServerTokens.OPS_USE)));
         else
             return Optional.empty();
     }
@@ -116,7 +116,7 @@ class OpProcessor {
     private static Consumer<Context> useOp() {
         return (context) -> {
             final RequestMessage msg = context.getRequestMessage();
-            final List<Map<String,String>> usings = (List<Map<String,String>>) msg.args.get("use");
+            final List<Map<String,String>> usings = (List<Map<String,String>>) msg.args.get("coordinates");
             usings.forEach(c -> gremlinExecutor.select(msg).use(c.get("group"), c.get("artifact"), c.get("version")));
         };
     }
---
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/ServerTokens.java
@@ -0,0 +1,12 @@
+package com.tinkerpop.gremlin.server;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class ServerTokens {
+    public static final String OPS_EVAL = "eval";
+    public static final String OPS_IMPORT = "import";
+    public static final String OPS_INVALID = "invalid";
+    public static final String OPS_USE = "use";
+    public static final String OPS_VERSION = "version";
+}
