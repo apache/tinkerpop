478ad2b594cf5d84e411075c094bda8af8e952df
Stephen Mallette <spmva@genprime.com>
2013-11-01 11:30:06 -0400

    Abstraced the OpProcessor into an interface to allow commands to be plugged in to gremlin server.

---
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/op/OpLoader.java
@@ -0,0 +1,36 @@
+package com.tinkerpop.gremlin.server.op;
+
+import com.tinkerpop.gremlin.server.OpProcessor;
+import com.tinkerpop.gremlin.server.op.standard.StandardOpProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+/**
+ * Uses ServiceLoader to load Processor instances into a cache.
+ *
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public final class OpLoader {
+    private static final Logger logger = LoggerFactory.getLogger(OpLoader.class);
+
+    private static final Map<String,OpProcessor> processors = new HashMap<>();
+    static {
+        ServiceLoader.load(OpProcessor.class).forEach(op -> {
+            final String name = op.getName();
+            logger.info("Adding the {} OpProcessor.", name.equals(StandardOpProcessor.OP_PROCESSOR_NAME) ? "standard" : name);
+            if (processors.containsKey(name))
+                throw new RuntimeException(String.format("There is a naming conflict with the %s OpProcessor implementations.", name));
+
+            processors.put(name, op);
+        });
+    }
+
+    public static Optional<OpProcessor> getProcessor(final String name) {
+        return Optional.ofNullable(processors.get(name));
+    }
+}
