6eab932db355cc56d7cb599498c294bcf1171186
Victor Su <vsu1976@gmail.com>
2013-11-03 12:59:12 -0500

    Strip down Gremlin Workbench to REPL only

--- gremlin/gremlin-server/src/main/resources/web/js/main.js
+++ gremlin/gremlin-server/src/main/resources/web/js/main.js
@@ -2,12 +2,7 @@ requirejs.config({
     baseUrl: "/js",
     paths: {
         "bootstrap": "lib/bootstrap.min",
-        "cookie": "lib/jquery.cookie",
-        "dust": "lib/dust/dust-full-1.1.1",
-        "dust-helpers": "lib/dust/dust-helpers-1.1.0",
         "jquery": "lib/jquery-1.9.1.min",
-        "jquery-layout": "lib/jquery.layout-latest.min",
-        "jquery-ui": "lib/jquery-ui-1.10.3.custom.min",
         "moment": "lib/moment.min",
         "mustache": "lib/mustache",
         "underscore": "lib/underscore.min",
@@ -16,9 +11,6 @@ requirejs.config({
     },
     shim: {
         "bootstrap": ["jquery"],
-        "cookie": ["jquery"],
-        "dust-helpers": ["dust"],
-        "jquery-layout": ["jquery", "jquery-ui"],
         "uuid": ["jquery"]
     },
     locale: "en-us"
