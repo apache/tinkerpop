5c47960c26584f8bc18425053fb8dedbc7a69cda
Marko A. Rodriguez <okrammarko@gmail.com>
2013-11-07 23:29:01 -0700

    standard exceptions are not handled by Features.

--- blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/computer/GraphComputer.java
+++ blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/computer/GraphComputer.java
@@ -1,31 +1,14 @@
 package com.tinkerpop.blueprints.computer;
 
-import com.tinkerpop.blueprints.Featureable;
-
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-public interface GraphComputer extends Featureable {
+public interface GraphComputer {
 
     public GraphComputer isolation(Isolation isolation);
 
     public GraphComputer program(VertexProgram program);
 
     public ComputeResult submit();
-
-    public static GraphComputer.Features getFeatures() {
-        return new Features() {
-        };
-    }
-
-    public interface Features extends com.tinkerpop.blueprints.Features {
-        public default boolean supportsGraphQuery() {
-            return true;
-        }
-
-        public default boolean supportsVertexQuery() {
-            return true;
-        }
-    }
 
 }
