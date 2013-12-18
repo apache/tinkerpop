40430447458e26d95944e6bd141a13e75d973d59
Stephen Mallette <spmva@genoprime.com>
2013-12-18 16:40:00 -0500

    Add some actual performance tests with junitbenchmark.
    Basically these performance are some simple examples/cases.

--- blueprints/blueprints-test/src/main/java/com/tinkerpop/blueprints/GraphGeneratePerformanceTest.java
+++ blueprints/blueprints-test/src/main/java/com/tinkerpop/blueprints/GraphGeneratePerformanceTest.java
@@ -1,5 +1,11 @@
 package com.tinkerpop.blueprints;
 
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import com.carrotsearch.junitbenchmarks.annotation.AxisRange;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
+import com.carrotsearch.junitbenchmarks.annotation.LabelType;
 import com.tinkerpop.blueprints.generator.CommunityGenerator;
 import com.tinkerpop.blueprints.generator.Distribution;
 import com.tinkerpop.blueprints.generator.DistributionGenerator;
@@ -7,12 +13,15 @@ import com.tinkerpop.blueprints.generator.NormalDistribution;
 import com.tinkerpop.blueprints.generator.PowerLawDistribution;
 import com.tinkerpop.blueprints.generator.SizableIterable;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,6 +30,42 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(Enclosed.class)
 public class GraphGeneratePerformanceTest {
+
+    @AxisRange(min = 0, max = 1)
+    @BenchmarkMethodChart(filePrefix = "blueprints-write")
+    @BenchmarkHistoryChart(labelWith = LabelType.CUSTOM_KEY, maxRuns = 20, filePrefix = "hx-blueprints-write")
+    public static class WriteToGraph extends AbstractBlueprintsTest {
+
+        @Rule
+        public TestRule benchmarkRun = new BenchmarkRule();
+
+        @Test
+        @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 0, concurrency = BenchmarkOptions.CONCURRENCY_SEQUENTIAL)
+        public void writeEmptyVertices() throws Exception {
+            final int verticesToGenerate = 100000;
+            for (int ix = 0; ix < verticesToGenerate; ix++) {
+                g.addVertex();
+            }
+
+            AbstractBlueprintsSuite.assertVertexEdgeCounts(g, verticesToGenerate, 0);
+        }
+
+        @Test
+        @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 0, concurrency = BenchmarkOptions.CONCURRENCY_SEQUENTIAL)
+        public void writeEmptyVerticesAndEdges() throws Exception {
+            final int verticesToGenerate = 100000;
+            Optional<Vertex> lastVertex = Optional.empty();
+            for (int ix = 0; ix < verticesToGenerate; ix++) {
+                final Vertex v = g.addVertex();
+                if (lastVertex.isPresent())
+                    v.addEdge("parent", lastVertex.get());
+
+                lastVertex = Optional.of(v);
+            }
+
+            AbstractBlueprintsSuite.assertVertexEdgeCounts(g, verticesToGenerate, verticesToGenerate - 1);
+        }
+    }
 
     @RunWith(Parameterized.class)
     public static class DistributionGeneratorTest extends AbstractBlueprintsTest {
