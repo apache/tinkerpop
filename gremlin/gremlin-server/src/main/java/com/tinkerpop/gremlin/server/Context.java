61ca2e718894045357e18533c9c9a24c7024248d
Stephen Mallette <spmva@genoprime.com>
2014-01-01 10:13:21 -0500

    More javadoc for Gremlin Server.

--- gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/Context.java
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/Context.java
@@ -3,13 +3,35 @@ package com.tinkerpop.gremlin.server;
 import io.netty.channel.ChannelHandlerContext;
 
 /**
+ * The context of Gremlin Server within which a particular request is made.
+ *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public class Context {
+
+    /**
+     * The current request.
+     */
     private final RequestMessage requestMessage;
+
+    /**
+     * The Netty context.
+     */
     private final ChannelHandlerContext channelHandlerContext;
+
+    /**
+     * The current configuration of Gremlin Server.
+     */
     private final Settings settings;
+
+    /**
+     * The set of {@link com.tinkerpop.blueprints.Graph} objects configured in Gremlin Server.
+     */
     private final GremlinServer.Graphs graphs;
+
+    /**
+     * The executor chosen to evaluate incoming Gremlin scripts based on the request.
+     */
     private final GremlinExecutor gremlinExecutor;
 
     public Context(final RequestMessage requestMessage, final ChannelHandlerContext ctx,
