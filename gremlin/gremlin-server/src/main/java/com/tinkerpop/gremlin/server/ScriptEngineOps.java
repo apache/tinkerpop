b3fbc255c37329f77b97138c227bca5d0a55c462
Stephen Mallette <spmva@genoprime.com>
2014-01-02 09:35:12 -0500

    Add javadoc for Gremlin Server.

--- gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/ScriptEngineOps.java
+++ gremlin/gremlin-server/src/main/java/com/tinkerpop/gremlin/server/ScriptEngineOps.java
@@ -9,44 +9,45 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 /**
- * Features that ScriptEngines expose regardless of whether they are in a session or shared.
+ * Features that {@code ScriptEngine} objects expose regardless of whether they are in a session or shared.
  *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public interface ScriptEngineOps {
     /**
-     * Evaluate a script with Bindings for a particular language.
+     * Evaluate a script with {@code Bindings} for a particular language.
      */
     public Object eval(final String script, final Bindings bindings, final String language)
             throws ScriptException, InterruptedException, ExecutionException, TimeoutException;
 
     /**
-     * Perform append to the existing import list for all ScriptEngine instances that implement the DependencyManager
-     * interface.
+     * Perform append to the existing import list for all {@code ScriptEngine} instances that implement the
+     * {@link com.tinkerpop.gremlin.groovy.jsr223.DependencyManager} interface.
      */
     public void addImports(final Set<String> imports);
 
     /**
-     * Pull in dependencies given some Maven coordinates.  Cycle through each ScriptEngine and determine if it
-     * implements DependencyManager.  For those that do call the DependencyManager.use() method to fire it up.
+     * Pull in dependencies given some Maven coordinates.  Cycle through each {@code ScriptEngine} object and determine
+     * if it implements {@link com.tinkerpop.gremlin.groovy.jsr223.DependencyManager}.  For those that do call the
+     * {@link com.tinkerpop.gremlin.groovy.jsr223.DependencyManager#use} method to fire it up.
      */
     public void use(final String group, final String artifact, final String version);
 
     /**
-     * Get a list of all dependencies loaded to the ScriptEngine where the key is the ScriptEngine name and the value is the
-     * dependency list for that ScriptEngine.
+     * Get a list of all dependencies loaded to the {@code ScriptEngine} where the key is the {@code ScriptEngine}
+     * name and the value is the dependency list for that {@code ScriptEngine}.
      */
     public Map<String,List<Map>> dependencies();
 
     /**
-     * Gets a list of all imports in the ScriptEngine where the key is the ScriptEngine name and the value is the
-     * import list for that ScriptEngine.
+     * Gets a list of all imports in the {@code ScriptEngine} where the key is the {@code ScriptEngine} name and the
+     * value is the import list for that {@code ScriptEngine}.
      */
     public Map<String, List<Map>> imports();
 
     /**
-     * Resets the ScriptEngine which recreates the classloader and desstroys all caches of compiled scripts. Doing
-     * a reset will imply a potential slowdown to server operations as future executions will have to recompile
+     * Resets the {@code ScriptEngine} which recreates the classloader and destroys all caches of compiled scripts.
+     * Doing a reset will imply a potential slowdown to server operations as future executions will have to recompile
      * scripts and potentially reload classes.
      */
     public void reset();
