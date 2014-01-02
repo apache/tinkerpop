5031f8611e4d4222a6c0c3f160c181ce9c399a83
Stephen Mallette <spmva@genoprime.com>
2014-01-02 15:31:19 -0500

    More tests around RequestMessage.Builder.

--- gremlin/gremlin-server/src/test/java/com/tinkerpop/gremlin/server/RequestMessageTest.java
+++ gremlin/gremlin-server/src/test/java/com/tinkerpop/gremlin/server/RequestMessageTest.java
@@ -1,10 +1,15 @@
 package com.tinkerpop.gremlin.server;
 
+import com.tinkerpop.gremlin.server.op.standard.StandardOpProcessor;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
@@ -21,6 +26,36 @@ public class RequestMessageTest {
         final RequestMessage msg = new RequestMessage.Builder("op").newSession().build();
         assertTrue(msg.optionalSessionId().isPresent());
         assertEquals(msg.sessionId, msg.optionalSessionId().get());
+    }
+
+    @Test
+    public void shouldOverrideSession() {
+        final UUID session = UUID.randomUUID();
+        final RequestMessage msg = new RequestMessage.Builder("op").existingSession(session).build();
+        assertEquals(session, msg.sessionId);
+    }
+
+    @Test
+    public void shouldOverrideRequest() {
+        final UUID request = UUID.randomUUID();
+        final RequestMessage msg = new RequestMessage.Builder("op").overrideRequestId(request).build();
+        assertEquals(request, msg.requestId);
+    }
+
+    @Test
+    public void shouldSetProcessor() {
+        final RequestMessage msg = new RequestMessage.Builder("op").setProcessor("ppp").build();
+        assertEquals("ppp", msg.processor);
+    }
+
+    @Test
+    public void shouldSetOpWithDefaults() {
+        final RequestMessage msg = new RequestMessage.Builder("op").build();
+        assertEquals(StandardOpProcessor.OP_PROCESSOR_NAME, msg.processor);
+        assertNotNull(msg.requestId);
+        assertNull(msg.sessionId);
+        assertEquals("op", msg.op);
+        assertNotNull(msg.args);
     }
 
     @Test
