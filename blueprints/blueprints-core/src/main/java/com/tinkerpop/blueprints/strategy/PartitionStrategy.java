eb845ba1ef0de5f643f8abacd7b941a045da9ab1
Stephen Mallette <spmva@genoprime.com>
2014-01-07 06:35:48 -0500

    Add accessors for PartitionGraph settings.

--- blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/strategy/PartitionStrategy.java
+++ blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/strategy/PartitionStrategy.java
@@ -2,21 +2,48 @@ package com.tinkerpop.blueprints.strategy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
-
 public class PartitionStrategy implements Strategy {
 
     private String writePartition;
     private final String partitionKey;
+    private final Set<String> readPartitions = new HashSet<>();
 
     public PartitionStrategy(final String partitionKey, final String partition) {
         this.writePartition = partition;
         this.partitionKey = partitionKey;
+    }
+
+    public String getWritePartition() {
+        return writePartition;
+    }
+
+    public void setWritePartition(final String writePartition) {
+        this.writePartition = writePartition;
+    }
+
+    public String getPartitionKey() {
+        return partitionKey;
+    }
+
+    public Set<String> getReadPartitions() {
+        return Collections.unmodifiableSet(readPartitions);
+    }
+
+    public void removeReadPartition(final String readPartition) {
+        this.readPartitions.remove(readPartition);
+    }
+
+    public void addReadPartition(final String readPartition) {
+        this.readPartitions.add(readPartition);
     }
 
     @Override
