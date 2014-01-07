0b3df250056b267891f7da103bd787f1506e61d1
Stephen Mallette <spmva@genoprime.com>
2014-01-07 07:28:37 -0500

    Added javadoc.

--- blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/Strategy.java
+++ blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/Strategy.java
@@ -12,7 +12,17 @@ public interface Strategy {
     public void set(final Optional<GraphStrategy> strategy);
     public Optional<GraphStrategy> get();
 
-    public default <T> T ifPresent(final Function<GraphStrategy, T> f, final T args) {
-        return get().isPresent() ? f.apply(get().get()) : args;
+    /**
+     * A variation on {@link Optional#ifPresent(java.util.function.Consumer)} where the argument is a
+     * {@link Function} instead of a {@link java.util.function.Consumer} that can only handle side-effects. The
+     * {@link Function} is only applied if a {@link GraphStrategy} is present.   When applied the {@link GraphStrategy}
+     * becomes the first argument to this function and the return value should be equivalent to the {@code defaultVal}
+     * that will be returned if the {@link GraphStrategy} is empty.
+     *
+     * @param f a function to execute if a {@link GraphStrategy} is present.
+     * @param defaultVal the value to return if no {@link GraphStrategy} is present.
+     */
+    public default <T> T ifPresent(final Function<GraphStrategy, T> f, final T defaultVal) {
+        return get().isPresent() ? f.apply(get().get()) : defaultVal;
     }
 }
