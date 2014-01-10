c6fd3cac13fd01c5b9ca5777bab38283224adc7c
Stephen Mallette <spmva@genoprime.com>
2014-01-09 20:14:57 -0500

    Add javadoc on Strategy.

--- blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/Strategy.java
+++ blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/Strategy.java
@@ -5,15 +5,27 @@ import com.tinkerpop.blueprints.strategy.GraphStrategy;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 /**
+ * A holder of a {@link GraphStrategy} object owned by a {@link Graph} instance.
+ *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public interface Strategy {
+
+    /**
+     * Set the {@link GraphStrategy} to utilized in the various Blueprints methods that it supports.  Set to
+     * {@link Optional#EMPTY} by default.
+     */
     public void set(final Optional<? extends GraphStrategy> strategy);
+
+    /**
+     * Gets the {@link GraphStrategy} for the {@link Graph}.
+     */
     public Optional<? extends GraphStrategy> getGraphStrategy();
 
     /**
@@ -29,6 +41,12 @@ public interface Strategy {
         return getGraphStrategy().isPresent() ? f.apply(getGraphStrategy().get()).apply(impl) : impl;
     }
 
+    /**
+     * The {@link Context} object is provided to the methods of {@link GraphStrategy} so that the strategy functions
+     * it constructs have some knowledge of the environment.
+     *
+     * @param <T> represents the object that is calling the strategy (i.e. the vertex on which addEdge was called).
+     */
     public static class Context<T> {
         private final Graph g;
         private final Map<String,Object> environment;
@@ -39,12 +57,9 @@ public interface Strategy {
         }
 
         public Context(final Graph g, final T current, final Optional<Map<String,Object>> environment) {
-            if (null == g)
-                throw new IllegalArgumentException("g");
-
-            this.g = g;
-            this.current = current;
-            this.environment = environment.orElse(new HashMap<>());
+            this.g = Objects.requireNonNull(g);
+            this.current = Objects.requireNonNull(current);
+            this.environment = Objects.requireNonNull(environment).orElse(new HashMap<>());
         }
 
         public T getCurrent() {
@@ -60,11 +75,15 @@ public interface Strategy {
         }
     }
 
+    /**
+     * Basic {@link Strategy} implementation where the {@link GraphStrategy} can be get or set.
+     */
     public static class Simple implements Strategy {
         private Optional<? extends GraphStrategy> strategy;
 
         @Override
         public void set(final Optional<? extends GraphStrategy> strategy) {
+            Objects.requireNonNull(strategy);
             this.strategy = strategy;
         }
 
@@ -74,6 +93,10 @@ public interface Strategy {
         }
     }
 
+    /**
+     * A {@link Strategy} implementation that does not allow the {@link GraphStrategy} to be set and returns an
+     * {@link Optional#EMPTY}.
+     */
     public static class None implements Strategy {
         @Override
         public void set(final Optional<? extends GraphStrategy> strategy) {
@@ -86,6 +109,9 @@ public interface Strategy {
         }
     }
 
+    /**
+     * A {@link Strategy} implementation where the {@link GraphStrategy} can be get or set as {@link ThreadLocal}.
+     */
     public static class Local implements Strategy {
         private ThreadLocal<Optional<? extends GraphStrategy>> strategy = new ThreadLocal<Optional<? extends GraphStrategy>>(){
             @Override
@@ -96,6 +122,7 @@ public interface Strategy {
 
         @Override
         public void set(final Optional<? extends GraphStrategy> strategy) {
+            Objects.requireNonNull(strategy);
             this.strategy.set(strategy);
         }
 
