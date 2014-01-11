9e9384dd85dba4f962e09d96e0820abedd0010c3
Stephen Mallette <spmva@genoprime.com>
2014-01-11 07:30:56 -0500

    Add HexFunctionTest.

---
+++ blueprints/blueprints-test/src/test/java/com/tinkerpop/blueprints/util/HexFunctionTest.java
@@ -0,0 +1,26 @@
+package com.tinkerpop.blueprints.util;
+
+import com.tinkerpop.blueprints.util.function.HexFunction;
+import org.junit.Test;
+
+import java.util.function.UnaryOperator;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class HexFunctionTest {
+    @Test
+    public void shouldApplyCurrentFunctionAndThenAnotherSuppliedOne() {
+        final HexFunction<String, String, String, String, String, String, String> f = (a,b,c,d,e,g) -> a + b + c + d + e + g;
+        final UnaryOperator<String> after = (s) -> s + "last";
+        assertEquals("123456last", f.andThen(after).apply("1", "2", "3", "4", "5", "6"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowIfAfterFunctionIsNull() {
+        final HexFunction<String, String, String, String, String, String, String> f = (a,b,c,d,e,g) -> a + b + c + e + g;
+        f.andThen(null);
+    }
+}
