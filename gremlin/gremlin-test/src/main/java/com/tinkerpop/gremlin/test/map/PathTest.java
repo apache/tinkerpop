6ecc43b7f4215ea2a75f4ab6fb789ca8807ca864
Stephen Mallette <spmva@genoprime.com>
2014-01-16 08:28:58 -0500

    Minor code cleanup in gremlin-test.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/test/map/PathTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/test/map/PathTest.java
@@ -24,20 +24,20 @@ public class PathTest {
     }
 
     public void test_g_v1_propertyXnameX_path(final Iterator<Path> pipe) {
-        Path path = pipe.next();
+        final Path path = pipe.next();
         assertFalse(pipe.hasNext());
         assertEquals(2, path.size());
-        assertEquals("marko", ((Vertex) path.get(0)).getValue("name"));
-        assertEquals("marko", path.get(1));
+        assertEquals("marko", ((Vertex) path.get(0)).<String>getValue("name"));
+        assertEquals("marko", path.<String>get(1));
     }
 
     public void test_g_v1_out_pathXage__nameX(final Iterator<Path> pipe) {
         int counter = 0;
-        Set<String> names = new HashSet<String>();
+        final Set<String> names = new HashSet<>();
         while (pipe.hasNext()) {
             counter++;
-            Path path = pipe.next();
-            assertEquals(Integer.valueOf(29), path.get(0));
+            final Path path = pipe.next();
+            assertEquals(Integer.valueOf(29), path.<Integer>get(0));
             assertTrue(path.get(1).equals("josh") || path.get(1).equals("vadas") || path.get(1).equals("lop"));
             names.add(path.get(1));
         }
@@ -49,11 +49,11 @@ public class PathTest {
         int counter = 0;
         while (pipe.hasNext()) {
             counter++;
-            Path path = pipe.next();
+            final Path path = pipe.next();
             assertEquals(3, path.size());
-            assertEquals("marko", ((Vertex) path.get(0)).getValue("name"));
-            assertEquals("josh", path.get(1));
-            assertEquals("java", path.get(2));
+            assertEquals("marko", ((Vertex) path.get(0)).<String>getValue("name"));
+            assertEquals("josh", path.<String>get(1));
+            assertEquals("java", path.<String>get(2));
         }
         assertEquals(2, counter);
     }
