a2e059a3972f3857aa0ceb5ec04c2ab602c6aede
Stephen Mallette <spmva@genoprime.com>
2014-01-18 08:40:09 -0500

    Exception consistency on vertex/edge setProperty.

--- blueprints/blueprints-test/src/main/java/com/tinkerpop/blueprints/ExceptionConsistencyTest.java
+++ blueprints/blueprints-test/src/main/java/com/tinkerpop/blueprints/ExceptionConsistencyTest.java
@@ -28,7 +28,7 @@ public class ExceptionConsistencyTest {
     @RunWith(Parameterized.class)
     public static class PropertyValidationOnAddTest extends AbstractBlueprintsTest {
 
-        @Parameterized.Parameters(name = "{index}: add({1})")
+        @Parameterized.Parameters(name = "{index}: expect - {1}")
         public static Iterable<Object[]> data() {
             return Arrays.asList(new Object[][]{
                     { new Object[] {"odd", "number", "arguments"},Element.Exceptions.providedKeyValuesMustBeAMultipleOfTwo()},
@@ -75,51 +75,35 @@ public class ExceptionConsistencyTest {
      * Checks that properties added to an {@link Element} are validated in a consistent way when they are set after
      * {@link Vertex} or {@link Edge} construction by throwing an appropriate exception.
      */
-    /*
     @RunWith(Parameterized.class)
     public static class PropertyValidationOnSetTest extends AbstractBlueprintsTest {
 
-        @Parameterized.Parameters(name = "{index}: validate({2})")
+        @Parameterized.Parameters(name = "{index}: expect - {2}")
         public static Iterable<Object[]> data() {
             return Arrays.asList(new Object[][]{
-                    { null, "val",
-                      Element.Exceptions.providedKeyValuesMustBeAMultipleOfTwo()},
-                    { Graph.Features.PropertyFeatures.FEATURE_PROPERTIES,
-                            new Object[] {"odd"},
-                            Element.Exceptions.providedKeyValuesMustBeAMultipleOfTwo()},
-                    { Graph.Features.PropertyFeatures.FEATURE_PROPERTIES,
-                            new Object[] {"odd", "number", 123, "test"},
-                            Element.Exceptions.providedKeyValuesMustHaveALegalKeyOnEvenIndices()},
-                    { Graph.Features.PropertyFeatures.FEATURE_PROPERTIES,
-                            new Object[] {"odd", null},
-                            Property.Exceptions.propertyValueCanNotBeNull()},
-                    { Graph.Features.PropertyFeatures.FEATURE_PROPERTIES,
-                            new Object[] {null, "val"},
-                            Element.Exceptions.providedKeyValuesMustHaveALegalKeyOnEvenIndices()},
-                    { Graph.Features.PropertyFeatures.FEATURE_PROPERTIES,
-                            new Object[] {"", "val"},
-                            Property.Exceptions.propertyKeyCanNotBeEmpty()}});
+                    { "k", null, Property.Exceptions.propertyValueCanNotBeNull()},
+                    { null, "v", Property.Exceptions.propertyKeyCanNotBeNull()},
+                    { Property.Key.ID, "v", Property.Exceptions.propertyKeyIdIsReserved()},
+                    { Property.Key.LABEL, "v", Property.Exceptions.propertyKeyLabelIsReserved()},
+                    { "", "v", Property.Exceptions.propertyKeyCanNotBeEmpty()}});
         };
 
         @Parameterized.Parameter(value = 0)
-        public String featureName;
-
-        @Parameterized.Parameter(value = 1)
         public String key;
 
-        @Parameterized.Parameter(value = 2)
+        @Parameterized.Parameter(value = 1)
         public Object val;
 
-        @Parameterized.Parameter(value = 3)
+        @Parameterized.Parameter(value = 2)
         public Exception expectedException;
 
         @Test
+        @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_PROPERTIES)
         public void testGraphAddVertex() throws Exception {
-            assumeThat(g.getFeatures().supports(Graph.Features.VertexPropertyFeatures.class, featureName), is(true));
-
             try {
-                this.g.addVertex(arguments);
-                fail(String.format("Call to addVertex should have thrown an exception with these arguments [%s]", arguments));
+                final Vertex v = this.g.addVertex();
+                v.setProperty(key, val);
+                fail(String.format("Call to Vertex.setProperty should have thrown an exception with these arguments [%s, %s]", key, val));
             } catch (Exception ex) {
                 assertEquals(expectedException.getClass(), ex.getClass());
                 assertEquals(expectedException.getMessage(), ex.getMessage());
@@ -127,18 +111,16 @@ public class ExceptionConsistencyTest {
         }
 
         @Test
+        @FeatureRequirement(featureClass = Graph.Features.EdgePropertyFeatures.class, feature = FEATURE_PROPERTIES)
         public void testGraphAddEdge() throws Exception {
-            assumeThat(g.getFeatures().supports(Graph.Features.EdgePropertyFeatures.class, featureName), is(true));
-
             try {
                 final Vertex v = this.g.addVertex();
-                v.addEdge("label", v, arguments);
-                fail(String.format("Call to addVertex should have thrown an exception with these arguments [%s]", arguments));
+                v.addEdge("label", v).setProperty(key, val);
+                fail(String.format("Call to Edge.setProperty should have thrown an exception with these arguments [%s, %s]", key, val));
             } catch (Exception ex) {
                 assertEquals(expectedException.getClass(), ex.getClass());
                 assertEquals(expectedException.getMessage(), ex.getMessage());
             }
         }
     }
-    */
 }
