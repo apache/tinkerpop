ad0ac63c7be3c8eed46ef8e35e74877d79f4bf8f
Stephen Mallette <spmva@genoprime.com>
2014-01-18 06:31:09 -0500

    Add javadoc.

--- blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/util/StreamFactory.java
+++ blueprints/blueprints-core/src/main/java/com/tinkerpop/blueprints/util/StreamFactory.java
@@ -7,26 +7,43 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 /**
+ * Utility methods for constructing {@link Stream} objects.
+ *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class StreamFactory {
 
+    /**
+     * Construct a {@link Stream} from an {@link Iterable}.
+     */
     public static <T> Stream<T> stream(final Iterable<T> iterable) {
         return StreamFactory.stream(iterable.iterator());
     }
 
+    /**
+     * Construct a parallel {@link Stream} from an {@link Iterable}.
+     */
     public static <T> Stream<T> parallelStream(final Iterable<T> iterable) {
         return StreamFactory.parallelStream(iterable.iterator());
     }
 
+    /**
+     * Construct a {@link Stream} from an {@link Iterator}.
+     */
     public static <T> Stream<T> stream(final Iterator<T> iterator) {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, Spliterator.IMMUTABLE), false);
     }
 
+    /**
+     * Construct a parallel {@link Stream} from an {@link Iterator}.
+     */
     public static <T> Stream<T> parallelStream(final Iterator<T> iterator) {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, Spliterator.IMMUTABLE), true);
     }
 
+    /**
+     * Construct an {@link Iterable} from an {@link Stream}.
+     */
     public static <T> Iterable<T> iterable(final Stream<T> stream) {
         return () -> stream.iterator();
     }
