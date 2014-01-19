c9cdf875bbb83636e4721842659308c5d65b3f46
Marko A. Rodriguez <okrammarko@gmail.com>
2014-01-19 14:30:50 -0700

    added RangeTest optimizations to VertexQueryOptimizer

--- gremlin/gremlin-java/src/main/java/com/tinkerpop/gremlin/pipes/util/optimizers/VertexQueryOptimizer.java
+++ gremlin/gremlin-java/src/main/java/com/tinkerpop/gremlin/pipes/util/optimizers/VertexQueryOptimizer.java
@@ -6,6 +6,7 @@ import com.tinkerpop.gremlin.Pipe;
 import com.tinkerpop.gremlin.Pipeline;
 import com.tinkerpop.gremlin.pipes.filter.HasPipe;
 import com.tinkerpop.gremlin.pipes.filter.IntervalPipe;
+import com.tinkerpop.gremlin.pipes.filter.RangePipe;
 import com.tinkerpop.gremlin.pipes.map.EdgeVertexPipe;
 import com.tinkerpop.gremlin.pipes.map.IdentityPipe;
 import com.tinkerpop.gremlin.pipes.map.VertexQueryPipe;
@@ -17,7 +18,7 @@ import com.tinkerpop.gremlin.pipes.util.GremlinHelper;
 public class VertexQueryOptimizer implements Optimizer.StepOptimizer, Optimizer {
 
     public boolean optimize(final Pipeline pipeline, final Pipe pipe) {
-        if (!(pipe instanceof HasPipe || pipe instanceof IntervalPipe || pipe instanceof EdgeVertexPipe))
+        if (!(pipe instanceof HasPipe || pipe instanceof IntervalPipe || pipe instanceof EdgeVertexPipe || pipe instanceof RangePipe))
             return true;
         else {
             if (GremlinHelper.isLabeled(pipe))
@@ -31,7 +32,8 @@ public class VertexQueryOptimizer implements Optimizer.StepOptimizer, Optimizer 
                 break;
             } else if (!(pipeline.getPipes().get(i) instanceof IdentityPipe
                     || pipeline.getPipes().get(i) instanceof HasPipe
-                    || pipeline.getPipes().get(i) instanceof IntervalPipe))
+                    || pipeline.getPipes().get(i) instanceof IntervalPipe
+                    || pipeline.getPipes().get(i) instanceof RangePipe))
                 break;
         }
 
@@ -48,6 +50,10 @@ public class VertexQueryOptimizer implements Optimizer.StepOptimizer, Optimizer 
                 final IntervalPipe intervalPipe = (IntervalPipe) pipe;
                 vertexQueryPipe.queryBuilder.has(intervalPipe.startContainer.key, intervalPipe.startContainer.predicate, intervalPipe.startContainer.value);
                 vertexQueryPipe.queryBuilder.has(intervalPipe.endContainer.key, intervalPipe.endContainer.predicate, intervalPipe.endContainer.value);
+            } else if (pipe instanceof RangePipe) {
+                final RangePipe rangePipe = (RangePipe) pipe;
+                vertexQueryPipe.low = rangePipe.low;
+                vertexQueryPipe.high = rangePipe.high;
             } else {
                 throw new IllegalStateException("This pipe should not be accessible via this optimizer: " + pipe.getClass());
             }
