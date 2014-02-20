62c9116499cf032bc040519ef529c34107760bcc
Stephen Mallette <spmva@genoprime.com>
2014-02-19 20:19:50 -0500

    Add test for vertex id maker in IdGraphStrategy.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/IdGraphStrategyTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/IdGraphStrategyTest.java
@@ -7,6 +7,8 @@ import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Property;
 import com.tinkerpop.gremlin.structure.Vertex;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -19,51 +21,80 @@ import static org.junit.Assert.assertNotNull;
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
-public class IdGraphStrategyTest extends AbstractGremlinTest {
+@RunWith(Enclosed.class)
+public class IdGraphStrategyTest {
     private static final String idKey = "myId";
 
-    public IdGraphStrategyTest() {
-        super(Optional.of(new IdGraphStrategy.Builder(idKey).build()));
+    public static class DefaultIdGraphStrategyTest extends AbstractGremlinTest {
+        public DefaultIdGraphStrategyTest() {
+            super(Optional.of(new IdGraphStrategy.Builder(idKey).build()));
+        }
+
+        @Test
+        @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
+        @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
+        public void shouldInjectAnIdAndReturnBySpecifiedId() {
+            final Vertex v = g.addVertex(Element.ID, "test", "something", "else");
+
+            tryCommit(g, c -> {
+                assertNotNull(v);
+                assertEquals("test", v.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("else", v.getProperty("something").get());
+
+                final Vertex found = g.v("test");
+                assertEquals(v, found);
+                assertEquals("test", found.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("else", found.getProperty("something").get());
+
+            });
+        }
+
+        @Test
+        @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
+        @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
+        public void shouldCreateAnIdAndReturnByCreatedId() {
+            final Vertex v = g.addVertex("something", "else");
+
+            tryCommit(g, c -> {
+                assertNotNull(v);
+                assertNotNull(UUID.fromString(v.getProperty(Property.Key.hidden(idKey)).get().toString()));
+                assertEquals("else", v.getProperty("something").get());
+
+                final Object suppliedId = v.getId();
+                final Vertex found = g.v(suppliedId);
+                assertEquals(v, found);
+                assertNotNull(UUID.fromString(found.getProperty(Property.Key.hidden(idKey)).get().toString()));
+                assertEquals("else", found.getProperty("something").get());
+
+            });
+        }
     }
 
-    @Test
-    @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
-    @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
-    public void shouldInjectAnIdAndReturnBySpecifiedId() {
-        final Vertex v = g.addVertex(Element.ID, "test", "something", "else");
+    public static class VertexIdMakerIdGraphStrategyTest extends AbstractGremlinTest {
+        public VertexIdMakerIdGraphStrategyTest() {
+            super(Optional.of(new IdGraphStrategy.Builder(idKey)
+                    .vertexIdMaker(() -> "100").build()));
+        }
 
-        tryCommit(g, c -> {
-            assertNotNull(v);
-            assertEquals("test", v.getProperty(Property.Key.hidden(idKey)).get());
-            assertEquals("else", v.getProperty("something").get());
+        @Test
+        @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
+        @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
+        public void shouldCreateAnIdAndReturnByCreatedId() {
+            final Vertex v = g.addVertex("something", "else");
 
-            final Vertex found = g.v("test");
-            assertEquals(v, found);
-            assertEquals("test", found.getProperty(Property.Key.hidden(idKey)).get());
-            assertEquals("else", found.getProperty("something").get());
+            tryCommit(g, c -> {
+                assertNotNull(v);
+                assertEquals("100", v.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("else", v.getProperty("something").get());
 
-        });
+                final Vertex found = g.v("100");
+                assertEquals(v, found);
+                assertEquals("100", found.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("else", found.getProperty("something").get());
+
+            });
+        }
     }
 
-    @Test
-    @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
-    @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
-    public void shouldCreateAnIdAndReturnBySpecifiedId() {
-        final Vertex v = g.addVertex("something", "else");
-
-        tryCommit(g, c -> {
-            assertNotNull(v);
-            assertNotNull(UUID.fromString(v.getProperty(Property.Key.hidden(idKey)).get().toString()));
-            assertEquals("else", v.getProperty("something").get());
-
-            final Object suppliedId = v.getId();
-            final Vertex found = g.v(suppliedId);
-            assertEquals(v, found);
-            assertNotNull(UUID.fromString(found.getProperty(Property.Key.hidden(idKey)).get().toString()));
-            assertEquals("else", found.getProperty("something").get());
-
-        });
-    }
-
-    // todo: test g.e()
+    // todo: test edge creation still
 }
