ff9f43b6e40e28fb73af68e934a6c427b8c5d0d7
Stephen Mallette <spmva@genoprime.com>
2014-02-20 08:27:45 -0500

    Add test for generating a custom id for IdGraphStrategy.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/IdGraphStrategyTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/IdGraphStrategyTest.java
@@ -117,7 +117,6 @@ public class IdGraphStrategyTest {
         @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
         public void shouldCreateAnIdAndReturnByCreatedId() {
             final Vertex v = g.addVertex("something", "else");
-
             tryCommit(g, c -> {
                 assertNotNull(v);
                 assertEquals("100", v.getProperty(Property.Key.hidden(idKey)).get());
@@ -128,6 +127,31 @@ public class IdGraphStrategyTest {
                 assertEquals("100", found.getProperty(Property.Key.hidden(idKey)).get());
                 assertEquals("else", found.getProperty("something").get());
 
+            });
+        }
+    }
+
+    public static class EdgeIdMakerIdGraphStrategyTest extends AbstractGremlinTest {
+        public EdgeIdMakerIdGraphStrategyTest() {
+            super(Optional.of(new IdGraphStrategy.Builder(idKey)
+                    .edgeIdMaker(() -> "100").build()));
+        }
+
+        @Test
+        @FeatureRequirement(featureClass = Graph.Features.GraphFeatures.class, feature = FEATURE_STRATEGY)
+        @FeatureRequirement(featureClass = Graph.Features.VertexPropertyFeatures.class, feature = FEATURE_STRING_VALUES)
+        public void shouldCreateAnIdAndReturnByCreatedId() {
+            final Vertex v = g.addVertex("something", "else");
+            final Edge e = v.addEdge("self", v, "try", "this");
+            tryCommit(g, c -> {
+                assertNotNull(e);
+                assertEquals("100", e.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("this", e.getProperty("try").get());
+
+                final Edge found = g.e("100");
+                assertEquals(e, found);
+                assertEquals("100", found.getProperty(Property.Key.hidden(idKey)).get());
+                assertEquals("this", found.getProperty("try").get());
             });
         }
     }
