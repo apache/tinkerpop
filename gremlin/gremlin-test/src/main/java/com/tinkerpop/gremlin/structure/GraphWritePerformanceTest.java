33006ce19e0ffc6001c6386f05c7ae0a8206ff8a
Stephen Mallette <spmva@genoprime.com>
2014-02-28 13:10:20 -0500

    Add performance tests for IO.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/GraphWritePerformanceTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/GraphWritePerformanceTest.java
@@ -8,12 +8,18 @@ import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
 import com.carrotsearch.junitbenchmarks.annotation.LabelType;
 import com.tinkerpop.gremlin.AbstractGremlinSuite;
 import com.tinkerpop.gremlin.AbstractGremlinTest;
+import com.tinkerpop.gremlin.LoadGraphWith;
+import com.tinkerpop.gremlin.structure.io.GraphWriter;
+import com.tinkerpop.gremlin.structure.io.graphml.GraphMLWriter;
+import com.tinkerpop.gremlin.structure.io.kryo.KryoWriter;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.util.Optional;
 
 /**
@@ -55,6 +61,33 @@ public class GraphWritePerformanceTest {
             }
 
             AbstractGremlinSuite.assertVertexEdgeCounts(verticesToGenerate, verticesToGenerate - 1).accept(g);
+        }
+    }
+
+    @AxisRange(min = 0, max = 1)
+    @BenchmarkMethodChart(filePrefix = "io-write")
+    @BenchmarkHistoryChart(labelWith = LabelType.CUSTOM_KEY, maxRuns = 20, filePrefix = "hx-io-write")
+    public static class WriteToIO extends AbstractGremlinTest {
+
+        @Rule
+        public TestRule benchmarkRun = new BenchmarkRule();
+
+        @Test
+        @LoadGraphWith(LoadGraphWith.GraphData.GRATEFUL)
+        @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 0, concurrency = BenchmarkOptions.CONCURRENCY_SEQUENTIAL)
+        public void writeKryo() throws Exception {
+            final GraphWriter writer = new KryoWriter.Builder(g).build();
+            final OutputStream os = new ByteArrayOutputStream();
+            writer.writeGraph(os);
+        }
+
+        @Test
+        @LoadGraphWith(LoadGraphWith.GraphData.GRATEFUL)
+        @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 0, concurrency = BenchmarkOptions.CONCURRENCY_SEQUENTIAL)
+        public void writeGraphML() throws Exception {
+            final GraphWriter writer = new GraphMLWriter.Builder(g).build();
+            final OutputStream os = new ByteArrayOutputStream();
+            writer.writeGraph(os);
         }
     }
 }
