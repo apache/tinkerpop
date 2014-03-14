917f6ca0ab803e0c20242710d6292811ccc8213b
Stephen Mallette <spmva@genoprime.com>
2014-03-14 10:16:24 -0400

    Do more full assertion of graph equality for generator tests.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/algorithm/generator/AbstractGeneratorTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/algorithm/generator/AbstractGeneratorTest.java
@@ -5,17 +5,32 @@ import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Vertex;
 import org.javatuples.Triplet;
 
+import java.util.List;
+
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public class AbstractGeneratorTest extends AbstractGremlinTest {
+
     protected boolean same(final Graph g1, final Graph g2) {
         return g1.V().toList().stream()
-                .map(v -> Triplet.with(v.getValue("oid"), v.inE().count(), v.outE().count()))
+                .map(v -> Triplet.<Integer, List<Vertex>, List<Vertex>>with(v.getValue("oid"), v.in().toList(), v.out().toList()))
                 .allMatch(p -> {
                     final Vertex v = (Vertex) g2.V().has("oid", p.getValue0()).next();
-                    return p.getValue1() == v.inE().count()
-                            && p.getValue2() == v.outE().count();
+                    final boolean sameInVertices = sameInVertices(v, p.getValue1());
+                    final boolean sameOutVertices = sameOutVertices(v, p.getValue2());
+                    System.out.println(String.format("Comparison on IN %s OUT %s", sameInVertices, sameOutVertices));
+                    return sameInVertices && sameOutVertices;
                 });
+    }
+
+    private boolean sameInVertices(final Vertex v, final List<Vertex> inVertices) {
+        return inVertices.stream()
+                .allMatch(inVertex -> v.in().filter(hv -> hv.get().getValue("oid") == inVertex.getValue("oid")).hasNext());
+    }
+
+    private boolean sameOutVertices(final Vertex v, final List<Vertex> outVertices) {
+        return outVertices.stream()
+                .allMatch(outVertex -> v.out().filter(hv -> hv.get().getValue("oid") == outVertex.getValue("oid")).hasNext());
     }
 }
