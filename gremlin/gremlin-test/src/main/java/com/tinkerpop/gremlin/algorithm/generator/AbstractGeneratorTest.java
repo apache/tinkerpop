8fe6f37a9d752034b5a6ba61fb2eec78de355025
Stephen Mallette <spmva@genoprime.com>
2014-03-14 10:23:07 -0400

    Add javadoc and cleanup debug code in generator tests.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/algorithm/generator/AbstractGeneratorTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/algorithm/generator/AbstractGeneratorTest.java
@@ -12,15 +12,18 @@ import java.util.List;
  */
 public class AbstractGeneratorTest extends AbstractGremlinTest {
 
+    /**
+     * Asserts that two graphs are the "same" in way of structure.  It assumes that the graphs both have vertices
+     * with an "oid" property that is an Integer value.  It iterates each vertex in graph 1, using the "oid" to
+     * lookup vertices in graph 2.  For each one found it checks both IN and OUT vertices to ensure that they
+     * attach to the same IN/OUT vertices given their "oid" properties.
+     */
     protected boolean same(final Graph g1, final Graph g2) {
         return g1.V().toList().stream()
                 .map(v -> Triplet.<Integer, List<Vertex>, List<Vertex>>with(v.getValue("oid"), v.in().toList(), v.out().toList()))
                 .allMatch(p -> {
                     final Vertex v = (Vertex) g2.V().has("oid", p.getValue0()).next();
-                    final boolean sameInVertices = sameInVertices(v, p.getValue1());
-                    final boolean sameOutVertices = sameOutVertices(v, p.getValue2());
-                    System.out.println(String.format("Comparison on IN %s OUT %s", sameInVertices, sameOutVertices));
-                    return sameInVertices && sameOutVertices;
+                    return sameInVertices(v, p.getValue1()) && sameOutVertices(v, p.getValue2());
                 });
     }
 
