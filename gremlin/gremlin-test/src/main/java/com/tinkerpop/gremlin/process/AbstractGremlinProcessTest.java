632628df2928c500c217e1321da27a33fad1cdc4
Stephen Mallette <spmva@genoprime.com>
2014-03-17 11:15:54 -0400

    Java doc for test class.

--- gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/process/AbstractGremlinProcessTest.java
+++ gremlin/gremlin-test/src/main/java/com/tinkerpop/gremlin/process/AbstractGremlinProcessTest.java
@@ -3,11 +3,23 @@ package com.tinkerpop.gremlin.process;
 import com.tinkerpop.gremlin.AbstractGremlinTest;
 
 /**
+ * Base test class for Gremlin Process tests.
+ *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public abstract class AbstractGremlinProcessTest extends AbstractGremlinTest {
+
+    /**
+     * Determines if a test case implementation of a process test uses graph computer.  This value should be
+     * set in the constructor of the class that implements this.
+     */
     protected boolean requiresGraphComputer;
 
+    /**
+     * Determines if a graph meets requirements for execution.  All gremlin process tests should check this
+     * method as part of a call to {@code assumeTrue} to ensure that the test doesn't require the computer
+     * feature or if it does require the computer feature then ensure that the graph being tested supports it.
+     */
     protected boolean graphMeetsTestRequirements() {
         return !requiresGraphComputer || g.getFeatures().graph().supportsComputer();
     }
