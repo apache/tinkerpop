ba36e99db04aea3b31eebd5e2cb0d412a207f2e8
Stephen Mallette <spmva@genoprime.com>
2014-03-24 10:59:25 -0400

    Stup out tests specific to Neo4j.

---
+++ neo4j-gremlin/src/test/java/com/tinkerpop/gremlin/neo4j/structure/Neo4jGraphTest.java
@@ -0,0 +1,18 @@
+package com.tinkerpop.gremlin.neo4j.structure;
+
+import org.junit.Test;
+
+/**
+ * These are tests specific to Neo4j.
+ *
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class Neo4jGraphTest {
+
+    // todo: include tests for proper updates to indices
+
+    @Test
+    public void shouldExecuteCypher() {
+        // todo: test cypher
+    }
+}
