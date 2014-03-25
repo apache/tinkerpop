1ce072f7d219fa835fac6d7b52a32b11a23667b2
Stephen Mallette <spmva@genoprime.com>
2014-03-25 12:14:33 -0400

    Optimize imports.

--- tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/process/TinkerGraphProcessComputerStandardTest.java
+++ tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/process/TinkerGraphProcessComputerStandardTest.java
@@ -1,17 +1,8 @@
 package com.tinkerpop.gremlin.tinkergraph.process;
 
-import com.tinkerpop.gremlin.AbstractGraphProvider;
 import com.tinkerpop.gremlin.process.ProcessComputerStandardSuite;
-import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.tinkergraph.TinkerGraphGraphProvider;
-import com.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
-import org.apache.commons.configuration.Configuration;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Executes the Standard Gremlin Process Test Suite using TinkerGraph.
--- tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/structure/TinkerGraphStructurePerformanceTest.java
+++ tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/structure/TinkerGraphStructurePerformanceTest.java
@@ -1,16 +1,8 @@
 package com.tinkerpop.gremlin.tinkergraph.structure;
 
-import com.tinkerpop.gremlin.AbstractGraphProvider;
 import com.tinkerpop.gremlin.structure.StructurePerformanceSuite;
-import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.tinkergraph.TinkerGraphGraphProvider;
-import org.apache.commons.configuration.Configuration;
 import org.junit.runner.RunWith;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
 
 /**
  * Executes the Gremlin Structure Performance Test Suite using TinkerGraph.
