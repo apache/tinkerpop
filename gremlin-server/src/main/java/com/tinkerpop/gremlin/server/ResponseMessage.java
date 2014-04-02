8f44c3e50d35aa243f25dd91af546612c1c506a3
Stephen Mallette <spmva@genoprime.com>
2014-04-02 15:25:24 -0400

    Forgot to add file on previous commit

---
+++ gremlin-server/src/main/java/com/tinkerpop/gremlin/server/ResponseMessage.java
@@ -0,0 +1,61 @@
+package com.tinkerpop.gremlin.server;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class ResponseMessage {
+
+    /**
+     * The current request that generated this response.
+     */
+    private final RequestMessage requestMessage;
+    private final ResultCode code;
+    private final Object result;
+
+    private ResponseMessage(final RequestMessage requestMessage, final ResultCode code, final Object result) {
+        this.requestMessage = requestMessage;
+        this.code = code;
+        this.result = result;
+    }
+
+    public RequestMessage getRequestMessage() {
+        return requestMessage;
+    }
+
+    public ResultCode getCode() {
+        return code;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public static Builder create(final RequestMessage requestMessage) {
+        return new Builder(requestMessage);
+    }
+
+    public static class Builder {
+        private final RequestMessage requestMessage;
+
+        private ResultCode code;
+        private Object result;
+
+        public Builder(final RequestMessage requestMessage) {
+            this.requestMessage = requestMessage;
+        }
+
+        public Builder code(final ResultCode code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder result(final Object result) {
+            this.result = result;
+            return this;
+        }
+
+        public ResponseMessage build() {
+            return new ResponseMessage(requestMessage, code, result);
+        }
+    }
+}
