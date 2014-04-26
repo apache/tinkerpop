ffcad801c17ef5cdc4f0908e46b856c5410edfee
Stephen Mallette <spmva@genoprime.com>
2014-04-26 15:09:00 -0400

    Removed commenting around tests.

--- gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/StructureStandardSuite.java
+++ gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/StructureStandardSuite.java
@@ -44,7 +44,6 @@ import java.util.stream.Stream;
  */
 public class StructureStandardSuite extends AbstractGremlinSuite {
 
-    /*
     private static final Class<?>[] allTests = new Class<?>[]{
         AnnotationTest.class,
         BatchTest.class,
@@ -66,8 +65,7 @@ public class StructureStandardSuite extends AbstractGremlinSuite {
         TransactionTest.class,
         VertexTest.class
     };
-    */
-    private static final Class<?>[] allTests = new Class<?>[]{IoTest.class};
+
     /**
      * This list of tests in the suite that will be executed.  Gremlin developers should add to this list
      * as needed to enforce tests upon implementations.
