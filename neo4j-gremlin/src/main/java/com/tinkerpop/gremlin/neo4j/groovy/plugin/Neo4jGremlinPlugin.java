c6598f86ccac53460a566fbfd586b1843e19f8e2
Stephen Mallette <spmva@genoprime.com>
2014-05-19 10:09:19 -0400

    Add gremlin plugin class for neo4j.

---
+++ neo4j-gremlin/src/main/java/com/tinkerpop/gremlin/neo4j/groovy/plugin/Neo4jGremlinPlugin.java
@@ -0,0 +1,31 @@
+package com.tinkerpop.gremlin.neo4j.groovy.plugin;
+
+import com.tinkerpop.gremlin.groovy.plugin.GremlinPlugin;
+import com.tinkerpop.gremlin.groovy.plugin.PluginAcceptor;
+import com.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class Neo4jGremlinPlugin implements GremlinPlugin {
+
+	private static final String IMPORT = "import ";
+	private static final String DOT_STAR = ".*";
+
+	private static final Set<String> IMPORTS = new HashSet<String>() {{
+		add(IMPORT + Neo4jGraph.class.getPackage().getName() + DOT_STAR);
+	}};
+
+	@Override
+	public String getName() {
+		return "neo4j";
+	}
+
+	@Override
+	public void pluginTo(final PluginAcceptor pluginAcceptor) {
+		pluginAcceptor.addImports(IMPORTS);
+	}
+}
