7be32bd42a5a2712af004c980392b931f8b48327
Joshua Shinavier <josh@fortytwo.net>
2014-06-05 11:56:47 -0400

    Created a drop-in replacement for EdgeVertexStep to be used within SubgraphStrategy

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/strategy/SubgraphStrategy.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/strategy/SubgraphStrategy.java
@@ -5,10 +5,13 @@ import com.tinkerpop.gremlin.process.TraversalStrategy;
 import com.tinkerpop.gremlin.process.graph.GraphTraversal;
 import com.tinkerpop.gremlin.process.graph.filter.FilterStep;
 import com.tinkerpop.gremlin.process.graph.map.EdgeVertexStep;
+import com.tinkerpop.gremlin.process.graph.map.FlatMapStep;
 import com.tinkerpop.gremlin.process.graph.map.GraphStep;
 import com.tinkerpop.gremlin.process.graph.map.VertexStep;
+import com.tinkerpop.gremlin.process.util.EmptyTraversal;
 import com.tinkerpop.gremlin.process.util.Reversible;
 import com.tinkerpop.gremlin.process.util.TraversalHelper;
+import com.tinkerpop.gremlin.structure.Direction;
 import com.tinkerpop.gremlin.structure.Edge;
 import com.tinkerpop.gremlin.structure.Element;
 import com.tinkerpop.gremlin.structure.Graph;
@@ -17,7 +20,9 @@ import com.tinkerpop.gremlin.structure.Vertex;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
@@ -28,12 +33,12 @@ import java.util.function.UnaryOperator;
  */
 public class SubgraphStrategy implements GraphStrategy {
 
-    private final Predicate<Vertex> vertexCriterion;
-    private final Predicate<Edge> edgeCriterion;
+    private final Predicate<Vertex> vertexPredicate;
+    private final Predicate<Edge> edgePredicate;
 
-    public SubgraphStrategy(final Predicate<Vertex> vertexCriterion, final Predicate<Edge> edgeCriterion) {
-        this.vertexCriterion = vertexCriterion;
-        this.edgeCriterion = edgeCriterion;
+    public SubgraphStrategy(final Predicate<Vertex> vertexPredicate, final Predicate<Edge> edgePredicate) {
+        this.vertexPredicate = vertexPredicate;
+        this.edgePredicate = edgePredicate;
     }
 
     @Override
@@ -68,12 +73,12 @@ public class SubgraphStrategy implements GraphStrategy {
     }
 
     private boolean testVertex(final Vertex vertex) {
-        return vertexCriterion.test(vertex);
+        return vertexPredicate.test(vertex);
     }
 
     private boolean testEdge(final Edge edge) {
-        // the edge must pass the edge criterion, and both of its incident vertices must also pass the vertex criterion
-        return edgeCriterion.test(edge) && edge.inV().hasNext() && edge.outV().hasNext();
+        // the edge must pass the edge predicate, and both of its incident vertices must also pass the vertex predicate
+        return edgePredicate.test(edge) && edge.inV().hasNext() && edge.outV().hasNext();
     }
 
     private boolean testElement(final Element element) {
@@ -101,10 +106,10 @@ public class SubgraphStrategy implements GraphStrategy {
         };
     }*/
 
-	@Override
-	public String toString() {
-		return SubgraphStrategy.class.getSimpleName();
-	}
+    @Override
+    public String toString() {
+        return SubgraphStrategy.class.getSimpleName();
+    }
 
     public class SubgraphGraphTraversalStrategy implements TraversalStrategy.FinalTraversalStrategy {
 
@@ -115,7 +120,8 @@ public class SubgraphStrategy implements GraphStrategy {
             final List<?> traversalSteps = traversal.getSteps();
             for (int ix = 0; ix < traversalSteps.size(); ix++) {
                 final int pos = ix;
-                if (stepsToLookFor.stream().anyMatch(c -> c.isAssignableFrom(traversalSteps.get(pos).getClass()))) positions.add(ix);
+                if (stepsToLookFor.stream().anyMatch(c -> c.isAssignableFrom(traversalSteps.get(pos).getClass())))
+                    positions.add(ix);
             }
 
             Collections.reverse(positions);
@@ -133,7 +139,77 @@ public class SubgraphStrategy implements GraphStrategy {
         }
 
         public String toString() {
-            return TraversalHelper.makeStepString(this, vertexCriterion, edgeCriterion);
+            return TraversalHelper.makeStepString(this, vertexPredicate, edgePredicate);
+        }
+    }
+
+    private class SubgraphEdgeVertexStep extends FlatMapStep<Edge, Vertex> implements Reversible {
+
+        public Direction direction;
+
+        public SubgraphEdgeVertexStep(final Traversal traversal, final Direction direction) {
+            super(traversal);
+            this.direction = direction;
+            this.setFunction(traverser -> {
+                Edge nextEdge = traverser.get();
+                if (testEdge(nextEdge)) {
+                    if (this.direction.equals(Direction.IN))
+                        return new VertexIterator(nextEdge.inV());
+                    else if (this.direction.equals(Direction.OUT))
+                        return new VertexIterator(nextEdge.outV());
+                    else
+                        return new VertexIterator(nextEdge.bothV());
+                } else {
+                    return new EmptyGraphTraversal();
+                }
+            });
+        }
+
+        public String toString() {
+            return TraversalHelper.makeStepString(this, this.direction);
+        }
+
+        public void reverse() {
+            this.direction = this.direction.opposite();
+        }
+    }
+
+    private class EmptyGraphTraversal<S, E> extends EmptyTraversal<S, E> implements GraphTraversal<S, E> {
+    }
+
+    private class VertexIterator implements Iterator<Vertex> {
+        private final Iterator<Vertex> baseIterator;
+
+        private Vertex nextElement;
+
+        private VertexIterator(Iterator<Vertex> baseIterator) {
+            this.baseIterator = baseIterator;
+        }
+
+        public boolean hasNext() {
+            return null != nextElement;
+        }
+
+        public Vertex next() {
+            if (null == nextElement) {
+                throw new NoSuchElementException();
+            }
+
+            Vertex tmp = nextElement;
+            advanceToNext();
+            return tmp;
+        }
+
+        private void advanceToNext() {
+            while (baseIterator.hasNext()) {
+                Vertex nextBaseElement = baseIterator.next();
+                if (testVertex(nextBaseElement)) {
+                    nextElement = nextBaseElement;
+                    return;
+                }
+            }
+
+            nextElement = null;
         }
     }
 }
--- tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/structure/util/subgraph/SubgraphTest.java
+++ tinkergraph-gremlin/src/test/java/com/tinkerpop/gremlin/tinkergraph/structure/util/subgraph/SubgraphTest.java
@@ -43,24 +43,24 @@ public class SubgraphTest {
 		sg.strategy().setGraphStrategy(strategyToTest);
 
         // three vertices are included in the subgraph
-        assertEquals(6, count(g.V().toList()));
-        assertEquals(3, count(sg.V().toList()));
+        assertEquals(6, g.V().count());
+        assertEquals(3, sg.V().count());
 
         // only two edges are present, even though edges are not explicitly excluded
         // (edges require their incident vertices)
-        assertEquals(6, count(g.E().toList()));
-        assertEquals(2, count(sg.E().toList()));
+        assertEquals(6, g.E().count());
+        assertEquals(2, sg.E().count());
 
 		final Vertex v1_g = g.v(1);
 		final Vertex v1_sg = sg.v(1);
-        assertEquals(2, count(v1_g.out("knows").toList()));
-        assertEquals(1, count(v1_sg.out("knows").toList()));
+        assertEquals(2, v1_g.out("knows").count());
+        assertEquals(1, v1_sg.out("knows").count());
 
-        assertEquals(2, count(g.v(1).out("knows").toList()));
-        assertEquals(1, count(sg.v(1).out("knows").toList()));
+        assertEquals(2, g.v(1).out("knows").count());
+        assertEquals(1, sg.v(1).out("knows").count());
 
-        assertEquals(2, count(g.v(1).outE("knows").toList()));
-        assertEquals(1, count(sg.v(1).outE("knows").toList()));
+        assertEquals(2, g.v(1).outE("knows").count());
+        assertEquals(1, sg.v(1).outE("knows").count());
     }
 
     @Test
@@ -72,10 +72,12 @@ public class SubgraphTest {
 
         Graph g = TinkerFactory.createClassic();
 
+        //*
         Function<Vertex, Boolean> vertexCriterion = vertex -> true;
         Function<Edge, Boolean> edgeCriterion = edge -> includedEdgeIds.contains((int) edge.id());
 
         Subgraph sg = new Subgraph(g, vertexCriterion, edgeCriterion);
+        //*/
 
         /*
         Predicate<Vertex> vertexCriterion = vertex -> true;
@@ -84,32 +86,26 @@ public class SubgraphTest {
         final Optional<GraphStrategy> strategyToTest = Optional.<GraphStrategy>of(new SubgraphStrategy(vertexCriterion, edgeCriterion));
         final StrategyWrappedGraph sg = new StrategyWrappedGraph(g);
         sg.strategy().setGraphStrategy(strategyToTest);
-        */
+        //*/
 
         // all vertices are here
-        assertEquals(6, count(g.V().toList()));
-        assertEquals(6, count(sg.V().toList()));
+        assertEquals(6, g.V().count());
+        assertEquals(6, sg.V().count());
 
         // only the given edges are included
-        assertEquals(6, count(g.E().toList()));
-        assertEquals(3, count(sg.E().toList()));
+        assertEquals(6, g.E().count());
+        assertEquals(3, sg.E().count());
 
-        assertEquals(2, count(g.v(1).outE("knows").toList()));
-        assertEquals(1, count(sg.v(1).outE("knows").toList()));
+        assertEquals(2, g.v(1).outE("knows").count());
+        assertEquals(1, sg.v(1).outE("knows").count());
+
+        assertEquals(2, g.v(1).outE("knows").count());
+        assertEquals(1, sg.v(1).outE("knows").count());
 
         // wrapped Traversal<Vertex, Vertex> takes into account the edges it must pass through
-        assertEquals(2, count(g.v(1).out("knows").toList()));
-        assertEquals(1, count(sg.v(1).out("knows").toList()));
-        assertEquals(2, count(g.v(4).out("created").toList()));
-        assertEquals(1, count(sg.v(4).out("created").toList()));
-    }
-
-    private <T> long count(final Iterable<T> iter) {
-        long count = 0;
-        for (T anIter : iter) {
-            count++;
-        }
-
-        return count;
+        assertEquals(2, g.v(1).out("knows").count());
+        assertEquals(1, sg.v(1).out("knows").count());
+        assertEquals(2, g.v(4).out("created").count());
+        assertEquals(1, sg.v(4).out("created").count());
     }
 }
