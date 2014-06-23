c6c151f6b71da35d262fd8c6615ede355f46b109
Stephen Mallette <spmva@genoprime.com>
2014-06-23 07:41:34 -0400

    Converted :submit command to groovy.

---
+++ gremlin-console/src/main/groovy/com/tinkerpop/gremlin/console/commands/SubmitCommand.groovy
@@ -0,0 +1,22 @@
+package com.tinkerpop.gremlin.console.commands
+
+import com.tinkerpop.gremlin.console.Mediator
+import org.codehaus.groovy.tools.shell.CommandSupport
+import org.codehaus.groovy.tools.shell.Groovysh
+
+/**
+ * Submit a script to a Gremlin Server instance.
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+class SubmitCommand extends CommandSupport {
+
+    private final Mediator mediator
+
+    public SubmitCommand(final Groovysh shell, final Mediator mediator) {
+        super(shell, ":submit", ":>")
+        this.mediator = mediator
+    }
+
+    @Override
+    public Object execute(final List<String> arguments) { return mediator.currentRemote().submit(arguments) }
+}
