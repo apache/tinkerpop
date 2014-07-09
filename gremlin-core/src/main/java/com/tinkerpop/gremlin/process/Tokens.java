043bd5d1fe9166326f04cf1779202faca707476c
Stephen Mallette <spmva@genoprime.com>
2014-07-09 08:45:44 -0400

    Remove Tokens as it only contained a "version" which is now retrieved from the manifest rather than being hardcoded.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/process/Tokens.java
+++
@@ -1,8 +0,0 @@
-package com.tinkerpop.gremlin.process;
-
-/**
- * @author Stephen Mallette (http://stephen.genoprime.com)
- */
-public class Tokens {
-    public static final String VERSION = "3.0.0-SNAPSHOT";
-}
