09e9b414e591ad502dd7076098c9051e2725be28
Marko A. Rodriguez <okrammarko@gmail.com>
2014-07-17 10:25:57 -0600

    added GroovyTreeTestImpl to make sure Closures->Lambdas work right. They do. Fixed a bug in GroovyCylicPathTest.

--- gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/process/graph/step/filter/GroovyCyclicPathTestImpl.groovy
+++ gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/process/graph/step/filter/GroovyCyclicPathTestImpl.groovy
@@ -1,5 +1,6 @@
 package com.tinkerpop.gremlin.process.graph.step.filter
 
+import com.tinkerpop.gremlin.process.Path
 import com.tinkerpop.gremlin.process.Traversal
 import com.tinkerpop.gremlin.structure.Vertex
 
@@ -10,5 +11,9 @@ class GroovyCyclicPathTestImpl extends CyclicPathTest {
 
     public Traversal<Vertex, Vertex> get_g_v1_outXcreatedX_inXcreatedX_cyclicPath(final Object v1Id) {
         g.v(v1Id).out("created").in("created").cyclicPath
+    }
+
+    public Traversal<Vertex, Path> get_g_v1_outXcreatedX_inXcreatedX_cyclicPath_path(final Object v1Id) {
+        g.v(v1Id).out("created").in("created").cyclicPath.path
     }
 }
---
+++ gremlin-groovy/src/test/groovy/com/tinkerpop/gremlin/process/graph/step/sideEffect/GroovyTreeTestImpl.groovy
@@ -0,0 +1,15 @@
+package com.tinkerpop.gremlin.process.graph.step.sideEffect
+
+import com.tinkerpop.gremlin.process.Traversal
+import com.tinkerpop.gremlin.process.graph.step.util.Tree
+import com.tinkerpop.gremlin.structure.Vertex
+
+/**
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+class GroovyTreeTestImpl extends TreeTest {
+
+    public Traversal<Vertex, Tree> get_g_v1_out_out_treeXnameX(final Object v1Id) {
+        return (Traversal) g.v(v1Id).out().out().tree{it.value("name")};
+    }
+}
