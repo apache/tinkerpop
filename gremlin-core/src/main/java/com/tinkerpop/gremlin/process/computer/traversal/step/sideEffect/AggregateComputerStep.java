a6dc36bb88a3c5b71b6a7300d262c66723f03ef9
Marko A. Rodriguez <okrammarko@gmail.com>
2014-07-17 12:13:53 -0600

    added optimizable jump(x,3) to do 3 loops. made JumpStep slightly more efficient too -- not by much unfortunately. added more JumpTest test cases.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/process/computer/traversal/step/sideEffect/AggregateComputerStep.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/process/computer/traversal/step/sideEffect/AggregateComputerStep.java
@@ -3,6 +3,7 @@ package com.tinkerpop.gremlin.process.computer.traversal.step.sideEffect;
 import com.tinkerpop.gremlin.process.Traversal;
 import com.tinkerpop.gremlin.process.graph.step.sideEffect.AggregateStep;
 import com.tinkerpop.gremlin.process.graph.step.sideEffect.StoreStep;
+import com.tinkerpop.gremlin.process.util.TraversalHelper;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -11,5 +12,7 @@ public class AggregateComputerStep<S> extends StoreStep<S> {
 
     public AggregateComputerStep(final Traversal traversal, final AggregateStep aggregateStep) {
         super(traversal, aggregateStep.variable, aggregateStep.functionRing.functions);
+        if (TraversalHelper.isLabeled(aggregateStep))
+            this.setAs(aggregateStep.getAs());
     }
 }
