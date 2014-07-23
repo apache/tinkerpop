2de1d1e1223b06a57afdf2f950a990865c63d9e1
Stephen Mallette <spmva@genoprime.com>
2014-07-23 16:49:31 -0400

    Get rid of lgpl colt dependencies in gremlin-core.
    Using hppc now from carrot labs.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/util/batch/cache/LongIDVertexCache.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/util/batch/cache/LongIDVertexCache.java
@@ -1,11 +1,9 @@
 package com.tinkerpop.gremlin.structure.util.batch.cache;
 
-import cern.colt.function.LongProcedure;
-import cern.colt.list.AbstractLongList;
-import cern.colt.list.LongArrayList;
-import cern.colt.map.AbstractLongObjectMap;
-import cern.colt.map.OpenLongObjectHashMap;
-
+import com.carrotsearch.hppc.LongArrayList;
+import com.carrotsearch.hppc.LongObjectMap;
+import com.carrotsearch.hppc.LongObjectOpenHashMap;
+import com.carrotsearch.hppc.procedures.LongProcedure;
 import com.tinkerpop.gremlin.structure.Vertex;
 
 /**
@@ -14,12 +12,12 @@ import com.tinkerpop.gremlin.structure.Vertex;
  */
 public class LongIDVertexCache implements VertexCache {
 
-    private final AbstractLongObjectMap map;
-    private final AbstractLongList mapKeysInCurrentTx;
+    private final LongObjectMap<Object> map;
+    private final LongArrayList mapKeysInCurrentTx;
     private final LongProcedure newTransactionProcedure;
 
     public LongIDVertexCache() {
-        map = new OpenLongObjectHashMap(AbstractIDVertexCache.INITIAL_CAPACITY);
+        map = new LongObjectOpenHashMap<>(AbstractIDVertexCache.INITIAL_CAPACITY);
         mapKeysInCurrentTx = new LongArrayList(AbstractIDVertexCache.INITIAL_TX_CAPACITY);
         newTransactionProcedure = new VertexConverterLP();
     }
@@ -61,23 +59,18 @@ public class LongIDVertexCache implements VertexCache {
      * See {@link LongIDVertexCache#newTransaction()}
      */
     private class VertexConverterLP implements LongProcedure {
-
         /**
          * Retrieve the Object associated with each long from {@code map}. If it
          * is an {@code instanceof Vertex}, then replace it in the map with
-         * {@link Vertex#id()}. Otherwise, do nothing. Always returns true.
-         *
-         * @param l Colt long list element
-         * @return true
+         * {@link Vertex#id()}. Otherwise, do nothing.
          */
         @Override
-        public final boolean apply(final long l) {
+        public void apply(long l) {
             final Object o = map.get(l);
             assert null != o;
             if (o instanceof Vertex) {
                 map.put(l, ((Vertex) o).id());
             }
-            return true; // tell forEach to apply us to next long
         }
     }
 }
