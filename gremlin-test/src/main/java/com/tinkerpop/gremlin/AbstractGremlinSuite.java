d48aaf48ca4381b588d68e071ca67d5ce61366a1
Stephen Mallette <spmva@genoprime.com>
2014-08-14 09:22:11 -0400

    Provide a method for a test implementation to opt out of a test.

--- gremlin-test/src/main/java/com/tinkerpop/gremlin/AbstractGremlinSuite.java
+++ gremlin-test/src/main/java/com/tinkerpop/gremlin/AbstractGremlinSuite.java
@@ -1,15 +1,20 @@
 package com.tinkerpop.gremlin;
 
 import com.tinkerpop.gremlin.structure.Graph;
+import org.junit.runner.Description;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.NoTestsRemainException;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -34,12 +39,39 @@ public abstract class AbstractGremlinSuite extends Suite {
         public Class<? extends GraphProvider> value();
     }
 
+    /**
+     * Defines a test in the suite that the implementer does not want to run.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Repeatable(IgnoreTests.class)
+    @Inherited
+    public @interface IgnoreTest {
+        public Class<?> test();
+        public String method();
+        public String reason();
+    }
+
+    /**
+     * Holds a collection of {@link IgnoreTest} enabling multiple {@link IgnoreTest} to be applied to a
+     * single suite.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Inherited
+    public @interface IgnoreTests {
+        IgnoreTest[] value();
+    }
+
     public AbstractGremlinSuite(final Class<?> klass, final RunnerBuilder builder, final Class<?>[] testsToExecute) throws InitializationError {
         this(klass, builder, testsToExecute, null);
     }
 
     public AbstractGremlinSuite(final Class<?> klass, final RunnerBuilder builder, final Class<?>[] testsToExecute, final Class<?>[] testsToEnforce) throws InitializationError {
         super(builder, klass, enforce(testsToExecute, testsToEnforce));
+
+        // filter out tests ignored by the implementation
+        registerIgnoredTests(klass);
 
         // figures out what the implementer assigned as the GraphProvider class and make it available to tests.
         // the klass is the Suite that implements this suite (e.g. GroovyTinkerGraphProcessStandardTest).
@@ -49,6 +81,22 @@ public abstract class AbstractGremlinSuite extends Suite {
             GraphManager.set((GraphProvider) graphProviderClass.newInstance());
         } catch (Exception ex) {
             throw new InitializationError(ex);
+        }
+    }
+
+    private void registerIgnoredTests(final Class<?> klass) throws InitializationError {
+        final IgnoreTest[] ignoreTests = klass.getAnnotationsByType(IgnoreTest.class);
+
+        if (ignoreTests != null && ignoreTests.length > 0) {
+            // validate annotation - test class and reason must be set
+            if (!Arrays.stream(ignoreTests).allMatch(ignore -> ignore.test() != null && ignore.reason() != null && !ignore.reason().isEmpty()))
+                throw new InitializationError("Check @IgnoreTest annotations - all must have a 'test' and 'reason' set");
+
+            try {
+                filter(new IgnoresFilter(ignoreTests));
+            } catch (NoTestsRemainException ex) {
+                throw new InitializationError(ex);
+            }
         }
     }
 
@@ -80,4 +128,38 @@ public abstract class AbstractGremlinSuite extends Suite {
         };
     }
 
+    /**
+     * Filter for tests in the suite which is controlled by the {@link IgnoreTest} annotation.
+     */
+    public static class IgnoresFilter extends Filter {
+
+        private final List<Description> testsToIgnore;
+
+        public IgnoresFilter(final IgnoreTest[] ignoreTests) {
+            testsToIgnore = Arrays.stream(ignoreTests)
+                    .<Description>map(ignoreTest -> Description.createTestDescription(ignoreTest.test(), ignoreTest.method()))
+                    .collect(Collectors.toList());
+        }
+
+        @Override
+        public boolean shouldRun(final Description description) {
+            if (description.isTest()) {
+                return !testsToIgnore.contains(description);
+            }
+
+            // explicitly check if any children want to run
+            for (Description each : description.getChildren()) {
+                if (shouldRun(each)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public String describe() {
+            return String.format("Method %s",
+                    String.join(",", testsToIgnore.stream().map(Description::getDisplayName).collect(Collectors.toList())));
+        }
+    }
 }
