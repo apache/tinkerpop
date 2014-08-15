844d52e629093f17164a2b0145f4eb5ce1b9df2e
Stephen Mallette <spmva@genoprime.com>
2014-08-15 15:18:01 -0400

    Break up tests in Neo4j to cover the cypher steps explicitly.

--- neo4j-gremlin/src/test/java/com/tinkerpop/gremlin/neo4j/process/graph/step/map/Neo4jCypherTest.java
+++ neo4j-gremlin/src/test/java/com/tinkerpop/gremlin/neo4j/process/graph/step/map/Neo4jCypherTest.java
@@ -1,7 +1,31 @@
 package com.tinkerpop.gremlin.neo4j.process.graph.step.map;
 
+import com.tinkerpop.gremlin.neo4j.BaseNeo4jGraphTest;
+import com.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-public class Neo4jCypherTest {
+public class Neo4jCypherTest extends BaseNeo4jGraphTest {
+
+    @Test
+    public void shouldExecuteCypherWithVerticesInput() throws Exception {
+        this.g.addVertex("name", "marko", "age", 29, "color", "yellow");
+        this.g.addVertex("name", "marko", "age", 30, "color", "yellow");
+        final Vertex v = this.g.addVertex("name", "marko", "age", 30, "color", "orange");
+        this.g.tx().commit();
+
+        final List result = g.cypher("MATCH n WHERE n.age=30 RETURN n").select("n").id().fold()
+                .cypher("MATCH n WHERE id(n) IN {start} AND n.color = \"orange\" RETURN n")
+                .select("n").id().toList();
+
+        assertEquals(1, result.size());
+        assertTrue(result.contains(v.id()));
+    }
 }
