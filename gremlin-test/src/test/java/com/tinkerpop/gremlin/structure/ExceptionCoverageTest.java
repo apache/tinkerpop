be018dec127c777f8b62679217c5f32144bd89ab
Stephen Mallette <spmva@genoprime.com>
2014-08-15 08:12:41 -0400

    Make exception coverage test more clear as to what is expected.

--- gremlin-test/src/test/java/com/tinkerpop/gremlin/structure/ExceptionCoverageTest.java
+++ gremlin-test/src/test/java/com/tinkerpop/gremlin/structure/ExceptionCoverageTest.java
@@ -1,18 +1,23 @@
 package com.tinkerpop.gremlin.structure;
 
 import com.tinkerpop.gremlin.process.computer.GraphComputer;
+import com.tinkerpop.gremlin.process.computer.GraphComputerTest;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
 
 /**
- * A set of tests that ensure that the {@link ExceptionConsistencyTest} covers all defined Blueprints exceptions.
+ * A set of tests that ensure that the {@link ExceptionConsistencyTest} covers all defined TinkerPop exceptions.
  *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
@@ -22,7 +27,7 @@ public class ExceptionCoverageTest {
     public void shouldCoverAllExceptionsInTests() {
 
         // these are the classes that have Exceptions that need to be checked.
-        final Class[] blueprintsExceptions = {
+        final Class[] exceptionDefinitionClasses = {
                 Edge.Exceptions.class,
                 Element.Exceptions.class,
                 Graph.Exceptions.class,
@@ -54,7 +59,6 @@ public class ExceptionCoverageTest {
             add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#adjacentVerticesCanNotBeQueried");
             add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#isolationNotSupported");
             //add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#computerHasAlreadyBeenSubmittedAVertexProgram");
-            add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#providedKeyIsNotASideEffectKey");
 
             add("com.tinkerpop.gremlin.structure.Graph$Exceptions#vertexAdditionsNotSupported");
             add("com.tinkerpop.gremlin.structure.Element$Exceptions#propertyAdditionNotSupported");
@@ -69,16 +73,21 @@ public class ExceptionCoverageTest {
 
         }};
 
+        // register test classes here that contain @ExceptionCoverage annotations
+        final List<Class<?>> testClassesThatContainConsistencyChecks = new ArrayList<>();
+        testClassesThatContainConsistencyChecks.add(GraphComputerTest.class);
+        testClassesThatContainConsistencyChecks.addAll(Arrays.asList(ExceptionConsistencyTest.class.getDeclaredClasses()));
+        testClassesThatContainConsistencyChecks.addAll(Arrays.asList(FeatureSupportTest.class.getDeclaredClasses()));
+
         // implemented exceptions are the classes that potentially contains exception consistency checks.
-        final Set<String> implementedExceptions = Stream.concat(Stream.of(ExceptionConsistencyTest.class.getDeclaredClasses()),
-                Stream.of(FeatureSupportTest.class.getDeclaredClasses()))
+        final Set<String> implementedExceptions = testClassesThatContainConsistencyChecks.stream()
                 .<ExceptionCoverage>flatMap(c -> Stream.of(c.getAnnotationsByType(ExceptionCoverage.class)))
                 .flatMap(ec -> Stream.of(ec.methods()).map(m -> String.format("%s#%s", ec.exceptionClass().getName(), m)))
                 .collect(Collectors.<String>toSet());
 
         // evaluate each of the blueprints exceptions and find the list of exception methods...assert them against
         // the list of implementedExceptions to make sure they are covered.
-        Stream.of(blueprintsExceptions).flatMap(c -> Stream.of(c.getDeclaredMethods()).map((Method m) -> String.format("%s#%s", c.getName(), m.getName())))
+        Stream.of(exceptionDefinitionClasses).flatMap(c -> Stream.of(c.getDeclaredMethods()).map((Method m) -> String.format("%s#%s", c.getName(), m.getName())))
                 .filter(s -> !ignore.contains(s))
                 .forEach(s -> {
                     System.out.println(s);
