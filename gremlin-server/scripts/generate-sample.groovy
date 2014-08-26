19db3c20260170f34ae11702aa319346aab29613
Stephen Mallette <spmva@genoprime.com>
2014-08-26 16:07:02 -0400

    Fixed sample generator syntax after some refactoring to all Builder method naming.

--- gremlin-server/scripts/generate-sample.groovy
+++ gremlin-server/scripts/generate-sample.groovy
@@ -1,5 +1,5 @@
 // Generates a graph into the "empty" graph using the graph generators. "g" refers to the empty graph configured in the standard yaml configuration
 (0..<10000).each { g.addVertex("oid", it) }
-DistributionGenerator.create(g).label("knows").seedGenerator {
+DistributionGenerator.build(g).label("knows").seedGenerator {
     987654321l
-}.outDistribution(new PowerLawDistribution(2.1)).inDistribution(new PowerLawDistribution(2.1)).expectedNumEdges(100000).build().generate();
+}.outDistribution(new PowerLawDistribution(2.1)).inDistribution(new PowerLawDistribution(2.1)).expectedNumEdges(100000).create().generate();
