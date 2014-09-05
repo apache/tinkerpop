8f422881a2139c0d4cd76c79e7b04562769c3c8a
Stephen Mallette <spmva@genoprime.com>
2014-09-05 13:13:35 -0400

    Add override annotations.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/MetaProperty.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/MetaProperty.java
@@ -14,8 +14,10 @@ public interface MetaProperty<V> extends Property<V>, Element {
     public static final String VALUE = "value";
     public static final String KEY = "key";
 
+    @Override
     public Vertex getElement();
 
+    @Override
     public default String label() {
         return META_PROPERTY;
     }
@@ -24,12 +26,15 @@ public interface MetaProperty<V> extends Property<V>, Element {
         return EmptyMetaProperty.instance();
     }
 
+    @Override
     public MetaProperty.Iterators iterators();
 
     public interface Iterators extends Element.Iterators {
 
+        @Override
         public <U> Iterator<Property<U>> properties(final String... propertyKeys);
 
+        @Override
         public <U> Iterator<Property<U>> hiddens(final String... propertyKeys);
     }
 
