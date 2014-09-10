3921f892a0ab4cd3065448103e9622d3076578ae
Stephen Mallette <spmva@genoprime.com>
2014-09-10 16:49:55 -0400

    Add javadoc to MetaProperty.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/MetaProperty.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/structure/MetaProperty.java
@@ -6,8 +6,17 @@ import com.tinkerpop.gremlin.structure.util.EmptyMetaProperty;
 import java.util.Iterator;
 
 /**
+ * A {@code MetaProperty} is similar to a {@link Property} in that it denotes a key/value pair associated with an
+ * {@link Vertex}, however it is different in the sense that it also represents an entity that it is an {@link Element}
+ * that can have properties of its own.
+ * <br/>
+ * A property is much like a Java8 {@link java.util.Optional} in that a property can be not present (i.e. empty).
+ * The key of a property is always a String and the value of a property is an arbitrary Java object.
+ * Each underlying graph engine will typically have constraints on what Java objects are allowed to be used as values.
+ *
  * @author Matthias Broecheler (me@matthiasb.com)
  * @author Marko A. Rodriguez (http://markorodriguez.com)
+ * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public interface MetaProperty<V> extends Property<V>, Element, MetaPropertyTraversal {
 
