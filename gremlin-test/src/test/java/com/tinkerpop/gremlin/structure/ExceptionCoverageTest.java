5832ad074d9add95cb2badb7df4edb3ffe3a0fd7
Stephen Mallette <spmva@genoprime.com>
2014-09-13 08:04:55 -0400

    Exception consistency covers vertex addition now.

--- gremlin-test/src/test/java/com/tinkerpop/gremlin/structure/ExceptionCoverageTest.java
+++ gremlin-test/src/test/java/com/tinkerpop/gremlin/structure/ExceptionCoverageTest.java
@@ -59,7 +59,6 @@ public class ExceptionCoverageTest {
             add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#adjacentVerticesCanNotBeQueried");
             add("com.tinkerpop.gremlin.process.computer.GraphComputer$Exceptions#isolationNotSupported");
 
-            add("com.tinkerpop.gremlin.structure.Graph$Exceptions#vertexAdditionsNotSupported");
             add("com.tinkerpop.gremlin.structure.Element$Exceptions#propertyAdditionNotSupported");
             add("com.tinkerpop.gremlin.structure.Vertex$Exceptions#edgeAdditionsNotSupported");
 
