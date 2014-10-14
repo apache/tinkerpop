d642036a6fa90526f6396a606dad42ec0a005a6d
Stephen Mallette <spmva@genoprime.com>
2014-10-14 15:53:32 -0400

    Add tests for groovy sugar for load/save kryo and graphson.

--- gremlin-groovy-test/src/main/groovy/com/tinkerpop/gremlin/groovy/loaders/GremlinLoaderTest.groovy
+++ gremlin-groovy-test/src/main/groovy/com/tinkerpop/gremlin/groovy/loaders/GremlinLoaderTest.groovy
@@ -1,6 +1,8 @@
 package com.tinkerpop.gremlin.groovy.loaders
 
 import com.tinkerpop.gremlin.AbstractGremlinTest
+import com.tinkerpop.gremlin.FeatureRequirement
+import com.tinkerpop.gremlin.FeatureRequirements
 import com.tinkerpop.gremlin.LoadGraphWith
 import com.tinkerpop.gremlin.structure.Graph
 import com.tinkerpop.gremlin.structure.IoTest
@@ -21,6 +23,11 @@ class GremlinLoaderTest extends AbstractGremlinTest {
     }
 
     @Test
+    @FeatureRequirements([
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_USER_SUPPLIED_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_STRING_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.EdgeFeatures.class, feature = Graph.Features.EdgeFeatures.FEATURE_ADD_EDGES),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_ADD_VERTICES)])
     @LoadGraphWith(LoadGraphWith.GraphData.CLASSIC)
     public void shouldSaveLoadGraphML() {
         def graphmlDirString = tempPath + File.separator + "graphml" + File.separator
@@ -36,6 +43,56 @@ class GremlinLoaderTest extends AbstractGremlinTest {
         gcopy.loadGraphML(graphml)
 
         IoTest.assertClassicGraph(gcopy, false, true)
+
+        graphProvider.clear(gcopy, conf)
+    }
+
+    @Test
+    @FeatureRequirements([
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_USER_SUPPLIED_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_NUMERIC_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.EdgeFeatures.class, feature = Graph.Features.EdgeFeatures.FEATURE_ADD_EDGES),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_ADD_VERTICES)])
+    @LoadGraphWith(LoadGraphWith.GraphData.CLASSIC)
+    public void shouldSaveLoadGraphSON() {
+        def graphsonDirString = tempPath + File.separator + "graphson" + File.separator
+        def graphsonDir = new File(graphsonDirString)
+        graphsonDir.deleteDir()
+        graphsonDir.mkdirs()
+
+        def graphson = graphsonDirString + UUID.randomUUID().toString() + ".json"
+        g.saveGraphSON(graphson)
+
+        def conf = graphProvider.newGraphConfiguration("g1", GremlinLoaderTest.class, name.methodName)
+        def gcopy = graphProvider.openTestGraph(conf)
+        gcopy.loadGraphSON(graphson)
+
+        IoTest.assertClassicGraph(gcopy, true, true)
+
+        graphProvider.clear(gcopy, conf)
+    }
+
+    @Test
+    @FeatureRequirements([
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_USER_SUPPLIED_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_NUMERIC_IDS),
+        @FeatureRequirement(featureClass = Graph.Features.EdgeFeatures.class, feature = Graph.Features.EdgeFeatures.FEATURE_ADD_EDGES),
+        @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_ADD_VERTICES)])
+    @LoadGraphWith(LoadGraphWith.GraphData.CREW)
+    public void shouldSaveLoadKryo() {
+        def kryoDirString = tempPath + File.separator + "kryo" + File.separator
+        def kryoDir = new File(kryoDirString)
+        kryoDir.deleteDir()
+        kryoDir.mkdirs()
+
+        def kryo = kryoDirString + UUID.randomUUID().toString() + ".json"
+        g.saveKryo(kryo)
+
+        def conf = graphProvider.newGraphConfiguration("g1", GremlinLoaderTest.class, name.methodName)
+        def gcopy = graphProvider.openTestGraph(conf)
+        gcopy.loadKryo(kryo)
+
+        IoTest.assertCrewGraph(gcopy, true)
 
         graphProvider.clear(gcopy, conf)
     }
