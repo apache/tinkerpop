b8b3bb74dfe996d154e05e796d9310345c34e2cf
Marko A. Rodriguez <okrammarko@gmail.com>
2014-10-20 14:02:34 -0600

    greatly simplified TraversalVertexProgram again. No need for a TraverserPathTracker and a TraverserCountTracker. With TraverserSet being the underlying data structure now, its just TraverserTracker. Much much simpler code -- especially in TraverserMapReducer. Stoked.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/process/computer/traversal/step/sideEffect/mapreduce/TraversalResultMapReduce.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/process/computer/traversal/step/sideEffect/mapreduce/TraversalResultMapReduce.java
@@ -1,10 +1,8 @@
 package com.tinkerpop.gremlin.process.computer.traversal.step.sideEffect.mapreduce;
 
-import com.tinkerpop.gremlin.process.Traverser;
 import com.tinkerpop.gremlin.process.computer.MapReduce;
 import com.tinkerpop.gremlin.process.computer.traversal.TraversalVertexProgram;
-import com.tinkerpop.gremlin.process.computer.traversal.TraverserCountTracker;
-import com.tinkerpop.gremlin.process.computer.traversal.TraverserPathTracker;
+import com.tinkerpop.gremlin.process.computer.traversal.TraverserTracker;
 import com.tinkerpop.gremlin.process.computer.util.GraphComputerHelper;
 import com.tinkerpop.gremlin.structure.Graph;
 import com.tinkerpop.gremlin.structure.Property;
@@ -30,31 +28,10 @@ public final class TraversalResultMapReduce implements MapReduce<MapReduce.NullO
     public void map(final Vertex vertex, final MapEmitter<MapReduce.NullObject, Object> emitter) {
         final Property mapProperty = vertex.property(TraversalVertexProgram.TRAVERSER_TRACKER);
         if (mapProperty.isPresent()) {
-            if (mapProperty.value() instanceof TraverserCountTracker) {
-                TraverserCountTracker tracker = (TraverserCountTracker) mapProperty.value();
-                tracker.getDoneObjectTracks().forEach((traverser, count) -> {
-                    traverser.setBulk(count); // TODO: Remove when updating TrackerCounter
-                    emitter.emit(NullObject.instance(), traverser);
-                });
-                tracker.getDoneGraphTracks().forEach((traverser, count) -> {
-                    traverser.setBulk(count); // TODO: Remove when updating TrackerCounter
-                    emitter.emit(NullObject.instance(), traverser);
-                });
-            } else {
-                TraverserPathTracker tracker = (TraverserPathTracker) mapProperty.value();
-                tracker.getDoneObjectTracks().forEach((object, traversers) -> {
-                    for (Traverser traverser : traversers) {
-                        emitter.emit(NullObject.instance(), traverser);
-                    }
-                });
-                tracker.getDoneGraphTracks().forEach((object, traversers) -> {
-                    for (Traverser traverser : traversers) {
-                        emitter.emit(NullObject.instance(), traverser);
-                    }
-                });
-            }
+            final TraverserTracker tracker = (TraverserTracker) mapProperty.value();
+            tracker.getDoneObjectTracks().forEach(emitter::emit);
+            tracker.getDoneGraphTracks().forEach(emitter::emit);
         }
-
     }
 
     @Override
