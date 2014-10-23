f7fa1a8b9121aad6ad68b60017ae68ce020b4530
Marko A. Rodriguez <okrammarko@gmail.com>
2014-10-23 09:46:35 -0600

    dah.

--- gremlin-groovy-test/src/main/java/com/tinkerpop/gremlin/process/GroovyProcessComputerSuite.java
+++ gremlin-groovy-test/src/main/java/com/tinkerpop/gremlin/process/GroovyProcessComputerSuite.java
@@ -50,7 +50,7 @@ public class GroovyProcessComputerSuite extends ProcessComputerSuite {
      */
     private static final Class<?>[] testsToExecute = new Class<?>[]{
 
-            //GroovyGraphComputerTest.ComputerTest.class,
+            GroovyGraphComputerTest.ComputerTest.class,
 
             //branch
             GroovyChooseTest.ComputerTest.class,
