0080b6b1cbd3e9e561e98ce7fb615ad881396e80
Stephen Mallette <spmva@genoprime.com>
2014-10-23 13:29:01 -0400

    Improvement on the bump.sh from @dkuppitz to not require us to know all the pom.xml files ahead of time or line numbers.

--- bin/bump.sh
+++ bin/bump.sh
@@ -5,22 +5,17 @@
 #
 # bin/bump.sh "version"
 
-# pom.xml - match the first occurrence of <version>*</version> after line X and replace
-sed '11,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' pom.xml > pom1.xml && mv pom1.xml pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' giraph-gremlin/pom.xml > giraph-gremlin/pom1.xml && mv giraph-gremlin/pom1.xml giraph-gremlin/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-algorithm/pom.xml > gremlin-algorithm/pom1.xml && mv gremlin-algorithm/pom1.xml gremlin-algorithm/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-console/pom.xml > gremlin-console/pom1.xml && mv gremlin-console/pom1.xml gremlin-console/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-core/pom.xml > gremlin-core/pom1.xml && mv gremlin-core/pom1.xml gremlin-core/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-driver/pom.xml > gremlin-driver/pom1.xml && mv gremlin-driver/pom1.xml gremlin-driver/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-groovy/pom.xml > gremlin-groovy/pom1.xml && mv gremlin-groovy/pom1.xml gremlin-groovy/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-groovy-test/pom.xml > gremlin-groovy-test/pom1.xml && mv gremlin-groovy-test/pom1.xml gremlin-groovy-test/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-server/pom.xml > gremlin-server/pom1.xml && mv gremlin-server/pom1.xml gremlin-server/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' gremlin-test/pom.xml > gremlin-test/pom1.xml && mv gremlin-test/pom1.xml gremlin-test/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' neo4j-gremlin/pom.xml > neo4j-gremlin/pom1.xml && mv neo4j-gremlin/pom1.xml neo4j-gremlin/pom.xml
-sed '1,/<version>.*<\/version>/{s/<version>.*<\/version>/<version>'"$1"'<\/version>/}' tinkergraph-gremlin/pom.xml > tinkergraph-gremlin/pom1.xml && mv tinkergraph-gremlin/pom1.xml tinkergraph-gremlin/pom.xml
+VERSION=$1
 
-# YAML configuration
-sed 's/\[com.tinkerpop, neo4j-gremlin, ".*"\]/\[com.tinkerpop, neo4j-gremlin, "'"$1"'"\]/' gremlin-server/conf/gremlin-server-neo4j.yaml > gremlin-server/conf/gremlin-server-neo4j1.yaml && mv gremlin-server/conf/gremlin-server-neo4j1.yaml gremlin-server/conf/gremlin-server-neo4j.yaml
+# update pom.xml
+for pom in $(find . -name pom.xml); do
+  cat $pom | grep -n -A2 -B2 '<groupId>com.tinkerpop</groupId>' \
+           | grep -A2 -B2 '<artifactId>tinkerpop</artifactId>'  \
+           | grep '<version>' | cut -f1 -d '-' | xargs -n1 -I{} sed -i -e "{}s@>.*<@>${VERSION}<@" $pom
+done
 
-# README
-sed 's/\(http:\/\/tinkerpop.com\/.*docs\/\)[A-Za-z0-9.-]*\/\(.*\)/\1'"$1"'\/\2/' README.asciidoc > README1.asciidoc && mv README1.asciidoc README.asciidoc
+# update YAML configuration
+sed 's/\[com.tinkerpop, neo4j-gremlin, ".*"\]/\[com.tinkerpop, neo4j-gremlin, "'"${VERSION}"'"\]/' gremlin-server/conf/gremlin-server-neo4j.yaml > gremlin-server/conf/gremlin-server-neo4j1.yaml && mv gremlin-server/conf/gremlin-server-neo4j1.yaml gremlin-server/conf/gremlin-server-neo4j.yaml
+
+# update README
+sed 's/\(http:\/\/tinkerpop.com\/.*docs\/\)[A-Za-z0-9.-]*\/\(.*\)/\1'"${VERSION}"'\/\2/' README.asciidoc > README1.asciidoc && mv README1.asciidoc README.asciidoc
