2f16f242c7d55d1916a587d3fd7e26597762ea68
Marko A. Rodriguez <okrammarko@gmail.com>
2014-11-05 13:37:26 -0700

    fixed a bug in DetachEdge around self-loop edges. Fixed a bug in KryoInputFormat where we were not going to the split boundaries correctly -- @dalaro. Added more notes to giraph-graphson.json (around using giraph properties). Synchronized the getNextId() method in TinkerHelper in case of parallel writes.

--- giraph-gremlin/src/main/java/com/tinkerpop/gremlin/giraph/structure/io/kryo/KryoInputFormat.java
+++ giraph-gremlin/src/main/java/com/tinkerpop/gremlin/giraph/structure/io/kryo/KryoInputFormat.java
@@ -3,6 +3,9 @@ package com.tinkerpop.gremlin.giraph.structure.io.kryo;
 import com.tinkerpop.gremlin.giraph.process.computer.GiraphComputeVertex;
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.compress.CompressionCodecFactory;
@@ -11,13 +14,20 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Joshua Shinavier (http://fortytwo.net)
  */
 public class KryoInputFormat extends FileInputFormat<NullWritable, GiraphComputeVertex> implements Configurable {
+
+    Logger LOGGER = LoggerFactory.getLogger(KryoInputFormat.class);
 
     private Configuration config;
 
@@ -42,4 +52,52 @@ public class KryoInputFormat extends FileInputFormat<NullWritable, GiraphCompute
     public Configuration getConf() {
         return this.config;
     }
+
+
+    //private static final double SPLIT_SLOP = 1.1;
+    //static final java.lang.String NUM_INPUT_FILES = "mapreduce.input.num.files";
+
+
+    /**
+     * Generate the list of files and make them into FileSplits.
+    public List<InputSplit> getSplits(final JobContext job) throws IOException {
+        long minSize = Math.max(getFormatMinSplitSize(), getMinSplitSize(job));
+        long maxSize = getMaxSplitSize(job);
+
+        // generate splits
+        List<InputSplit> splits = new ArrayList<>();
+        List<FileStatus> files = listStatus(job);
+        for (FileStatus file : files) {
+            Path path = file.getPath();
+            FileSystem fs = path.getFileSystem(job.getConfiguration());
+            long length = file.getLen();
+            BlockLocation[] blkLocations = fs.getFileBlockLocations(file, 0, length);
+            if ((length != 0) && isSplitable(job, path)) {
+                long blockSize = file.getBlockSize();
+                long splitSize = computeSplitSize(blockSize, minSize, maxSize);
+
+                long bytesRemaining = length;
+                while (((double) bytesRemaining) / splitSize > SPLIT_SLOP) {
+                    int blkIndex = getBlockIndex(blkLocations, length - bytesRemaining);
+                    splits.add(new FileSplit(path, length - bytesRemaining, splitSize, blkLocations[blkIndex].getHosts()));
+                    bytesRemaining -= splitSize;
+                }
+
+                if (bytesRemaining != 0) {
+                    splits.add(new FileSplit(path, length - bytesRemaining, bytesRemaining, blkLocations[blkLocations.length - 1].getHosts()));
+                }
+            } else if (length != 0) {
+                splits.add(new FileSplit(path, 0, length, blkLocations[0].getHosts()));
+            } else {
+                //Create empty hosts array for zero length files
+                splits.add(new FileSplit(path, 0, length, new String[0]));
+            }
+        }
+
+        // Save the number of input files in the job-conf
+        job.getConfiguration().setLong(NUM_INPUT_FILES, files.size());
+
+        LOGGER.debug("Total # of splits: " + splits.size());
+        return splits;
+    } */
 }
