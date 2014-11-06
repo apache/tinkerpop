235bf3b75166891216af79be07f8e801db9cc126
Marko A. Rodriguez <okrammarko@gmail.com>
2014-11-06 09:11:05 -0700

    made things final static for speed.

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/process/traversers/util/DefaultTraverserGeneratorFactory.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/process/traversers/util/DefaultTraverserGeneratorFactory.java
@@ -1,9 +1,9 @@
 package com.tinkerpop.gremlin.process.traversers.util;
 
 import com.tinkerpop.gremlin.process.Traversal;
+import com.tinkerpop.gremlin.process.TraverserGenerator;
 import com.tinkerpop.gremlin.process.traversers.PathTraverserGenerator;
 import com.tinkerpop.gremlin.process.traversers.SimpleTraverserGenerator;
-import com.tinkerpop.gremlin.process.TraverserGenerator;
 import com.tinkerpop.gremlin.process.traversers.TraverserGeneratorFactory;
 import com.tinkerpop.gremlin.process.util.TraversalHelper;
 
@@ -13,6 +13,8 @@ import com.tinkerpop.gremlin.process.util.TraversalHelper;
 public class DefaultTraverserGeneratorFactory implements TraverserGeneratorFactory {
 
     private static DefaultTraverserGeneratorFactory INSTANCE = new DefaultTraverserGeneratorFactory();
+    private static final PathTraverserGenerator PATH_TRAVERSER_GENERATOR = new PathTraverserGenerator();
+    private static final SimpleTraverserGenerator SIMPLE_TRAVERSER_GENERATOR = new SimpleTraverserGenerator();
 
     public static DefaultTraverserGeneratorFactory instance() {
         return INSTANCE;
@@ -23,7 +25,7 @@ public class DefaultTraverserGeneratorFactory implements TraverserGeneratorFacto
 
     public TraverserGenerator getTraverserGenerator(final Traversal traversal) {
         return TraversalHelper.trackPaths(traversal) ?
-                new PathTraverserGenerator() :
-                new SimpleTraverserGenerator();
+                PATH_TRAVERSER_GENERATOR :
+                SIMPLE_TRAVERSER_GENERATOR;
     }
 }
