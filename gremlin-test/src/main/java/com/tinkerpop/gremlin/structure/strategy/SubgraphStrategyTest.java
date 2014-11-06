ba3ae63e01f8e10f0ea4bcc0713170579e202813
Marko A. Rodriguez <okrammarko@gmail.com>
2014-11-06 16:34:05 -0700

    oops -- forgot to add back a few subgraph strategy tests.

--- gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/SubgraphStrategyTest.java
+++ gremlin-test/src/main/java/com/tinkerpop/gremlin/structure/strategy/SubgraphStrategyTest.java
@@ -18,6 +18,90 @@ import static org.junit.Assert.*;
  */
 public class SubgraphStrategyTest extends AbstractGremlinTest {
 
+    @Test
+    @LoadGraphWith(MODERN)
+    public void testVertexCriterion() throws Exception {
+        final Predicate<Vertex> vertexCriterion = vertex -> vertex.value("name").equals("josh") || vertex.value("name").equals("lop") || vertex.value("name").equals("ripple");
+        final Predicate<Edge> edgeCriterion = edge -> true;
+
+        final GraphStrategy strategyToTest = new SubgraphStrategy(vertexCriterion, edgeCriterion);
+        final StrategyWrappedGraph sg = new StrategyWrappedGraph(g);
+        sg.getStrategy().setGraphStrategy(strategyToTest);
+
+        // three vertices are included in the subgraph
+        assertEquals(6, g.V().count().next().longValue());
+        assertEquals(3, sg.V().count().next().longValue());
+
+        // only two edges are present, even though edges are not explicitly excluded
+        // (edges require their incident vertices)
+        assertEquals(6, g.E().count().next().longValue());
+        assertEquals(2, sg.E().count().next().longValue());
+
+        // from vertex
+
+        assertEquals(2, g.v(convertToVertexId("josh")).outE().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).outE().count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).out().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).out().count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).inE().count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).inE().count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).in().count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).in().count().next().longValue());
+        assertEquals(3, g.v(convertToVertexId("josh")).bothE().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).bothE().count().next().longValue());
+        assertEquals(3, g.v(convertToVertexId("josh")).both().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).both().count().next().longValue());
+
+        // with label
+
+        assertEquals(2, g.v(convertToVertexId("josh")).outE("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).outE("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).out("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).out("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).bothE("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).bothE("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).both("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).both("created").count().next().longValue());
+
+        assertEquals(1, g.v(convertToVertexId("josh")).inE("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).inE("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).in("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).in("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).bothE("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).both("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).both("knows").count().next().longValue());
+
+        // with label and branch factor
+
+        assertEquals(1, g.v(convertToVertexId("josh")).outE("created").localLimit(1).count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).outE("created").localLimit(1).count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).outE("created").localLimit(1).count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).outE("created").localLimit(1).count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE("created").localLimit(1).count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).bothE("created").localLimit(1).count().next().longValue());
+
+        // from edge
+
+        assertEquals(2, g.e(convertToEdgeId("josh", "created", "lop")).bothV().count().next().longValue());
+        assertEquals(2, sg.e(convertToEdgeId("josh", "created", "lop")).bothV().count().next().longValue());
+
+        assertEquals(2, g.e(convertToEdgeId("peter", "created", "lop")).bothV().count().next().longValue());
+        try {
+            sg.e(convertToEdgeId("peter", "created", "lop"));
+            fail("Edge 12 should not be in the graph because peter is not a vertex");
+        } catch (Exception ex) {
+            assertTrue(ex instanceof NoSuchElementException);
+        }
+
+        assertEquals(2, g.e(convertToEdgeId("marko", "knows", "vadas")).bothV().count().next().longValue());
+        try {
+            sg.e(convertToEdgeId("marko", "knows", "vadas"));
+            fail("Edge 7 should not be in the graph because marko is not a vertex");
+        } catch (Exception ex) {
+            assertTrue(ex instanceof NoSuchElementException);
+        }
+    }
 
     @Test
     @LoadGraphWith(MODERN)
@@ -114,5 +198,126 @@ public class SubgraphStrategyTest extends AbstractGremlinTest {
         assertEquals(2, sg.e(convertToEdgeId("marko", "knows", "josh")).outV().outE().count().next().longValue());
     }
 
+    @Test
+    @LoadGraphWith(MODERN)
+    public void shouldFilterMixedCriteria() throws Exception {
+        final Predicate<Vertex> vertexCriterion = vertex -> vertex.value("name").equals("josh") || vertex.value("name").equals("lop") || vertex.value("name").equals("ripple");
+        final Predicate<Edge> edgeCriterion = edge -> {
+            // 9 isn't present because marko is not in the vertex list
+            // 11
+            if (edge.<Double>value("weight") == 0.4d && edge.label().equals("created"))
+                return true;
+                // 10
+            else if (edge.<Double>value("weight") == 1.0d && edge.label().equals("created"))
+                return true;
+            else return false;
+        };
 
+        final GraphStrategy strategyToTest = new SubgraphStrategy(vertexCriterion, edgeCriterion);
+        final StrategyWrappedGraph sg = new StrategyWrappedGraph(g);
+        sg.getStrategy().setGraphStrategy(strategyToTest);
+
+        // three vertices are included in the subgraph
+        assertEquals(6, g.V().count().next().longValue());
+        assertEquals(3, sg.V().count().next().longValue());
+
+        // three edges are explicitly included, but one is missing its out-vertex due to the vertex criterion
+        assertEquals(6, g.E().count().next().longValue());
+        assertEquals(2, sg.E().count().next().longValue());
+
+        // from vertex
+
+        assertEquals(2, g.v(convertToVertexId("josh")).outE().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).outE().count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).out().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).out().count().next().longValue());
+
+        assertEquals(1, g.v(convertToVertexId("josh")).inE().count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).inE().count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).in().count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).in().count().next().longValue());
+
+        assertEquals(3, g.v(convertToVertexId("josh")).bothE().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).bothE().count().next().longValue());
+        assertEquals(3, g.v(convertToVertexId("josh")).both().count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).both().count().next().longValue());
+
+        // with label
+
+        assertEquals(2, g.v(convertToVertexId("josh")).outE("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).outE("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).out("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).out("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).bothE("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).bothE("created").count().next().longValue());
+        assertEquals(2, g.v(convertToVertexId("josh")).both("created").count().next().longValue());
+        assertEquals(2, sg.v(convertToVertexId("josh")).both("created").count().next().longValue());
+
+        assertEquals(1, g.v(convertToVertexId("josh")).inE("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).inE("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).in("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).in("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).bothE("knows").count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).both("knows").count().next().longValue());
+        assertEquals(0, sg.v(convertToVertexId("josh")).both("knows").count().next().longValue());
+
+        // with branch factor
+
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE().localLimit(1).count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).bothE().localLimit(1).count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE().localLimit(1).inV().count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).bothE().localLimit(1).inV().count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE("knows", "created").localLimit(1).count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).bothE("knows", "created").localLimit(1).count().next().longValue());
+        assertEquals(1, g.v(convertToVertexId("josh")).bothE("knows", "created").localLimit(1).inV().count().next().longValue());
+        assertEquals(1, sg.v(convertToVertexId("josh")).bothE("knows", "created").localLimit(1).inV().count().next().longValue());
+
+        // from edge
+
+        assertEquals(2, g.e(convertToEdgeId("marko", "created", "lop")).bothV().count().next().longValue());
+        try {
+            sg.e(convertToEdgeId("marko", "created", "lop"));
+            fail("Edge 9 should not be in the graph because marko is not a vertex");
+        } catch (Exception ex) {
+            assertTrue(ex instanceof NoSuchElementException);
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    @LoadGraphWith(MODERN)
+    public void shouldGetExcludedVertex() throws Exception {
+        final Predicate<Vertex> vertexCriterion = vertex -> vertex.value("name").equals("josh") || vertex.value("name").equals("lop") || vertex.value("name").equals("ripple");
+        final Predicate<Edge> edgeCriterion = edge -> true;
+
+        final GraphStrategy strategyToTest = new SubgraphStrategy(vertexCriterion, edgeCriterion);
+        final StrategyWrappedGraph sg = new StrategyWrappedGraph(g);
+        sg.getStrategy().setGraphStrategy(strategyToTest);
+
+        sg.v(convertToVertexId("marko"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    @LoadGraphWith(MODERN)
+    public void shouldGetExcludedEdge() throws Exception {
+        final Predicate<Vertex> vertexCriterion = vertex -> true;
+        final Predicate<Edge> edgeCriterion = edge -> {
+            // 8
+            if (edge.<Double>value("weight") == 1.0d && edge.label().equals("knows"))
+                return true;
+                // 9
+            else if (edge.<Double>value("weight") == 0.4d && edge.label().equals("created") && edge.outV().next().value("name").equals("marko"))
+                return true;
+                // 10
+            else if (edge.<Double>value("weight") == 1.0d && edge.label().equals("created"))
+                return true;
+            else return false;
+        };
+
+        final GraphStrategy strategyToTest = new SubgraphStrategy(vertexCriterion, edgeCriterion);
+        final StrategyWrappedGraph sg = new StrategyWrappedGraph(g);
+        sg.getStrategy().setGraphStrategy(strategyToTest);
+
+        sg.e(sg.e(convertToEdgeId("marko", "knows", "vadas")));
+    }
 }
