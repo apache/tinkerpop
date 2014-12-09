413bd3715953333ba5ca1c244edd2ecc753ddd92
Marko A. Rodriguez <okrammarko@gmail.com>
2014-12-09 15:41:26 -0700

    testing ComputerDataStrategy valueIterator().

--- gremlin-test/src/main/java/com/tinkerpop/gremlin/process/computer/util/ComputerDataStrategyTest.java
+++ gremlin-test/src/main/java/com/tinkerpop/gremlin/process/computer/util/ComputerDataStrategyTest.java
@@ -28,11 +28,15 @@ public class ComputerDataStrategyTest extends AbstractGremlinTest {
         sg.getStrategy().setGraphStrategy(new ComputerDataStrategy(new HashSet<>(Arrays.asList("***hidden-guy"))));
 
         final Vertex v = sg.addVertex("***hidden-guy", "X", "not-hidden-guy", "Y");
-        final Iterator<VertexProperty<Object>> props = v.iterators().propertyIterator();
+        final Iterator<VertexProperty<String>> props = v.iterators().propertyIterator();
         final VertexProperty v1 = props.next();
         assertEquals("Y", v1.value());
         assertEquals("not-hidden-guy", v1.key());
         assertFalse(props.hasNext());
+
+        final Iterator<String> values = v.iterators().valueIterator();
+        assertEquals("Y",values.next());
+        assertFalse(values.hasNext());
     }
 
     @Test
@@ -41,11 +45,15 @@ public class ComputerDataStrategyTest extends AbstractGremlinTest {
         sg.getStrategy().setGraphStrategy(new ComputerDataStrategy(new HashSet<>(Arrays.asList("***hidden-guy"))));
 
         final Vertex v = sg.addVertex("***hidden-guy", "X", "not-hidden-guy", "Y");
-        final Iterator<VertexProperty<Object>> props = v.iterators().propertyIterator("***hidden-guy");
-        final VertexProperty v1 = props.next();
+        final Iterator<VertexProperty<String>> props = v.iterators().propertyIterator("***hidden-guy");
+        final VertexProperty<String> v1 = props.next();
         assertEquals("X", v1.value());
         assertEquals("***hidden-guy", v1.key());
         assertFalse(props.hasNext());
+
+        final Iterator<String> values = v.iterators().valueIterator("***hidden-guy");
+        assertEquals("X",values.next());
+        assertFalse(values.hasNext());
     }
 
     @Test
