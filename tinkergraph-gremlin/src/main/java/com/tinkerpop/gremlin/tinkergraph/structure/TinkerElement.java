e4f97711001701482e76eb595649fdca46dc60f7
Marko A. Rodriguez <okrammarko@gmail.com>
2014-12-18 18:57:10 -0700

    minor nothing.

--- tinkergraph-gremlin/src/main/java/com/tinkerpop/gremlin/tinkergraph/structure/TinkerElement.java
+++ tinkergraph-gremlin/src/main/java/com/tinkerpop/gremlin/tinkergraph/structure/TinkerElement.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
