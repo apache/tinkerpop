685431a635d713fee7a9a4a2a4767a1a5e8f960e
stephen mallette <spmva@genoprime.com>
2015-01-01 12:04:36 -0500

    Change the method by which temp files are generated so as to allow building on windows.

--- neo4j-gremlin/src/test/java/com/tinkerpop/gremlin/neo4j/DefaultNeo4jGraphProvider.java
+++ neo4j-gremlin/src/test/java/com/tinkerpop/gremlin/neo4j/DefaultNeo4jGraphProvider.java
@@ -13,9 +13,10 @@ import java.util.Map;
 public class DefaultNeo4jGraphProvider extends AbstractNeo4jGraphProvider {
     @Override
     public Map<String, Object> getBaseConfiguration(final String graphName, final Class<?> test, final String testMethodName) {
+        // todo: convert to a regex replace when building the working directory
         return new HashMap<String, Object>() {{
             put(Graph.GRAPH, Neo4jGraph.class.getName());
-            put(Neo4jGraph.CONFIG_DIRECTORY, getWorkingDirectory() + File.separator + graphName);
+            put(Neo4jGraph.CONFIG_DIRECTORY, getWorkingDirectory() + File.separator + graphName + File.separator + testMethodName.replace(">","").replace("[","").replace("]","").replace("(","").replace(")","").replace(":","").replace(".",""));
             put(Neo4jGraph.CONFIG_META_PROPERTIES, true);
             put(Neo4jGraph.CONFIG_MULTI_PROPERTIES, true);
         }};
