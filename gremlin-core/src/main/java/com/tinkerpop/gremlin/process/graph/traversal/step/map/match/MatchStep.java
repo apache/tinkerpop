a0a13354da6bcdadedb7f417a9ba29a80c13a22c
Joshua Shinavier <josh@fortytwo.net>
2015-02-06 12:00:56 -0500

    Tiny tweak, just to test Git commit rights

--- gremlin-core/src/main/java/com/tinkerpop/gremlin/process/graph/traversal/step/map/match/MatchStep.java
+++ gremlin-core/src/main/java/com/tinkerpop/gremlin/process/graph/traversal/step/map/match/MatchStep.java
@@ -309,7 +309,9 @@ public final class MatchStep<S, E> extends AbstractStep<S, Map<String, E>> imple
         }
     }
 
-    // applies a visitor, skipping anonymous variables
+    /**
+     * Applies a visitor, skipping anonymous variables
+     */
     static <T> void visit(final String name,
                           final T value,
                           final BiConsumer<String, T> visitor) {
