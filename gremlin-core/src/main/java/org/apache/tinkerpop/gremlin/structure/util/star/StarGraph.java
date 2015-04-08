19ca8b6951536a79f38bbe8cab80e1dd154d5de1
Marko A. Rodriguez <okrammarko@gmail.com>
2015-04-08 13:40:41 -0600

    If an ID does not exist use an incrementing Long.

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/structure/util/star/StarGraph.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/structure/util/star/StarGraph.java
@@ -46,7 +46,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Random;
 import java.util.stream.Stream;
 
 /**
