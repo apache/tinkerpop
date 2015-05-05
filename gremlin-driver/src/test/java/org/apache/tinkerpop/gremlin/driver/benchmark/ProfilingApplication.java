f176870193300ffcbd9cd4f52c3a34631648b9ce
Stephen Mallette <spmva@genoprime.com>
2015-05-05 15:06:37 -0400

    Make profiler for driver a bit more robust.
    Include a warmup session. Restructure for better re-use and setup for taking application arguments.

--- gremlin-driver/src/test/java/org/apache/tinkerpop/gremlin/driver/benchmark/ProfilingApplication.java
+++ gremlin-driver/src/test/java/org/apache/tinkerpop/gremlin/driver/benchmark/ProfilingApplication.java
@@ -21,7 +21,10 @@ package org.apache.tinkerpop.gremlin.driver.benchmark;
 import org.apache.tinkerpop.gremlin.driver.Client;
 import org.apache.tinkerpop.gremlin.driver.Cluster;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
@@ -34,12 +37,96 @@ import java.util.stream.IntStream;
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public class ProfilingApplication {
+
+    private final Cluster cluster;
+    private final int requests;
+    private final int clients;
+
+    public ProfilingApplication(final Cluster cluster, final int clients, final int requests) {
+        this.cluster = cluster;
+        this.clients = clients;
+        this.requests = requests;
+    }
+
+    public long execute() throws Exception {
+        final Map<Thread, Long> rps = new ConcurrentHashMap<>();
+        final AtomicInteger tooSlow = new AtomicInteger(0);
+
+        // let all the clients fully init before starting to send messages
+        final CyclicBarrier barrier = new CyclicBarrier(clients);
+
+        final List<Thread> threads = IntStream.range(0, clients).mapToObj(t -> new Thread(() -> {
+            final Client client = cluster.connect();
+            try {
+                final CountDownLatch latch = new CountDownLatch(requests);
+                client.init();
+                barrier.await();
+
+                // timer starts after init of all clients
+                final long start = System.nanoTime();
+                IntStream.range(0, requests).forEach(i -> {
+                    client.submitAsync("1+1").thenAcceptAsync(r -> {
+                        try {
+                            r.all().get(125, TimeUnit.MILLISECONDS);
+                        } catch (TimeoutException ex) {
+                            tooSlow.incrementAndGet();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        } finally {
+                            latch.countDown();
+                        }
+                    });
+                });
+
+                latch.await();
+
+                final long end = System.nanoTime();
+                final long total = end - start;
+
+                System.out.println("All responses for [" + t + "] are accounted for at: " + end);
+
+                final long totalSeconds = Math.round(total / 1000000000d);
+                final long requestCount = requests;
+                final long reqSec = Math.round(requestCount / totalSeconds);
+                rps.put(Thread.currentThread(), reqSec);
+                System.out.println(String.format("[" + t + "] clients: %s | requests: %s | time(s): %s | req/sec: %s | too slow: %s", clients, requestCount, totalSeconds, reqSec, tooSlow.get()));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            } finally {
+                if (client != null) client.close();
+            }
+        }, "benchmark-client-" + t)).collect(Collectors.toList());
+
+        threads.forEach(t -> {
+            try {
+                t.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
+        });
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
+        });
+
+        return rps.values().stream().collect(Collectors.averagingLong(l -> l)).longValue();
+    }
+
     public static void main(final String[] args) {
         try {
             System.out.println("Initializing at: " + System.nanoTime());
 
             final String host = args.length == 0 ? "localhost" : args[0];
 
+            final int warmups = 1;
+            final int executions = 1;
             final int clients = 1;
             final int requests = 10000;
             final Cluster cluster = Cluster.build(host)
@@ -48,72 +135,16 @@ public class ProfilingApplication {
                     .nioPoolSize(clients)
                     .workerPoolSize(clients * 2).create();
 
-            final AtomicInteger tooSlow = new AtomicInteger(0);
+            for (int ix = 0; ix < warmups; ix ++) {
+                new ProfilingApplication(cluster, clients, requests).execute();
+            }
 
-            // let all the clients fully init before starting to send messages
-            final CyclicBarrier barrier = new CyclicBarrier(clients);
+            long totalRequestsPerSecond = 0;
+            for (int ix = 0; ix < executions; ix ++) {
+                totalRequestsPerSecond += new ProfilingApplication(cluster, clients, requests).execute();
+            }
 
-            final List<Thread> threads = IntStream.range(0, clients).mapToObj(t -> new Thread(() -> {
-                try {
-                    final CountDownLatch latch = new CountDownLatch(requests);
-
-                    final Client client = cluster.connect();
-                    client.init();
-
-                    barrier.await();
-                    final long start = System.nanoTime();
-
-                    System.out.println("Executing at [" + t + "]:" + start);
-
-                    IntStream.range(0, requests).forEach(i -> {
-                        client.submitAsync("1+1").thenAcceptAsync(r -> {
-                            try {
-                                r.all().get(100, TimeUnit.MILLISECONDS);
-                            } catch (TimeoutException ex) {
-                                tooSlow.incrementAndGet();
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                            } finally {
-                                latch.countDown();
-                            }
-                        });
-                    });
-
-                    latch.await();
-
-                    final long end = System.nanoTime();
-                    final long total = end - start;
-
-                    System.out.println("All responses for [" + t + "] are accounted for at: " + end);
-
-                    final long totalSeconds = Math.round(total / 1000000000d);
-                    final long requestCount = requests;
-                    final long reqSec = Math.round(requestCount / totalSeconds);
-                    System.out.println(String.format("[" + t + "] clients: %s | requests: %s | time(s): %s | req/sec: %s | too slow: %s", clients, requestCount, totalSeconds, reqSec, tooSlow.get()));
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    throw new RuntimeException(ex);
-                }
-            })).collect(Collectors.toList());
-
-            threads.forEach(t -> {
-                try {
-                    t.start();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    throw new RuntimeException(ex);
-                }
-            });
-
-            threads.forEach(t -> {
-                try {
-                    t.join();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                    throw new RuntimeException(ex);
-                }
-            });
-
+            System.out.println(String.format("avg req/sec: %s", totalRequestsPerSecond / executions));
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
