b488cf99c194ae8ec2df7f29c8e8063a2005a3a1
mhfrantz <mfrantz@redsealnetworks.com>
2015-05-13 18:17:10 -0700

    TINKERPOP3-670: Add tail step to __

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/dsl/graph/__.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/dsl/graph/__.java
@@ -415,6 +415,22 @@ public class __ {
         return __.<A>start().limit(scope, limit);
     }
 
+    public static <A> GraphTraversal<A, A> tail() {
+        return __.<A>start().tail();
+    }
+
+    public static <A> GraphTraversal<A, A> tail(final long limit) {
+        return __.<A>start().tail(limit);
+    }
+
+    public static <A> GraphTraversal<A, A> tail(final Scope scope) {
+        return __.<A>start().tail(scope);
+    }
+
+    public static <A> GraphTraversal<A, A> tail(final Scope scope, final long limit) {
+        return __.<A>start().tail(scope, limit);
+    }
+
     public static <A> GraphTraversal<A, A> retain(final String sideEffectKeyOrPathLabel) {
         return __.<A>start().retain(sideEffectKeyOrPathLabel);
     }
