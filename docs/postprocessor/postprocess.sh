9de37bd76eef8ac98be6764cf6220be62ac4f6de
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-05-19 13:04:27 +0200

    added AsciiDocs postprocessor which removes Apache license header from code samples in the docs

---
+++ docs/postprocessor/postprocess.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+pushd "$(dirname $0)/../.." > /dev/null
+
+if [ -f "target/docs/htmlsingle/index.html" ]; then
+  awk -f "docs/postprocessor/processor.awk" target/docs/htmlsingle/index.html \
+    | perl -0777 -pe 's/<span class="comment">\/\*\n \*\/<\/span>//igs'       \
+    > /tmp/index.html && mv /tmp/index.html target/docs/htmlsingle/
+fi
+
+popd > /dev/null
