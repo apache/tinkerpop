0bb1136723683a5a46549518032231df8711ec45
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-07-31 23:13:13 +0200

    implemented LoopsStep

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/step/map/LoopsStep.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/step/map/LoopsStep.java
@@ -16,39 +16,22 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.tinkerpop.gremlin.process.traversal.lambda;
+package org.apache.tinkerpop.gremlin.process.traversal.step.map;
 
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
 
 /**
- * @author Marko A. Rodriguez (http://markorodriguez.com)
+ * @author Daniel Kuppitz (http://gremlin.guru)
  */
-public final class LoopTraversal<S, E> extends AbstractLambdaTraversal<S, E> {
+public final class LoopsStep<S> extends MapStep<S, Integer> {
 
-    private final long maxLoops;
-    private boolean allow = false;
-
-    public LoopTraversal(final long maxLoops) {
-        this.maxLoops = maxLoops;
+    public LoopsStep(final Traversal.Admin traversal) {
+        super(traversal);
     }
 
     @Override
-    public boolean hasNext() {
-        return this.allow;
-    }
-
-    @Override
-    public void addStart(final Traverser<S> start) {
-        this.allow = start.loops() >= this.maxLoops;
-    }
-
-    @Override
-    public String toString() {
-        return "loops(" + this.maxLoops + ')';
-    }
-
-    @Override
-    public int hashCode() {
-        return this.getClass().hashCode() ^ Long.hashCode(this.maxLoops);
+    protected Integer map(final Traverser.Admin<S> traverser) {
+        return traverser.loops();
     }
 }
