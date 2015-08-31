e5d519234a2cab1ed9e44591cfb391e435fee7a6
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-08-31 02:31:18 +0200

    added a few unit tests for BulkLoaderVertexProgram and IncrementalBulkLoader

---
+++ gremlin-test/src/main/java/org/apache/tinkerpop/gremlin/process/computer/bulkloading/BulkLoaderVertexProgramTest.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tinkerpop.gremlin.process.computer.bulkloading;
+
+import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.apache.tinkerpop.gremlin.LoadGraphWith;
+import org.apache.tinkerpop.gremlin.process.AbstractGremlinProcessTest;
+import org.apache.tinkerpop.gremlin.process.computer.VertexProgram;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.apache.tinkerpop.gremlin.LoadGraphWith.GraphData.MODERN;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Daniel Kuppitz (http://gremlin.guru)
+ */
+public class BulkLoaderVertexProgramTest extends AbstractGremlinProcessTest {
+
+    private BulkLoader getBulkLoader(final Configuration configuration) throws Exception {
+        final VertexProgram.Builder builder = BulkLoaderVertexProgram.build();
+        if (configuration != null)
+            configuration.getKeys().forEachRemaining(key -> builder.configure(key, configuration.getProperty(key)));
+        BulkLoaderVertexProgram blvp = builder.create(graph);
+        final Field field = BulkLoaderVertexProgram.class.getDeclaredField("bulkLoader");
+        field.setAccessible(true);
+        return (BulkLoader) field.get(blvp);
+    }
+
+    private Graph getTargetGraph() {
+        final Configuration configuration = new BaseConfiguration();
+        configuration.setProperty(Graph.GRAPH, "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph");
+        return GraphFactory.open(configuration);
+    }
+
+    @Test
+    public void shouldUseIncrementalBulkLoaderByDefault() throws Exception {
+        final BulkLoader loader = getBulkLoader(null);
+        assertTrue(loader instanceof IncrementalBulkLoader);
+        assertTrue(loader.keepOriginalIds());
+        assertFalse(loader.useUserSuppliedIds());
+    }
+
+    @Test
+    @LoadGraphWith(MODERN)
+    public void shouldStoreOriginalIds() throws Exception {
+        final Configuration configuration = new BaseConfiguration();
+        configuration.setProperty("loader.userSuppliedIds", false);
+        final BulkLoader loader = getBulkLoader(configuration);
+        assertFalse(loader.useUserSuppliedIds());
+        final Graph target = getTargetGraph();
+        graph.vertices().forEachRemaining(v -> loader.getOrCreateVertex(v, target, target.traversal()));
+        target.vertices().forEachRemaining(v -> assertTrue(v.property(loader.getVertexIdProperty()).isPresent()));
+    }
+
+    @Test
+    @LoadGraphWith(MODERN)
+    public void shouldNotStoreOriginalIds() throws Exception {
+        final Configuration configuration = new BaseConfiguration();
+        configuration.setProperty("loader.userSuppliedIds", true);
+        final BulkLoader loader = getBulkLoader(configuration);
+        assertTrue(loader.useUserSuppliedIds());
+        final Graph target = getTargetGraph();
+        graph.vertices().forEachRemaining(v -> loader.getOrCreateVertex(v, target, target.traversal()));
+        target.vertices().forEachRemaining(v -> assertFalse(v.property(loader.getVertexIdProperty()).isPresent()));
+    }
+
+    // TODO: once Neo4j supports concurrent connections, write a real integration test that leverages BLVP
+}
