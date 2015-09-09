1039ae8a640fed4a94cfe88bdbb2df3c7f448734
Marko A. Rodriguez <okrammarko@gmail.com>
2015-09-09 16:19:08 -0600

    stubbed some docs on Spark/Giraph stuff. Need to work with @dkuppitz tomorrow on how this will work with the doc processor. Logger tweaks.

--- hadoop-gremlin/src/main/java/org/apache/tinkerpop/gremlin/hadoop/groovy/plugin/HadoopGremlinPlugin.java
+++ hadoop-gremlin/src/main/java/org/apache/tinkerpop/gremlin/hadoop/groovy/plugin/HadoopGremlinPlugin.java
@@ -96,8 +96,10 @@ public final class HadoopGremlinPlugin extends AbstractGremlinPlugin {
             pluginAcceptor.addBinding("local", FileSystem.getLocal(new Configuration()));
             if (null == System.getenv(Constants.HADOOP_GREMLIN_LIBS))
                 HadoopGraph.LOGGER.warn("Be sure to set the environmental variable: " + Constants.HADOOP_GREMLIN_LIBS);
-            else
-                HadoopGraph.LOGGER.info(Constants.HADOOP_GREMLIN_LIBS + " is set to: " + System.getenv(Constants.HADOOP_GREMLIN_LIBS));
+            else {
+                System.setProperty(Constants.HADOOP_GREMLIN_LIBS, System.getenv(Constants.HADOOP_GREMLIN_LIBS));
+                HadoopGraph.LOGGER.info(Constants.HADOOP_GREMLIN_LIBS + " is set to: " + System.getProperty(Constants.HADOOP_GREMLIN_LIBS));
+            }
         } catch (final Exception e) {
             throw new PluginInitializationException(e.getMessage(), e);
         }
