c94ecd0973a39313c7c3a45a220bc410ea0536b7
Marko A. Rodriguez <okrammarko@gmail.com>
2015-12-08 06:44:47 -0700

    removed wildcard import on statics.

--- spark-gremlin/src/test/java/org/apache/tinkerpop/gremlin/spark/structure/SparkTest.java
+++ spark-gremlin/src/test/java/org/apache/tinkerpop/gremlin/spark/structure/SparkTest.java
@@ -37,7 +37,10 @@ import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
 import org.junit.Test;
 import scala.collection.JavaConversions;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
