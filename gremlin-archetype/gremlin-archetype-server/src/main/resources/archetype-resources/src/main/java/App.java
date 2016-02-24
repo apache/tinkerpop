68b16eb90570c4ed4cd36ab9b93e650bddc568ae
Stephen Mallette <spmva@genoprime.com>
2016-02-24 13:54:30 -0500

    Made some adjustments based on pull request feedback.
    Fixed changelog to use the right name of the archetype. Better logging control/output for the server archetype. Documentation improvements. Included log4j as a compile time dependency.

--- gremlin-archetype/gremlin-archetype-server/src/main/resources/archetype-resources/src/main/java/App.java
+++ gremlin-archetype/gremlin-archetype-server/src/main/resources/archetype-resources/src/main/java/App.java
@@ -19,17 +19,24 @@
 package ${package};
 
 import org.apache.tinkerpop.gremlin.driver.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws Exception {
         Service service = Service.getInstance();
         try {
-            service.findCreatorsOfSoftware("lop").iterator().forEachRemaining(r -> System.out.println(r));
+            logger.info("Sending request....");
+
+            // should print marko, josh and peter to the logs
+            service.findCreatorsOfSoftware("lop").iterator().forEachRemaining(r -> logger.info(String.format("  - %s", r)));
         } catch (Exception ex) {
-            ex.printStackTrace();
+            logger.error("Could not execute traversal", ex);
         } finally {
             service.close();
+            logger.info("Service closed and resources released");
         }
     }
 }
