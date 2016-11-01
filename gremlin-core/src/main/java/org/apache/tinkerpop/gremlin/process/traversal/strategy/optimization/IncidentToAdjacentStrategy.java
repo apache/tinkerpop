6a1a0741ea8ee64427a126e8980ecf7db1a3299a
Marko A. Rodriguez <okrammarko@gmail.com>
2016-11-01 11:54:50 -0600

    IncidentToAdjacentStrategy now uses a hidden marker label to remove the need for constant rootTraversal recrussion in search of invalidating steps.

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/strategy/optimization/IncidentToAdjacentStrategy.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/strategy/optimization/IncidentToAdjacentStrategy.java
@@ -22,14 +22,17 @@ import org.apache.tinkerpop.gremlin.process.traversal.Step;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.step.LambdaHolder;
+import org.apache.tinkerpop.gremlin.process.traversal.step.TraversalParent;
 import org.apache.tinkerpop.gremlin.process.traversal.step.map.EdgeOtherVertexStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.map.EdgeVertexStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.map.PathStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.map.TreeStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.map.VertexStep;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.EmptyStep;
 import org.apache.tinkerpop.gremlin.process.traversal.strategy.AbstractTraversalStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalHelper;
 import org.apache.tinkerpop.gremlin.structure.Direction;
+import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.javatuples.Pair;
 
@@ -66,14 +69,25 @@ public final class IncidentToAdjacentStrategy extends AbstractTraversalStrategy<
 
     private static final IncidentToAdjacentStrategy INSTANCE = new IncidentToAdjacentStrategy();
     private static final Set<Class> INVALIDATING_STEP_CLASSES = new HashSet<>(Arrays.asList(PathStep.class, TreeStep.class, LambdaHolder.class));
+    private static final String MARKER = Graph.Hidden.hide("gremlin.incidentToAdjacent");
 
     private IncidentToAdjacentStrategy() {
     }
 
     @Override
     public void apply(final Traversal.Admin<?, ?> traversal) {
-        if (TraversalHelper.hasStepOfAssignableClassRecursively(INVALIDATING_STEP_CLASSES, TraversalHelper.getRootTraversal(traversal)))
+        // using a hidden label marker to denote whether the traversal should not be processed by this strategy
+        if (traversal.getParent() instanceof EmptyStep) {
+            if (TraversalHelper.hasStepOfAssignableClassRecursively(INVALIDATING_STEP_CLASSES, traversal)) {
+                addLabelMarker(traversal);
+                traversal.getStartStep().removeLabel(MARKER);
+                return;
+            }
+        } else if (traversal.getStartStep().getLabels().contains(MARKER)) {
+            traversal.getStartStep().removeLabel(MARKER);
             return;
+        }
+        ////////////////////////////////////////////////////////////////////////////
         final Collection<Pair<VertexStep, Step>> stepsToReplace = new ArrayList<>();
         Step prev = null;
         for (final Step curr : traversal.getSteps()) {
@@ -85,6 +99,16 @@ public final class IncidentToAdjacentStrategy extends AbstractTraversalStrategy<
         if (!stepsToReplace.isEmpty()) {
             for (final Pair<VertexStep, Step> pair : stepsToReplace) {
                 optimizeSteps(traversal, pair.getValue0(), pair.getValue1());
+            }
+        }
+    }
+
+    private final void addLabelMarker(final Traversal.Admin<?, ?> traversal) {
+        traversal.getStartStep().addLabel(MARKER);
+        for (final Step<?, ?> step : traversal.getSteps()) {
+            if (step instanceof TraversalParent) {
+                ((TraversalParent) step).getLocalChildren().forEach(this::addLabelMarker);
+                ((TraversalParent) step).getGlobalChildren().forEach(this::addLabelMarker);
             }
         }
     }
