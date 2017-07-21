726b4050c875799b4b8754ec29533be670ccb492
Stephen Mallette <spmva@genoprime.com>
2017-07-21 08:18:43 -0400

    Removed "performance" tests.
    These don't execute on tp32, but do on master as all performance testing is handled through gremlin-benchmark now. These tests that were removed fail from time to time in certain environments like travis. As these tests were only running in the performance test profile on tp32 they weren't running in any continual way before so their use is minimal. If we want these kinds of tests they should be re-developed under gremlin-benchmark. CTR

--- gremlin-core/src/test/java/org/apache/tinkerpop/gremlin/process/traversal/strategy/optimization/IdentityRemovalStrategyTest.java
+++ gremlin-core/src/test/java/org/apache/tinkerpop/gremlin/process/traversal/strategy/optimization/IdentityRemovalStrategyTest.java
@@ -20,102 +20,54 @@ package org.apache.tinkerpop.gremlin.process.traversal.strategy.optimization;
 
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategies;
-import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategy;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-import org.apache.tinkerpop.gremlin.process.traversal.strategy.TraversalStrategyPerformanceTest;
 import org.apache.tinkerpop.gremlin.process.traversal.util.DefaultTraversalStrategies;
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
-
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-@RunWith(Enclosed.class)
+@RunWith(Parameterized.class)
 public class IdentityRemovalStrategyTest {
 
-    @RunWith(Parameterized.class)
-    public static class ParameterizedTests {
+    @Parameterized.Parameter(value = 0)
+    public Traversal original;
 
-        @Parameterized.Parameter(value = 0)
-        public Traversal original;
-
-        @Parameterized.Parameter(value = 1)
-        public Traversal optimized;
+    @Parameterized.Parameter(value = 1)
+    public Traversal optimized;
 
 
-        private void applyIdentityRemovalStrategy(final Traversal traversal) {
-            final TraversalStrategies strategies = new DefaultTraversalStrategies();
-            strategies.addStrategies(IdentityRemovalStrategy.instance());
-            traversal.asAdmin().setStrategies(strategies);
-            traversal.asAdmin().applyStrategies();
-
-        }
-
-        @Test
-        public void doTest() {
-            applyIdentityRemovalStrategy(original);
-            assertEquals(optimized, original);
-        }
-
-        @Parameterized.Parameters(name = "{0}")
-        public static Iterable<Object[]> generateTestParameters() {
-
-            return Arrays.asList(new Traversal[][]{
-                    {__.identity(), __.identity()},
-                    {__.identity().out(), __.out()},
-                    {__.identity().out().identity(), __.out()},
-                    {__.identity().as("a").out().identity(), __.identity().as("a").out()},
-                    {__.identity().as("a").out().identity().as("b"), __.identity().as("a").out().as("b")},
-                    {__.identity().as("a").out().in().identity().identity().as("b").identity().out(), __.identity().as("a").out().in().as("b").out()},
-                    {__.out().identity().as("a").out().in().identity().identity().as("b").identity().out(), __.out().as("a").out().in().as("b").out()},
-            });
-        }
+    private void applyIdentityRemovalStrategy(final Traversal traversal) {
+        final TraversalStrategies strategies = new DefaultTraversalStrategies();
+        strategies.addStrategies(IdentityRemovalStrategy.instance());
+        traversal.asAdmin().setStrategies(strategies);
+        traversal.asAdmin().applyStrategies();
 
     }
 
-    public static class PerformanceTest extends TraversalStrategyPerformanceTest {
+    @Test
+    public void doTest() {
+        applyIdentityRemovalStrategy(original);
+        assertEquals(optimized, original);
+    }
 
-        @Override
-        protected Class<? extends TraversalStrategy> getStrategyUnderTest() {
-            return IdentityRemovalStrategy.class;
-        }
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable<Object[]> generateTestParameters() {
 
-        @Override
-        protected Iterator<GraphTraversal> getTraversalIterator() {
-
-            return new Iterator<GraphTraversal>() {
-
-                final int minIdentities = 5;
-                final int maxIdentities = 10;
-                final Integer[] starts = IntStream.range(0, 1000).boxed().toArray(Integer[]::new);
-
-                private int numberOfIdentities = minIdentities;
-
-                @Override
-                public boolean hasNext() {
-                    return numberOfIdentities <= maxIdentities;
-                }
-
-                @Override
-                public GraphTraversal next() {
-                    final GraphTraversal traversal = __.inject(starts);
-                    for (int j = 0; j < numberOfIdentities; j++) {
-                        traversal.identity();
-                    }
-                    numberOfIdentities++;
-                    return traversal;
-                }
-            };
-        }
+        return Arrays.asList(new Traversal[][]{
+                {__.identity(), __.identity()},
+                {__.identity().out(), __.out()},
+                {__.identity().out().identity(), __.out()},
+                {__.identity().as("a").out().identity(), __.identity().as("a").out()},
+                {__.identity().as("a").out().identity().as("b"), __.identity().as("a").out().as("b")},
+                {__.identity().as("a").out().in().identity().identity().as("b").identity().out(), __.identity().as("a").out().in().as("b").out()},
+                {__.out().identity().as("a").out().in().identity().identity().as("b").identity().out(), __.out().as("a").out().in().as("b").out()},
+        });
     }
 }
