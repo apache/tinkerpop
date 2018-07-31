63ae13c24a86f59f84a9b30189bb9a73e22fe50a
Dmitri Bourlatchkov <dmitri.bourlatchkov@datastax.com>
2018-07-31 10:25:01 -0400

    TINKERPOP-2005 Expand .* imports in ResponseStatusCodeTest

--- gremlin-driver/src/test/java/org/apache/tinkerpop/gremlin/driver/message/ResponseStatusCodeTest.java
+++ gremlin-driver/src/test/java/org/apache/tinkerpop/gremlin/driver/message/ResponseStatusCodeTest.java
@@ -21,7 +21,8 @@ package org.apache.tinkerpop.gremlin.driver.message;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ResponseStatusCodeTest {
 
