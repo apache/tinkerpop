72be3549a5e4f99115e9d491e0fc051fff77998a
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2018-10-04 07:15:09 -0700

    TINKERPOP-2058 Use `Compare.eq` in `Contains.within` to ensure equal filter behaviors.
    If the object to be filtered is a number, `Contains` predicates will now scan the provided collection, comparing
    each element using `Compare.eq`. For non-numeric values `Contains.within` will still use `collection.contains()`
    in order to make use of search-optimized data types (e.g. `Set`).

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/Contains.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/Contains.java
@@ -18,18 +18,26 @@
  */
 package org.apache.tinkerpop.gremlin.process.traversal;
 
+import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
+
 import java.util.Collection;
 import java.util.function.BiPredicate;
 
 /**
  * {@link Contains} is a {@link BiPredicate} that evaluates whether the first object is contained within (or not
- * within) the second collection object. For example:
+ * within) the second collection object. If the first object is a number, each element in the second collection
+ * will be compared to the first object using {@link Compare}'s {@code eq} predicate. This will ensure, that numbers
+ * are matched by their value only, ignoring the number type. For example:
  * <p/>
  * <pre>
  * gremlin Contains.within [gremlin, blueprints, furnace] == true
  * gremlin Contains.without [gremlin, rexster] == false
  * rexster Contains.without [gremlin, blueprints, furnace] == true
+ * 123 Contains.within [1, 2, 3] == false
+ * 100 Contains.within [1L, 10L, 100L] == true
  * </pre>
+ *
+ *
  *
  * @author Pierre De Wilde
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -44,7 +52,9 @@ public enum Contains implements BiPredicate<Object, Collection> {
     within {
         @Override
         public boolean test(final Object first, final Collection second) {
-            return second.contains(first);
+            return first instanceof Number
+                    ? IteratorUtils.anyMatch(second.iterator(), o -> Compare.eq.test(first, o))
+                    : second.contains(first);
         }
     },
 
@@ -56,7 +66,7 @@ public enum Contains implements BiPredicate<Object, Collection> {
     without {
         @Override
         public boolean test(final Object first, final Collection second) {
-            return !second.contains(first);
+            return !within.test(first, second);
         }
     };
 
