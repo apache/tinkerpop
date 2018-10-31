a8fba325d7433ca71e2209d7cf89a716ccc3958b
Stephen Mallette <spmva@genoprime.com>
2018-10-31 13:31:28 -0400

    TINKERPOP-2084 Display remote stacktrace in gremlin console
    Making the remote stacktrace visible to the user saves a visit to the server to determine what went amiss with failed script when the remote failing message is not enough to debug the problem. Plus, it makes the Gremlin Console in ":remote console" mode behave more like it is in local mode.

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/jsr223/console/RemoteException.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/jsr223/console/RemoteException.java
@@ -18,23 +18,43 @@
  */
 package org.apache.tinkerpop.gremlin.jsr223.console;
 
+import java.util.Optional;
+
 /**
  * A mapper {@code Exception} to be thrown when there are problems with processing a command given to a
- * {@link RemoteAcceptor}.  The message provided to the exception will
- * be displayed to the user in the Console.
+ * {@link RemoteAcceptor}.  The message provided to the exception will be displayed to the user in the Console.
  *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
 public class RemoteException extends Exception {
+    private String remoteStackTrace = null;
+
     public RemoteException(final String message) {
+        this(message, (String) null);
+    }
+
+    public RemoteException(final String message, final String remoteStackTrace) {
         super(message);
+        this.remoteStackTrace = remoteStackTrace;
     }
 
     public RemoteException(final String message, final Throwable cause) {
+        this(message, cause, null);
+    }
+
+    public RemoteException(final String message, final Throwable cause, final String remoteStackTrace) {
         super(message, cause);
+        this.remoteStackTrace = remoteStackTrace;
     }
 
     public RemoteException(final Throwable cause) {
         super(cause);
+    }
+
+    /**
+     * The stacktrace produced by the remote server.
+     */
+    public Optional<String> getRemoteStackTrace() {
+        return Optional.ofNullable(remoteStackTrace);
     }
 }
