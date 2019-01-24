9949e87be98599bec347860ddfc42fb951e51cac
Dominik Franek <dominik.franek@cogniware.com>
2019-01-25 00:27:07 +1300

    better use of standard Buffer of Browser instead of Buffer class from NodeJS (not available in Browsers)

--- gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
+++ gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
@@ -157,7 +157,8 @@ class Connection extends EventEmitter {
     };
     }
 
-    const message = Buffer.from(this._header + JSON.stringify(this._getRequest(requestId, bytecode, op, args, processor)));
+    //const message = Buffer.from(this._header + JSON.stringify(this._getRequest(requestId, bytecode, op, args, processor)));
+    var message = new ArrayBuffer(this._header + JSON.stringify(this._getRequest(requestId, bytecode, op, args, processor)));
     this._ws.send(message);
   }));
   }
