a30ff44128157ac8620f3bdb51eb3ed27e62b600
Stephen Mallette <spmva@genoprime.com>
2019-02-11 14:48:15 -0500

    Bump to kerby 1.0.0 CTR

--- gremlin-server/src/main/java/org/apache/tinkerpop/gremlin/server/auth/JaasKrbUtil.java
+++ gremlin-server/src/main/java/org/apache/tinkerpop/gremlin/server/auth/JaasKrbUtil.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
  *  under the License. 
- *  
+ *
  */
 package org.apache.tinkerpop.gremlin.server.auth;
 
@@ -51,7 +51,8 @@ public final class JaasKrbUtil {
 
     private JaasKrbUtil() { }
 
-    public static Subject loginUsingPassword(String principal, String password) throws LoginException {
+    public static Subject loginUsingPassword(
+            String principal, String password) throws LoginException {
         Set<Principal> principals = new HashSet<Principal>();
         principals.add(new KerberosPrincipal(principal));
 
@@ -66,7 +67,8 @@ public final class JaasKrbUtil {
         return loginContext.getSubject();
     }
 
-    public static Subject loginUsingTicketCache(String principal, File cacheFile) throws LoginException {
+    public static Subject loginUsingTicketCache(
+            String principal, File cacheFile) throws LoginException {
         Set<Principal> principals = new HashSet<Principal>();
         principals.add(new KerberosPrincipal(principal));
 
@@ -80,7 +82,8 @@ public final class JaasKrbUtil {
         return loginContext.getSubject();
     }
 
-    public static Subject loginUsingKeytab(String principal, File keytabFile) throws LoginException {
+    public static Subject loginUsingKeytab(
+            String principal, File keytabFile) throws LoginException {
         Set<Principal> principals = new HashSet<Principal>();
         principals.add(new KerberosPrincipal(principal));
 
