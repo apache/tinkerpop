984f086ffd65d963707cac6ede3acb440f222961
Stephen Mallette <spmva@genoprime.com>
2019-02-28 09:28:54 -0500

    TINKERPOP-2171 Allow sparql() to be extended with Gremlin steps.

--- sparql-gremlin/src/main/java/org/apache/tinkerpop/gremlin/sparql/process/traversal/dsl/sparql/SparqlTraversalSource.java
+++ sparql-gremlin/src/main/java/org/apache/tinkerpop/gremlin/sparql/process/traversal/dsl/sparql/SparqlTraversalSource.java
@@ -19,13 +19,17 @@
 package org.apache.tinkerpop.gremlin.sparql.process.traversal.dsl.sparql;
 
 import org.apache.commons.configuration.Configuration;
+import org.apache.tinkerpop.gremlin.process.computer.Computer;
+import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
 import org.apache.tinkerpop.gremlin.process.remote.RemoteConnection;
 import org.apache.tinkerpop.gremlin.process.remote.traversal.strategy.decoration.RemoteStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.Bytecode;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategies;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategy;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.DefaultGraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.step.sideEffect.InjectStep;
 import org.apache.tinkerpop.gremlin.sparql.process.traversal.strategy.SparqlStrategy;
 import org.apache.tinkerpop.gremlin.structure.Graph;
@@ -33,121 +37,167 @@ import org.apache.tinkerpop.gremlin.structure.Transaction;
 import org.apache.tinkerpop.gremlin.structure.util.StringFactory;
 import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
 /**
  * A {@link TraversalSource} implementation that spawns {@link SparqlTraversal} instances.
  *
  * @author Stephen Mallette (http://stephen.genoprime.com)
  */
-public class SparqlTraversalSource implements TraversalSource {
-    protected transient RemoteConnection connection;
-    protected final Graph graph;
-    protected TraversalStrategies strategies;
-    protected Bytecode bytecode = new Bytecode();
+public class SparqlTraversalSource extends GraphTraversalSource {
 
     public SparqlTraversalSource(final Graph graph, final TraversalStrategies traversalStrategies) {
-        this.graph = graph;
-        this.strategies = traversalStrategies;
+        super(graph, traversalStrategies);
     }
 
     public SparqlTraversalSource(final Graph graph) {
-        this(graph, TraversalStrategies.GlobalCache.getStrategies(graph.getClass()));
+        super(graph);
     }
 
     public SparqlTraversalSource(final RemoteConnection connection) {
-        this(EmptyGraph.instance(), TraversalStrategies.GlobalCache.getStrategies(EmptyGraph.class).clone());
-        this.connection = connection;
-        this.strategies.addStrategies(new RemoteStrategy(connection));
-    }
-
-    @Override
-    public TraversalStrategies getStrategies() {
-        return this.strategies;
-    }
-
-    @Override
-    public Graph getGraph() {
-        return this.graph;
-    }
-
-    @Override
-    public Bytecode getBytecode() {
-        return this.bytecode;
+        super(connection);
     }
 
     @SuppressWarnings("CloneDoesntDeclareCloneNotSupportedException")
     public SparqlTraversalSource clone() {
-        try {
-            final SparqlTraversalSource clone = (SparqlTraversalSource) super.clone();
-            clone.strategies = this.strategies.clone();
-            clone.bytecode = this.bytecode.clone();
-            return clone;
-        } catch (final CloneNotSupportedException e) {
-            throw new IllegalStateException(e.getMessage(), e);
-        }
+        return (SparqlTraversalSource) super.clone();
     }
 
     //// CONFIGURATIONS
 
+
+    @Override
+    public SparqlTraversalSource with(final String key) {
+        return (SparqlTraversalSource) super.with(key);
+    }
+
+    @Override
+    public SparqlTraversalSource with(final String key, final Object value) {
+        return (SparqlTraversalSource) super.with(key, value);
+    }
+
+    @Override
+    public SparqlTraversalSource withComputer(final Computer computer) {
+        return (SparqlTraversalSource) super.withComputer(computer);
+    }
+
+    @Override
+    public SparqlTraversalSource withComputer(final Class<? extends GraphComputer> graphComputerClass) {
+        return (SparqlTraversalSource) super.withComputer(graphComputerClass);
+    }
+
+    @Override
+    public SparqlTraversalSource withComputer() {
+        return (SparqlTraversalSource) super.withComputer();
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSideEffect(final String key, final Supplier<A> initialValue, final BinaryOperator<A> reducer) {
+        return (SparqlTraversalSource) super.withSideEffect(key, initialValue, reducer);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSideEffect(final String key, final A initialValue, final BinaryOperator<A> reducer) {
+        return (SparqlTraversalSource) super.withSideEffect(key, initialValue, reducer);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSideEffect(final String key, final A initialValue) {
+        return (SparqlTraversalSource) super.withSideEffect(key, initialValue);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSideEffect(final String key, final Supplier<A> initialValue) {
+        return (SparqlTraversalSource) super.withSideEffect(key, initialValue);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final Supplier<A> initialValue, final UnaryOperator<A> splitOperator, final BinaryOperator<A> mergeOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, splitOperator, mergeOperator);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final A initialValue, final UnaryOperator<A> splitOperator, final BinaryOperator<A> mergeOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, splitOperator, mergeOperator);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final A initialValue) {
+        return (SparqlTraversalSource) super.withSack(initialValue);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final Supplier<A> initialValue) {
+        return (SparqlTraversalSource) super.withSack(initialValue);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final Supplier<A> initialValue, final UnaryOperator<A> splitOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, splitOperator);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final A initialValue, final UnaryOperator<A> splitOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, splitOperator);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final Supplier<A> initialValue, final BinaryOperator<A> mergeOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, mergeOperator);
+    }
+
+    @Override
+    public <A> SparqlTraversalSource withSack(final A initialValue, final BinaryOperator<A> mergeOperator) {
+        return (SparqlTraversalSource) super.withSack(initialValue, mergeOperator);
+    }
+
+    @Override
+    public SparqlTraversalSource withBulk(final boolean useBulk) {
+        return (SparqlTraversalSource) super.withBulk(useBulk);
+    }
+
+    @Override
+    public SparqlTraversalSource withPath() {
+        return (SparqlTraversalSource) super.withPath();
+    }
+
     @Override
     public SparqlTraversalSource withStrategies(final TraversalStrategy... traversalStrategies) {
-        return (SparqlTraversalSource) TraversalSource.super.withStrategies(traversalStrategies);
+        return (SparqlTraversalSource) super.withStrategies(traversalStrategies);
     }
 
     @Override
     @SuppressWarnings({"unchecked"})
     public SparqlTraversalSource withoutStrategies(final Class<? extends TraversalStrategy>... traversalStrategyClasses) {
-        return (SparqlTraversalSource) TraversalSource.super.withoutStrategies(traversalStrategyClasses);
+        return (SparqlTraversalSource) super.withoutStrategies(traversalStrategyClasses);
     }
 
     @Override
     public SparqlTraversalSource withRemote(final Configuration conf) {
-        return (SparqlTraversalSource) TraversalSource.super.withRemote(conf);
+        return (SparqlTraversalSource) super.withRemote(conf);
     }
 
     @Override
     public SparqlTraversalSource withRemote(final String configFile) throws Exception {
-        return (SparqlTraversalSource) TraversalSource.super.withRemote(configFile);
+        return (SparqlTraversalSource) super.withRemote(configFile);
     }
 
     @Override
     public SparqlTraversalSource withRemote(final RemoteConnection connection) {
-        try {
-            // check if someone called withRemote() more than once, so just release resources on the initial
-            // connection as you can't have more than one. maybe better to toss IllegalStateException??
-            if (this.connection != null)
-                this.connection.close();
-        } catch (Exception ignored) {
-            // not sure there's anything to do here
-        }
-        this.connection = connection;
-        final TraversalSource clone = this.clone();
-        clone.getStrategies().addStrategies(new RemoteStrategy(connection));
-        return (SparqlTraversalSource) clone;
+        return (SparqlTraversalSource) super.withRemote(connection);
     }
 
     /**
      * The start step for a SPARQL based traversal that accepts a string representation of the query to execute.
      */
-    public <S> SparqlTraversal<S,?> sparql(final String query) {
+    public <S> GraphTraversal<S,?> sparql(final String query) {
         final SparqlTraversalSource clone = this.withStrategies(SparqlStrategy.instance()).clone();
 
         // the inject() holds the sparql which the SparqlStrategy then detects and converts to a traversal
         clone.bytecode.addStep(GraphTraversal.Symbols.inject, query);
-        final SparqlTraversal.Admin<S, S> traversal = new DefaultSparqlTraversal<>(clone);
+        final GraphTraversal.Admin<S, S> traversal = new DefaultGraphTraversal(clone);
         return traversal.addStep(new InjectStep<>(traversal, query));
-    }
-
-    public Transaction tx() {
-        return this.graph.tx();
-    }
-
-    @Override
-    public void close() throws Exception {
-        if (connection != null) connection.close();
-    }
-
-    @Override
-    public String toString() {
-        return StringFactory.traversalSourceString(this);
     }
 }
