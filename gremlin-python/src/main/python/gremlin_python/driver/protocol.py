610b10d3b60cff2fd2f5df01d9d3d100503dbffd
HadoopMarc <vtslab@xs4all.nl>
2020-05-10 20:41:30 +0200

    Minor revert code formatting

--- gremlin-python/src/main/python/gremlin_python/driver/protocol.py
+++ gremlin-python/src/main/python/gremlin_python/driver/protocol.py
@@ -88,8 +88,8 @@ class GremlinServerWSProtocol(AbstractBaseProtocol):
     def data_received(self, message, results_dict):
         # if Gremlin Server cuts off then we get a None for the message
         if message is None:
-            raise GremlinServerError({'code': 500, 'message': 'Server disconnected - please try to reconnect',
-                                      'attributes': {}})
+            raise GremlinServerError({'code': 500,
+                                      'message': 'Server disconnected - please try to reconnect', 'attributes': {}})
 
         message = self._message_serializer.deserialize_message(message)
         request_id = message['requestId']
