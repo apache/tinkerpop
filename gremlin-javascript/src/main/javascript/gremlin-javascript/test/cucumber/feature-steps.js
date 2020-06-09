ea0191e46a13847f8472fb173f4b1c13627a2e56
Stephen Mallette <spmva@genoprime.com>
2020-06-09 09:26:41 -0400

    Replaced deprecated cucumber code
    Doing so cleared up build warnings. CTR

--- gremlin-javascript/src/main/javascript/gremlin-javascript/test/cucumber/feature-steps.js
+++ gremlin-javascript/src/main/javascript/gremlin-javascript/test/cucumber/feature-steps.js
@@ -22,7 +22,7 @@
  */
 'use strict';
 
-const defineSupportCode = require('cucumber').defineSupportCode;
+const {Given, Then, When} = require('cucumber');
 const vm = require('vm');
 const expect = require('chai').expect;
 const util = require('util');
@@ -70,112 +70,110 @@ const ignoredScenarios = {
   'g_V_shortestPath_edgesIncluded_edgesXoutEX': new IgnoreError(ignoreReason.needsFurtherInvestigation)
 };
 
-defineSupportCode(function(methods) {
-  methods.Given(/^the (.+) graph$/, function (graphName) {
-    if (ignoredScenarios[this.scenario]) {
+Given(/^the (.+) graph$/, function (graphName) {
+  if (ignoredScenarios[this.scenario]) {
+    return 'skipped';
+  }
+  this.graphName = graphName;
+  const data = this.getData();
+  this.g = traversal().withRemote(data.connection);
+  if (graphName === 'empty') {
+    return this.cleanEmptyGraph();
+  }
+});
+
+Given('the graph initializer of', function (traversalText) {
+  const traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
+  return traversal.toList();
+});
+
+Given('an unsupported test', () => {});
+
+Given('the traversal of', function (traversalText) {
+  this.traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
+});
+
+Given(/^using the parameter (.+) defined as "(.+)"$/, function (paramName, stringValue) {
+  // Remove escaped chars
+  stringValue = stringValue.replace(/\\"/g, '"');
+  let p = Promise.resolve();
+  if (this.graphName === 'empty') {
+    p = this.loadEmptyGraphData();
+  }
+  return p.then(() => {
+    this.parameters[paramName] = parseValue.call(this, stringValue);
+  }).catch(err => {
+    if (err instanceof IgnoreError) {
       return 'skipped';
     }
-    this.graphName = graphName;
-    const data = this.getData();
-    this.g = traversal().withRemote(data.connection);
-    if (graphName === 'empty') {
-      return this.cleanEmptyGraph();
-    }
+    throw err;
   });
-
-  methods.Given('the graph initializer of', function (traversalText) {
-    const traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
-    return traversal.toList();
-  });
-
-  methods.Given('an unsupported test', () => {});
-
-  methods.Given('the traversal of', function (traversalText) {
-    this.traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
-  });
-
-  methods.Given(/^using the parameter (.+) defined as "(.+)"$/, function (paramName, stringValue) {
-    // Remove escaped chars
-    stringValue = stringValue.replace(/\\"/g, '"');
-    let p = Promise.resolve();
-    if (this.graphName === 'empty') {
-      p = this.loadEmptyGraphData();
-    }
-    return p.then(() => {
-      this.parameters[paramName] = parseValue.call(this, stringValue);
-    }).catch(err => {
-      if (err instanceof IgnoreError) {
-        return 'skipped';
-      }
-      throw err;
-    });
-  });
-
-  methods.When('iterated to list', function () {
-    return this.traversal.toList().then(list => this.result = list);
-  });
-
-  methods.When('iterated next', function () {
-    return this.traversal.next().then(it => {
-      this.result = it.value;
-      if (this.result instanceof Path) {
-        // Compare using the objects array
-        this.result = this.result.objects;
-      }
-    });
-  });
-
-  methods.Then(/^the result should be (\w+)$/, function assertResult(characterizedAs, resultTable) {
-    if (characterizedAs === 'empty') {
-      expect(this.result).to.be.empty;
-      if (typeof resultTable === 'function'){
-        return resultTable();
-      }
-      return;
-    }
-    const expectedResult = resultTable.rows().map(row => parseRow.call(this, row));
-    switch (characterizedAs) {
-      case 'ordered':
-        expect(toCompare(this.result)).to.have.deep.ordered.members(expectedResult);
-        break;
-      case 'unordered':
-        expect(toCompare(this.result)).to.have.deep.members(expectedResult);
-        break;
-      case 'of':
-        // result is a subset of the expected
-        expect(expectedResult).to.include.deep.members(toCompare(this.result));
-        break;
-    }
-  });
-
-  methods.Then(/^the graph should return (\d+) for count of "(.+)"$/, function (stringCount, traversalText) {
-    const traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
-    return traversal.toList().then(list => {
-      expect(list).to.have.lengthOf(parseInt(stringCount, 10));
-    });
-  });
-
-  methods.Then(/^the result should have a count of (\d+)$/, function (stringCount) {
-    const expected = parseInt(stringCount, 10);
-    if (!Array.isArray(this.result)) {
-      let count = 0;
-      if (this.result instanceof Map) {
-        count = this.result.size;
-      }
-      else if (typeof this.result === 'object') {
-        count = Object.keys(this.result).length;
-      }
-      else {
-        throw new Error('result not supported: ' + util.inspect(this.result));
-      }
-      expect(count).to.be.equal(expected);
-      return;
-    }
-    expect(this.result).to.have.lengthOf(expected);
-  });
-
-  methods.Then('nothing should happen because', _ => {});
 });
+
+When('iterated to list', function () {
+  return this.traversal.toList().then(list => this.result = list);
+});
+
+When('iterated next', function () {
+  return this.traversal.next().then(it => {
+    this.result = it.value;
+    if (this.result instanceof Path) {
+      // Compare using the objects array
+      this.result = this.result.objects;
+    }
+  });
+});
+
+Then(/^the result should be (\w+)$/, function assertResult(characterizedAs, resultTable) {
+  if (characterizedAs === 'empty') {
+    expect(this.result).to.be.empty;
+    if (typeof resultTable === 'function'){
+      return resultTable();
+    }
+    return;
+  }
+  const expectedResult = resultTable.rows().map(row => parseRow.call(this, row));
+  switch (characterizedAs) {
+    case 'ordered':
+      expect(toCompare(this.result)).to.have.deep.ordered.members(expectedResult);
+      break;
+    case 'unordered':
+      expect(toCompare(this.result)).to.have.deep.members(expectedResult);
+      break;
+    case 'of':
+      // result is a subset of the expected
+      expect(expectedResult).to.include.deep.members(toCompare(this.result));
+      break;
+  }
+});
+
+Then(/^the graph should return (\d+) for count of "(.+)"$/, function (stringCount, traversalText) {
+  const traversal = vm.runInNewContext(translate(traversalText), getSandbox(this.g, this.parameters));
+  return traversal.toList().then(list => {
+    expect(list).to.have.lengthOf(parseInt(stringCount, 10));
+  });
+});
+
+Then(/^the result should have a count of (\d+)$/, function (stringCount) {
+  const expected = parseInt(stringCount, 10);
+  if (!Array.isArray(this.result)) {
+    let count = 0;
+    if (this.result instanceof Map) {
+      count = this.result.size;
+    }
+    else if (typeof this.result === 'object') {
+      count = Object.keys(this.result).length;
+    }
+    else {
+      throw new Error('result not supported: ' + util.inspect(this.result));
+    }
+    expect(count).to.be.equal(expected);
+    return;
+  }
+  expect(this.result).to.have.lengthOf(expected);
+});
+
+Then('nothing should happen because', _ => {});
 
 function getSandbox(g, parameters) {
   const sandbox = {
