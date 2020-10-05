e364b0ab5207befc8e5f674ae232cb302981fe9e
Stephen Mallette <stepmall@amazon.com>
2020-10-05 07:35:00 -0400

    Improved clone() test in python
    Tried a few different things to recreate a problem described in the comments of TINKERPOP-2315 but wasn't able to. All my variations seemed to pass. Test seems a bit better as it is now so will leave it at that.

--- gremlin-python/src/main/jython/tests/driver/test_driver_remote_connection.py
+++ gremlin-python/src/main/jython/tests/driver/test_driver_remote_connection.py
@@ -313,8 +313,8 @@ class TestDriverRemoteConnection(object):
 
     def test_clone(self, remote_connection):
         g = traversal().withRemote(remote_connection)
-        t = g.V().count()
-        assert 6 == t.next()
-        assert 6 == t.clone().next()
-        assert 6 == t.clone().next()
+        t = g.V().both()
+        assert 12 == len(t.toList())
+        assert 5 == t.clone().limit(5).count().next()
+        assert 10 == t.clone().limit(10).count().next()
 
