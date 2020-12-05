39db2b60da01125a2049a7e8a38a133e38cbcd57
HadoopMarc <vtslab@xs4all.nl>
2020-12-05 17:14:51 +0100

    Adds sessioned string requests to GremlinServerAuthzIntegrateTest

--- gremlin-server/src/test/java/org/apache/tinkerpop/gremlin/server/GremlinServerAuthzIntegrateTest.java
+++ gremlin-server/src/test/java/org/apache/tinkerpop/gremlin/server/GremlinServerAuthzIntegrateTest.java
@@ -258,6 +258,20 @@ public class GremlinServerAuthzIntegrateTest extends AbstractGremlinServerIntegr
     }
 
     @Test
+    public void shouldAuthorizeSessionedStringRequest() throws Exception {
+        final Cluster cluster = TestClientFactory.build().credentials("marko", "rainbow-dash").create();
+        final Client client = cluster.connect("session1");
+
+        try {
+            assertEquals(2, client.submit("a = 4; 1+1").all().get().get(0).getInt());
+            assertEquals(10, client.submit("gclassic.V().count().next() + a").all().get().get(0).getInt());
+            assertEquals(6, client.submit("gmodern.V().map{it.get().value('name')}.count()").all().get().get(0).getInt());
+        } finally {
+            cluster.close();
+        }
+    }
+
+    @Test
     public void shouldFailBytecodeRequestWithAllowAllAuthenticator() {
         final Cluster cluster = TestClientFactory.build().create();
         try {
