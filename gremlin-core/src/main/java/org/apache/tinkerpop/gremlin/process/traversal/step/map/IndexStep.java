85215e31800357e4514f761f67e8df7986b816c7
Stephen Mallette <stepmall@amazon.com>
2021-06-02 11:52:43 -0400

    TINKERPOP-2548 Provided an enum to represent the IndexerType used on the IndexStep CTR

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/step/map/IndexStep.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/step/map/IndexStep.java
@@ -48,15 +48,23 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
 
     private final Parameters parameters = new Parameters();
 
-    private Function<Iterator, Object> indexer;
+    private Function<Iterator<?>, Object> indexer;
+    private IndexerType indexerType;
 
     public IndexStep(final Traversal.Admin traversal) {
         super(traversal);
         this.configure(WithOptions.indexer, WithOptions.list);
     }
 
-    public Function<Iterator, Object> getIndexer() {
+    public Function<Iterator<?>, Object> getIndexer() {
         return indexer;
+    }
+
+    /**
+     * Gets the type of indexer that is configured for this step.
+     */
+    public IndexerType getIndexerType() {
+        return indexerType;
     }
 
     @Override
@@ -70,7 +78,7 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
         return super.hashCode() ^ this.indexer.hashCode();
     }
 
-    private static List<List<Object>> indexedList(final Iterator iterator) {
+    private static List<List<Object>> indexedList(final Iterator<?> iterator) {
         final List<List<Object>> list = new ArrayList<>();
         int i = 0;
         while (iterator.hasNext()) {
@@ -79,7 +87,7 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
         return Collections.unmodifiableList(list);
     }
 
-    private static Map<Integer, Object> indexedMap(final Iterator iterator) {
+    private static Map<Integer, Object> indexedMap(final Iterator<?> iterator) {
         final Map<Integer, Object> map = new LinkedHashMap<>();
         int i = 0;
         while (iterator.hasNext()) {
@@ -95,7 +103,7 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
 
     @Override
     public void configure(final Object... keyValues) {
-        final Integer indexer;
+        final int indexer;
         if (keyValues[0].equals(WithOptions.indexer)) {
             if (keyValues.length == 2 && keyValues[1] instanceof Integer) {
                 indexer = (Integer) keyValues[1];
@@ -103,8 +111,10 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
                 throw INVALID_CONFIGURATION_EXCEPTION;
             }
             if (indexer == WithOptions.list) {
+                this.indexerType = IndexerType.LIST;
                 this.indexer = IndexStep::indexedList;
             } else if (indexer == WithOptions.map) {
+                this.indexerType = IndexerType.MAP;
                 this.indexer = IndexStep::indexedMap;
             } else {
                 throw INVALID_CONFIGURATION_EXCEPTION;
@@ -117,6 +127,23 @@ public class IndexStep<S, E> extends MapStep<S, E> implements TraversalParent, C
     @Override
     public Parameters getParameters() {
         return parameters;
+    }
+
+    /**
+     * Type of the index as it corresponds to the associated {@link WithOptions#list} and {@link WithOptions#map}.
+     */
+    public enum IndexerType {
+        LIST(WithOptions.list),
+        MAP(WithOptions.map);
+
+        private final int type;
+        IndexerType(final int type) {
+            this.type = type;
+        }
+
+        public int getType() {
+            return type;
+        }
     }
 }
 
