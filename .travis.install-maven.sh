1c07696df44181dffaf5905fabb0fdb5c73e3523
Stephen Mallette <stepmall@amazon.com>
2021-09-30 18:56:00 -0400

    Fix maven install for python3 CTR

--- .travis.install-maven.sh
+++ .travis.install-maven.sh
@@ -8,7 +8,7 @@ if [ ! -f "${MVN_INSTALL_DIR}/lib/maven-artifact-${MVN_VERSION}.jar" ]; then
   rm -Rf "${MVN_INSTALL_DIR}"
   mkdir -p "${MVN_INSTALL_DIR}"
 
-  APACHE_MIRROR="$(curl -sL https://www.apache.org/dyn/closer.cgi?asjson=1 | python -c 'import sys, json; print json.load(sys.stdin)["preferred"]')"
+  APACHE_MIRROR="$(curl -sL https://www.apache.org/dyn/closer.cgi?asjson=1 | python -c 'import sys, json; print(json.load(sys.stdin)["preferred"])')"
   curl -o "${HOME}/apache-maven-$MVN_VERSION-bin.tar.gz" "$APACHE_MIRROR/maven/maven-3/$MVN_VERSION/binaries/apache-maven-$MVN_VERSION-bin.tar.gz"
   cd "${MVN_INSTALL_DIR}"
   tar -xzf "${HOME}/apache-maven-$MVN_VERSION-bin.tar.gz" --strip 1
