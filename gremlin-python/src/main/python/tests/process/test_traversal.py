13b616f2b2d21ab2d5d189f2335381e44fc05ae0
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-12-07 22:59:57 -0800

    Source: [1] Added neo4j-gremlin dependency to pom.xml, fixed issue with TEST_TRANSACTION environment variable. [2] Made DriverRemoteConnection latch parameters so they can be reused to create a subsequent session [3] Added logging throughout the driver [4] Added commit and rollback to DriverRemoteConnection [5] Added some logging to receive message [6] Added transaction support to RemoteConnection [7] Added bytecode support to Session processor [8] Fixed bug in aiohttp transport layer that popped up when it was not shutdown properly [9] Added Transaction class to graph_traversal.py, added tx function which generates a Transaction [10] Added create_graph_op method that allows a simple Bytecode with source to be created [11] Added GraphOp class with commit and rollback functions which return Bytecode for commit and rollback
    Test:
    [12] Enhance test configuration to have remote_transaction_connection
    [13] Updated submitAsync -> submit_async for test_client
    [14] Added transaction test to exercise basic commit
    [15] Added transaction test to exercise basic rollback
    [16] Added transaction test to exercise multi commit
    [17] Added transaction test to exercise multi rollback
    [18] Added transaction test to exercise multi commit and rollback together
    [19] Added transaction test to exercise illegal operations (negative test)
    
    Documentation:
    [20] Added updated documentation for gtx implementation

--- gremlin-python/src/main/python/tests/process/test_traversal.py
+++ gremlin-python/src/main/python/tests/process/test_traversal.py
@@ -17,8 +17,10 @@
 # under the License.
 #
 
-__author__ = 'Marko A. Rodriguez (http://markorodriguez.com)'
+__author__ = 'Marko A. Rodriguez (http://markorodriguez.com), Lyndon Bauto (lyndonb@bitquilltech.com)'
 
+import os
+import pytest
 from pytest import fail
 
 from gremlin_python.structure.graph import Graph
@@ -26,6 +28,10 @@ from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.process.traversal import P
 from gremlin_python.process.traversal import Binding, Bindings
 from gremlin_python.process.graph_traversal import __
+
+
+def transactions_enabled():
+    return os.environ['TEST_TRANSACTIONS'] if 'TEST_TRANSACTIONS' in os.environ else False
 
 
 class TestTraversal(object):
@@ -58,9 +64,9 @@ class TestTraversal(object):
         bytecode = g.V(Bindings.of('a', [1, 2, 3])) \
             .out(Bindings.of('b', 'created')) \
             .where(__.in_(Bindings.of('c', 'created'), Bindings.of('d', 'knows')) \
-            .count().is_(Bindings.of('e', P.gt(2)))).bytecode
+                   .count().is_(Bindings.of('e', P.gt(2)))).bytecode
         assert 5 == len(bytecode.bindings.keys())
-        assert [1,2,3] == bytecode.bindings['a']
+        assert [1, 2, 3] == bytecode.bindings['a']
         assert 'created' == bytecode.bindings['b']
         assert 'created' == bytecode.bindings['c']
         assert 'knows' == bytecode.bindings['d']
@@ -116,7 +122,8 @@ class TestTraversal(object):
             t = g.V().__len__
             fail("can't do sugar with magic")
         except AttributeError as err:
-            assert str(err) == 'Python magic methods or keys starting with double underscore cannot be used for Gremlin sugar - prefer values(__len__)'
+            assert str(
+                err) == 'Python magic methods or keys starting with double underscore cannot be used for Gremlin sugar - prefer values(__len__)'
 
     def test_enforce_anonymous_child_traversal(self):
         g = traversal().withGraph(Graph())
@@ -124,8 +131,228 @@ class TestTraversal(object):
 
         try:
             g.V(0).addE("self").to(g.V(1))
-            assert false
+            assert False
         except TypeError:
             pass
 
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_transaction_commit(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        start_count = g.V().count().next()
+        tx = g.tx()
 
+        # Except transaction to not be open until begin is called.
+        assert not tx.isOpen()
+        gtx = tx.begin()
+        assert tx.isOpen()
+
+        add_node_validate_transaction_state(g, gtx, start_count, start_count, [tx])
+        add_node_validate_transaction_state(gtx, g, start_count + 2, start_count, [tx])
+
+        # Commit the transaction, this should close it and add our transaction only vertex to the graph,
+        # so our vertex count outside the transaction should be 2 + the start count.
+        tx.commit()
+        assert not tx.isOpen()
+        assert g.V().count().next() == start_count + 2
+
+        drop_graph_check_count(g)
+        verify_gtx_closed(gtx)
+
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_transaction_rollback(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        start_count = g.V().count().next()
+        tx = g.tx()
+
+        # Except transaction to not be open until begin is called.
+        assert not tx.isOpen()
+        gtx = tx.begin()
+        assert tx.isOpen()
+
+        add_node_validate_transaction_state(g, gtx, start_count, start_count, [tx])
+        add_node_validate_transaction_state(gtx, g, start_count + 2, start_count, [tx])
+
+        # Commit the transaction, this should close it and not add our transaction only vertex to the graph,
+        # so our vertex count outside the transaction should be the start count.
+        tx.rollback()
+        assert not tx.isOpen()
+        assert g.V().count().next() == start_count + 1
+
+        drop_graph_check_count(g)
+        verify_gtx_closed(gtx)
+
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_transaction_no_begin(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        tx = g.tx()
+
+        # Except transaction to not be open until begin is called.
+        assert not tx.isOpen()
+
+        try:
+            # Attempt to commit even though no tx is started.
+            tx().commit()
+            assert False
+        except Exception as e:
+            assert not tx.isOpen()
+
+        try:
+            # Attempt to rollback even though no tx is started.
+            tx().rollback()
+            assert False
+        except Exception as e:
+            assert not tx.isOpen()
+
+        try:
+            # Attempt to invoke tx().tx() which is illegal.
+            tx().tx()
+            assert False
+        except Exception as e:
+            assert not tx.isOpen()
+
+        # Create transaction and verify it is open.
+        gtx = tx.begin()
+        assert tx.isOpen()
+        try:
+            # Attempt to begin gtx which is illegal.
+            gtx().begin()
+            assert False
+        except Exception as e:
+            assert tx.isOpen()
+
+        # Commit this unused transaction and verify it is no longer open.
+        tx.commit()
+        assert not tx.isOpen()
+
+        # Create another transaction and verify it is open.
+        gtx = tx.begin()
+        assert tx.isOpen()
+
+        # Rollback this unused transaction and verify it is no longer open.
+        tx.rollback()
+        assert not tx.isOpen()
+
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_multi_commit_transaction(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        start_count = g.V().count().next()
+
+        # Create two transactions.
+        tx1, tx2 = g.tx(), g.tx()
+
+        # Generate two GraphTraversalSource's for each transaction with begin.
+        gtx1, gtx2 = tx1.begin(), tx2.begin()
+        verify_tx_state([tx1, tx2], True)
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx2, which should be visible to gtx2, not gtx2
+        add_node_validate_transaction_state(g, gtx2, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2. Note previous node also added.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count + 1, [tx1, tx2])
+
+        tx1.commit()
+        verify_tx_state([tx1], False)
+        verify_tx_state([tx2], True)
+        assert g.V().count().next() == start_count + 2
+
+        tx2.commit()
+        verify_tx_state([tx1, tx2], False)
+        assert g.V().count().next() == start_count + 3
+
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_multi_rollback_transaction(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        start_count = g.V().count().next()
+
+        # Create two transactions.
+        tx1, tx2 = g.tx(), g.tx()
+
+        # Generate two GraphTraversalSource's for each transaction with begin.
+        gtx1, gtx2 = tx1.begin(), tx2.begin()
+        verify_tx_state([tx1, tx2], True)
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx2, which should be visible to gtx2, not gtx2
+        add_node_validate_transaction_state(g, gtx2, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2. Note previous node also added.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count + 1, [tx1, tx2])
+
+        tx1.rollback()
+        verify_tx_state([tx1], False)
+        verify_tx_state([tx2], True)
+        assert g.V().count().next() == start_count
+
+        tx2.rollback()
+        verify_tx_state([tx1, tx2], False)
+        assert g.V().count().next() == start_count
+
+    @pytest.mark.skipif(not transactions_enabled(), reason="Transactions are not enabled.")
+    def test_multi_commit_and_rollback(self, remote_transaction_connection):
+        # Start a transaction traversal.
+        g = traversal().withRemote(remote_transaction_connection)
+        start_count = g.V().count().next()
+
+        # Create two transactions.
+        tx1, tx2 = g.tx(), g.tx()
+
+        # Generate two GraphTraversalSource's for each transaction with begin.
+        gtx1, gtx2 = tx1.begin(), tx2.begin()
+        verify_tx_state([tx1, tx2], True)
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx2, which should be visible to gtx2, not gtx2
+        add_node_validate_transaction_state(g, gtx2, start_count, start_count, [tx1, tx2])
+
+        # Add node to gtx1, which should be visible to gtx1, not gtx2. Note previous node also added.
+        add_node_validate_transaction_state(g, gtx1, start_count, start_count + 1, [tx1, tx2])
+
+        tx1.commit()
+        verify_tx_state([tx1], False)
+        verify_tx_state([tx2], True)
+        assert g.V().count().next() == start_count + 2
+
+        tx2.rollback()
+        verify_tx_state([tx1, tx2], False)
+        assert g.V().count().next() == start_count + 2
+
+
+def add_node_validate_transaction_state(g, g_add_to, g_start_count, g_add_to_start_count, tx_verify_list):
+    # Add a single node to g_add_to, but not g.
+    # Check that vertex count in g is g_start_count and vertex count in g_add_to is g_add_to_start_count + 1.
+    g_add_to.addV("person").property("name", "lyndon").iterate()
+    assert g_add_to.V().count().next() == g_add_to_start_count + 1
+    assert g.V().count().next() == g_start_count
+    verify_tx_state(tx_verify_list, True)
+
+
+def verify_tx_state(gtx_list, value):
+    for tx in gtx_list:
+        assert tx.isOpen() == value
+
+
+def drop_graph_check_count(g):
+    g.V().drop().iterate()
+    assert g.V().count().next() == 0
+
+
+def verify_gtx_closed(gtx):
+    try:
+        # Attempt to add an additional vertex to the transaction. This should throw an exception since it
+        # has been rolled back.
+        gtx().addV("failure").iterate()
+        assert False
+    except Exception as e:
+        pass
