21163e34c3af08689bab0ec64377cf2afbdae7c2
Yang Xia <55853655+xiazcy@users.noreply.github.com>
2022-02-08 11:03:37 -0800

    Implemented Result with reference to Java GLV Result class

--- gremlin-go/driver/result.go
+++ gremlin-go/driver/result.go
@@ -21,18 +21,149 @@ package gremlingo
 
 // AN-968 Finish Result implementation.
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 // Result Struct to abstract the Result and provide functions to use it.
 type Result struct {
 	result interface{}
 }
 
-// AsString returns the current Result formatted as a string.
+func newResult(result interface{}) *Result {
+	return &Result{result}
+}
+
+// AsString returns the current result item formatted as a string.
 func (r *Result) AsString() string {
 	return fmt.Sprintf("%v", r.result)
 }
 
-func newResult(result interface{}) *Result {
-	return &Result{result}
+// ToString returns the string representation of the Result struct in Go-syntax format
+func (r *Result) ToString() string {
+	return fmt.Sprintf("%#v", *r)
+}
+
+// GetString gets the string representation of the result, equivalent to AsString()
+func (r *Result) GetString() string {
+	return r.AsString()
+}
+
+// GetInt gets the result by coercing it into an int, else returns an error if not parsable
+func (r *Result) GetInt() (int, error) {
+	return strconv.Atoi(r.AsString())
+}
+
+// GetByte gets the result by coercing it into a byte, else returns an error if not parsable
+func (r *Result) GetByte() (byte, error) {
+	res, err := strconv.ParseUint(r.AsString(), 10, 8)
+	if err != nil {
+		return 0, err
+	}
+	return byte(res), nil
+}
+
+// GetShort gets the result by coercing it into an int16, else returns an error if not parsable
+func (r *Result) GetShort() (int16, error) {
+	res, err := strconv.ParseInt(r.AsString(), 10, 16)
+	if err != nil {
+		return 0, err
+	}
+	return int16(res), nil
+}
+
+// GetLong gets the result by coercing it into an int64, else returns an error if not parsable
+func (r *Result) GetLong() (int64, error) {
+	return strconv.ParseInt(r.AsString(), 10, 64)
+}
+
+// GetFloat gets the result by coercing it into a float32, else returns an error if not parsable
+func (r *Result) GetFloat() (float32, error) {
+	res, err := strconv.ParseFloat(r.AsString(), 32)
+	if err != nil {
+		return 0, err
+	}
+	return float32(res), nil
+}
+
+// GetDouble gets the result by coercing it into a float64, else returns an error if not parsable
+func (r *Result) GetDouble() (float64, error) {
+	return strconv.ParseFloat(r.AsString(), 64)
+}
+
+// GetBoolean gets the result by coercing it into a boolean, else returns an error if not parsable
+func (r *Result) GetBoolean() (bool, error) {
+	return strconv.ParseBool(r.AsString())
+}
+
+// IsNil checks if the result is null
+func (r *Result) IsNil() bool {
+	return nil == r.result
+}
+
+// GetVertex returns the result if it is a vertex, otherwise returns an error
+func (r *Result) GetVertex() (*Vertex, error) {
+	res, ok := r.result.(Vertex)
+	if !ok {
+		return nil, errors.New("result is not a Vertex")
+	}
+	return &res, nil
+}
+
+// GetEdge returns the result if it is an edge, otherwise returns an error
+func (r *Result) GetEdge() (*Edge, error) {
+	res, ok := r.result.(Edge)
+	if !ok {
+		return nil, errors.New("result is not an Edge")
+	}
+	return &res, nil
+}
+
+// GetElement returns the result if it is an element, otherwise returns an error
+func (r *Result) GetElement() (*Element, error) {
+	res, ok := r.result.(Element)
+	if !ok {
+		return nil, errors.New("result is not an Element")
+	}
+	return &res, nil
+}
+
+// GetPath returns the result if it is a path, otherwise returns an error
+func (r *Result) GetPath() (*Path, error) {
+	res, ok := r.result.(Path)
+	if !ok {
+		return nil, errors.New("result is not a Path")
+	}
+	return &res, nil
+}
+
+// GetProperty returns the result if it is a property, otherwise returns an error
+func (r *Result) GetProperty() (*Property, error) {
+	res, ok := r.result.(Property)
+	if !ok {
+		return nil, errors.New("result is not a Property")
+	}
+	return &res, nil
+}
+
+// GetVertexProperty returns the result if it is a vertex property, otherwise returns an error
+func (r *Result) GetVertexProperty() (*VertexProperty, error) {
+	res, ok := r.result.(VertexProperty)
+	if !ok {
+		return nil, errors.New("result is not a VertexProperty")
+	}
+	return &res, nil
+}
+
+// GetType returns the type of the result
+func (r *Result) GetType() reflect.Type {
+	return reflect.TypeOf(r.result)
+}
+
+// GetInterface returns the result item
+func (r *Result) GetInterface() interface{} {
+	return r.result
 }
--- gremlin-go/driver/result_test.go
+++ gremlin-go/driver/result_test.go
@@ -20,6 +20,7 @@ under the License.
 package gremlingo
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -29,24 +30,248 @@ func TestResult(t *testing.T) {
 
 	t.Run("Test Result.AsString() string", func(t *testing.T) {
 		r := Result{"foo"}
-		assert.Equal(t, r.AsString(), "foo")
+		assert.Equal(t, "foo", r.AsString())
 	})
 
 	t.Run("Test Result.AsString() slice", func(t *testing.T) {
 		r := Result{[]int{1, 2, 3}}
-		assert.Equal(t, r.AsString(), "[1 2 3]")
-
+		assert.Equal(t, "[1 2 3]", r.AsString())
 	})
 
 	t.Run("Test Result.AsString() int", func(t *testing.T) {
 		r := Result{1}
-		assert.Equal(t, r.AsString(), "1")
-
+		assert.Equal(t, "1", r.AsString())
 	})
 
 	t.Run("Test Result.AsString() float", func(t *testing.T) {
 		r := Result{1.2}
-		assert.Equal(t, r.AsString(), "1.2")
+		assert.Equal(t, "1.2", r.AsString())
+	})
 
+	t.Run("Test Result.GetString()", func(t *testing.T) {
+		r := Result{1.2}
+		assert.Equal(t, "1.2", r.GetString())
+	})
+
+	t.Run("Test Result.GetInt()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetInt()
+		assert.Nil(t, err)
+		assert.Equal(t, 100, res)
+	})
+
+	t.Run("Test Result.GetInt() error expected", func(t *testing.T) {
+		r := Result{"not int"}
+		res, err := r.GetInt()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetByte()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetByte()
+		assert.Nil(t, err)
+		assert.Equal(t, byte(100), res)
+	})
+
+	t.Run("Test Result.GetByte() error expected", func(t *testing.T) {
+		r := Result{"not byte"}
+		res, err := r.GetByte()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetShort()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetShort()
+		assert.Nil(t, err)
+		assert.Equal(t, int16(100), res)
+	})
+
+	t.Run("Test Result.GetShort() error expected", func(t *testing.T) {
+		r := Result{"not short"}
+		res, err := r.GetShort()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetLong()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetLong()
+		assert.Nil(t, err)
+		assert.Equal(t, int64(100), res)
+	})
+
+	t.Run("Test Result.GetLong() error expected", func(t *testing.T) {
+		r := Result{"not long"}
+		res, err := r.GetLong()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetFloat()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetFloat()
+		assert.Nil(t, err)
+		assert.Equal(t, float32(100), res)
+	})
+
+	t.Run("Test Result.GetFloat() error expected", func(t *testing.T) {
+		r := Result{"not float"}
+		res, err := r.GetFloat()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetDouble()", func(t *testing.T) {
+		r := Result{100}
+		res, err := r.GetDouble()
+		assert.Nil(t, err)
+		assert.Equal(t, float64(100), res)
+	})
+
+	t.Run("Test Result.GetDouble() error expected", func(t *testing.T) {
+		r := Result{"not double"}
+		res, err := r.GetDouble()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetBoolean() number representation", func(t *testing.T) {
+		r := Result{1}
+		res, err := r.GetBoolean()
+		assert.Nil(t, err)
+		assert.Equal(t, true, res)
+	})
+
+	t.Run("Test Result.GetBoolean() bool representation", func(t *testing.T) {
+		r := Result{false}
+		res, err := r.GetBoolean()
+		assert.Nil(t, err)
+		assert.Equal(t, false, res)
+	})
+
+	t.Run("Test Result.GetBoolean() error expected", func(t *testing.T) {
+		r := Result{"not boolean"}
+		res, err := r.GetBoolean()
+		assert.Error(t, err)
+		assert.Zero(t, res)
+	})
+
+	t.Run("Test Result.GetVertex()", func(t *testing.T) {
+		vertex := Vertex{}
+		r := Result{vertex}
+		res, err := r.GetVertex()
+		assert.Nil(t, err)
+		assert.Equal(t, &vertex, res)
+	})
+
+	t.Run("Test Result.GetVertex() error expected", func(t *testing.T) {
+		r := Result{"not vertex"}
+		res, err := r.GetVertex()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not a Vertex")
+	})
+
+	t.Run("Test Result.GetEdge()", func(t *testing.T) {
+		edge := Edge{}
+		r := Result{edge}
+		res, err := r.GetEdge()
+		assert.Nil(t, err)
+		assert.Equal(t, &edge, res)
+	})
+
+	t.Run("Test Result.GetEdge() error expected", func(t *testing.T) {
+		r := Result{"not edge"}
+		res, err := r.GetEdge()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not an Edge")
+	})
+
+	t.Run("Test Result.GetElement()", func(t *testing.T) {
+		element := Element{}
+		r := Result{element}
+		res, err := r.GetElement()
+		assert.Nil(t, err)
+		assert.Equal(t, &element, res)
+	})
+
+	t.Run("Test Result.GetElement() error expected", func(t *testing.T) {
+		r := Result{"not element"}
+		res, err := r.GetElement()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not an Element")
+	})
+
+	t.Run("Test Result.GetPath()", func(t *testing.T) {
+		path := Path{}
+		r := Result{path}
+		res, err := r.GetPath()
+		assert.Nil(t, err)
+		assert.Equal(t, &path, res)
+	})
+
+	t.Run("Test Result.GetPath() error expected", func(t *testing.T) {
+		r := Result{"not path"}
+		res, err := r.GetPath()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not a Path")
+	})
+
+	t.Run("Test Result.GetProperty()", func(t *testing.T) {
+		property := Property{}
+		r := Result{property}
+		res, err := r.GetProperty()
+		assert.Nil(t, err)
+		assert.Equal(t, &property, res)
+	})
+
+	t.Run("Test Result.GetProperty() error expected", func(t *testing.T) {
+		r := Result{"not property"}
+		res, err := r.GetProperty()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not a Property")
+	})
+
+	t.Run("Test Result.GetVertexProperty()", func(t *testing.T) {
+		vertexProperty := VertexProperty{}
+		r := Result{vertexProperty}
+		res, err := r.GetVertexProperty()
+		assert.Nil(t, err)
+		assert.Equal(t, &vertexProperty, res)
+	})
+
+	t.Run("Test Result.GetVertexProperty() error expected", func(t *testing.T) {
+		r := Result{"not vertex property"}
+		res, err := r.GetVertexProperty()
+		assert.Nil(t, res)
+		assert.Error(t, err, "result is not a VertexProperty")
+	})
+
+	t.Run("Test Result.ToString()", func(t *testing.T) {
+		r := Result{[]int{1, 2, 3}}
+		res := r.ToString()
+		assert.Equal(t, "gremlingo.Result{result:[]int{1, 2, 3}}", res)
+	})
+
+	t.Run("Test Result.GetType() simple type", func(t *testing.T) {
+		str := "result string"
+		r := Result{str}
+		res := r.GetType()
+		assert.Equal(t, reflect.TypeOf(str), res)
+	})
+
+	t.Run("Test Result.GetType() custom defined type", func(t *testing.T) {
+		vertexProperty := VertexProperty{}
+		r := Result{vertexProperty}
+		res := r.GetType()
+		assert.Equal(t, reflect.TypeOf(vertexProperty), res)
+	})
+
+	t.Run("Test Result.GetInterface()", func(t *testing.T) {
+		vertexProperty := VertexProperty{}
+		r := Result{vertexProperty}
+		res := r.GetInterface()
+		assert.Equal(t, vertexProperty, res)
 	})
 }
