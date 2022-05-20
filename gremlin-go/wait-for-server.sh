75f7c9ee86f3a72f9e26d2b71bf9fc0e4254c414
Yang Xia <55853655+xiazcy@users.noreply.github.com>
2022-05-20 13:41:26 -0700

    added a max retry field to wait for script to impose limit if needed.

--- gremlin-go/wait-for-server.sh
+++ gremlin-go/wait-for-server.sh
@@ -20,12 +20,40 @@
 
 set -e
 
+function usage {
+  echo -e "\nUsage: $(basename "$0") <host> <port> <max retry> [OPTIONS]" \
+          "\n\nCurls a Gremlin Server HTTP endpoint until success to signal that the server is ready, or until max number of retries reached to signal failure." \
+          "\n\nOptions:" \
+          "\n\t<host> \t\t Gremlin server host." \
+          "\n\t<host> \t\t Gremlin server port." \
+          "\n\t<max retry> \t Number of times to retry connection, entering 0 or less means no retry limit." \
+          "\n\t-h, --help \t Show this message." \
+          "\n"
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    usage
+    exit 0
+fi
+
 host="$1"; shift
 port="$1"; shift
+max_retries="$1"; shift
 
+# Setting max retry to 0 or less means no retry limit.
+if [ "$max_retries" -le 0 ] ; then
+  max_retries=-1
+fi
+
+counter=0
 until curl "$host:$port?gremlin=100-1"; do
+  if [ "$counter" -eq "$max_retries" ] ; then
+    echo "Gremlin server is unavailable - exiting: max retries $max_retries reached, please check server setup or increase max retry value"
+    exit 1
+  fi
   >&2 echo "Gremlin server is unavailable - sleeping"
   sleep 1
+  (( counter++ )) || true
 done
 
 >&2 echo "Gremlin Server is up - executing command"
