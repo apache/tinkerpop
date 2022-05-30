cf081508db2b2b60913652cfb4c7d05e844ff1b6
Igor Ostapenko <ihoro@users.noreply.github.com>
2022-05-30 22:46:34 +0300

    Add short overview doc comments about GraphBinary

--- gremlin-javascript/src/main/javascript/gremlin-javascript/lib/structure/io/binary/GraphBinary.js
+++ gremlin-javascript/src/main/javascript/gremlin-javascript/lib/structure/io/binary/GraphBinary.js
@@ -18,6 +18,18 @@
  */
 
 /**
+ * GraphBinary 1.0 support implementation.
+ *
+ * The officially expected entrypoint is GraphBinaryReader/GraphBinaryWriter pair of classes,
+ * examine lib/driver/** for use cases.
+ *
+ * See AnySerializer.serialize() for the mechanism of serializer selection for a given JavaScript value,
+ * also consider AnySerializer.serialize() unit tests for real examples.
+ * See NumberSerializationStrategy to understand how it deals with JavaScript numbers' serialization.
+ *
+ * Consider AnySerializer.serialize()/deserialize() unit tests to see what is not implemented,
+ * what is ignored, what is not expected to be (de)serialized, etc.
+ *
  * @author Igor Ostapenko
  */
 'use strict';
@@ -29,43 +41,43 @@ ioc.utils = require('./internals/utils');
 
 ioc.serializers = {};
 
-ioc.intSerializer = new (require('./internals/IntSerializer'))(ioc);
-ioc.longSerializer = new (require('./internals/LongSerializer'))(ioc);
-ioc.longSerializerNg = new (require('./internals/LongSerializerNg'))(ioc);
-ioc.stringSerializer = new (require('./internals/StringSerializer'))(ioc, ioc.DataType.STRING);
-ioc.dateSerializer = new (require('./internals/DateSerializer'))(ioc, ioc.DataType.DATE);
-ioc.timestampSerializer = new (require('./internals/DateSerializer'))(ioc, ioc.DataType.TIMESTAMP);
-ioc.classSerializer = new (require('./internals/StringSerializer'))(ioc, ioc.DataType.CLASS);
-ioc.doubleSerializer = new (require('./internals/DoubleSerializer'))(ioc);
-ioc.floatSerializer = new (require('./internals/FloatSerializer'))(ioc);
-ioc.listSerializer = new (require('./internals/ArraySerializer'))(ioc, ioc.DataType.LIST);
-ioc.mapSerializer = new (require('./internals/MapSerializer'))(ioc);
-ioc.setSerializer = new (require('./internals/ArraySerializer'))(ioc, ioc.DataType.SET);
-ioc.uuidSerializer = new (require('./internals/UuidSerializer'))(ioc);
-ioc.edgeSerializer = new (require('./internals/EdgeSerializer'))(ioc);
-ioc.pathSerializer = new (require('./internals/PathSerializer'))(ioc);
-ioc.propertySerializer = new (require('./internals/PropertySerializer'))(ioc);
-ioc.vertexSerializer = new (require('./internals/VertexSerializer'))(ioc);
-ioc.vertexPropertySerializer = new (require('./internals/VertexPropertySerializer'))(ioc);
-ioc.bytecodeSerializer = new (require('./internals/BytecodeSerializer'))(ioc);
-ioc.pSerializer = new (require('./internals/PSerializer'))(ioc);
-ioc.traverserSerializer = new (require('./internals/TraverserSerializer'))(ioc);
-ioc.enumSerializer = new (require('./internals/EnumSerializer'))(ioc);
-ioc.lambdaSerializer = new (require('./internals/LambdaSerializer'))(ioc);
-ioc.bigIntegerSerializer = new (require('./internals/BigIntegerSerializer'))(ioc);
-ioc.byteSerializer = new (require('./internals/ByteSerializer'))(ioc);
-ioc.byteBufferSerializer = new (require('./internals/ByteBufferSerializer'))(ioc);
-ioc.shortSerializer = new (require('./internals/ShortSerializer'))(ioc);
-ioc.booleanSerializer = new (require('./internals/BooleanSerializer'))(ioc);
-ioc.textPSerializer = new (require('./internals/TextPSerializer'))(ioc);
+ioc.intSerializer               = new (require('./internals/IntSerializer'))(ioc);
+ioc.longSerializer              = new (require('./internals/LongSerializer'))(ioc);
+ioc.longSerializerNg            = new (require('./internals/LongSerializerNg'))(ioc);
+ioc.stringSerializer            = new (require('./internals/StringSerializer'))(ioc, ioc.DataType.STRING);
+ioc.dateSerializer              = new (require('./internals/DateSerializer'))(ioc, ioc.DataType.DATE);
+ioc.timestampSerializer         = new (require('./internals/DateSerializer'))(ioc, ioc.DataType.TIMESTAMP);
+ioc.classSerializer             = new (require('./internals/StringSerializer'))(ioc, ioc.DataType.CLASS);
+ioc.doubleSerializer            = new (require('./internals/DoubleSerializer'))(ioc);
+ioc.floatSerializer             = new (require('./internals/FloatSerializer'))(ioc);
+ioc.listSerializer              = new (require('./internals/ArraySerializer'))(ioc, ioc.DataType.LIST);
+ioc.mapSerializer               = new (require('./internals/MapSerializer'))(ioc);
+ioc.setSerializer               = new (require('./internals/ArraySerializer'))(ioc, ioc.DataType.SET);
+ioc.uuidSerializer              = new (require('./internals/UuidSerializer'))(ioc);
+ioc.edgeSerializer              = new (require('./internals/EdgeSerializer'))(ioc);
+ioc.pathSerializer              = new (require('./internals/PathSerializer'))(ioc);
+ioc.propertySerializer          = new (require('./internals/PropertySerializer'))(ioc);
+ioc.vertexSerializer            = new (require('./internals/VertexSerializer'))(ioc);
+ioc.vertexPropertySerializer    = new (require('./internals/VertexPropertySerializer'))(ioc);
+ioc.bytecodeSerializer          = new (require('./internals/BytecodeSerializer'))(ioc);
+ioc.pSerializer                 = new (require('./internals/PSerializer'))(ioc);
+ioc.traverserSerializer         = new (require('./internals/TraverserSerializer'))(ioc);
+ioc.enumSerializer              = new (require('./internals/EnumSerializer'))(ioc);
+ioc.lambdaSerializer            = new (require('./internals/LambdaSerializer'))(ioc);
+ioc.bigIntegerSerializer        = new (require('./internals/BigIntegerSerializer'))(ioc);
+ioc.byteSerializer              = new (require('./internals/ByteSerializer'))(ioc);
+ioc.byteBufferSerializer        = new (require('./internals/ByteBufferSerializer'))(ioc);
+ioc.shortSerializer             = new (require('./internals/ShortSerializer'))(ioc);
+ioc.booleanSerializer           = new (require('./internals/BooleanSerializer'))(ioc);
+ioc.textPSerializer             = new (require('./internals/TextPSerializer'))(ioc);
 ioc.traversalStrategySerializer = new (require('./internals/TraversalStrategySerializer'))(ioc);
-ioc.bulkSetSerializer = new (require('./internals/BulkSetSerializer'))(ioc);
-ioc.unspecifiedNullSerializer = new (require('./internals/UnspecifiedNullSerializer'))(ioc);
+ioc.bulkSetSerializer           = new (require('./internals/BulkSetSerializer'))(ioc);
+ioc.unspecifiedNullSerializer   = new (require('./internals/UnspecifiedNullSerializer'))(ioc);
 
 ioc.numberSerializationStrategy = new (require('./internals/NumberSerializationStrategy'))(ioc);
-ioc.anySerializer = new (require('./internals/AnySerializer'))(ioc);
+ioc.anySerializer               = new (require('./internals/AnySerializer'))(ioc);
 
-ioc.graphBinaryReader = new (require('./internals/GraphBinaryReader.js'))(ioc);
-ioc.graphBinaryWriter = new (require('./internals/GraphBinaryWriter'))(ioc);
+ioc.graphBinaryReader           = new (require('./internals/GraphBinaryReader.js'))(ioc);
+ioc.graphBinaryWriter           = new (require('./internals/GraphBinaryWriter'))(ioc);
 
 module.exports = ioc;
