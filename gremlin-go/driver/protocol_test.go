d8f91f8f72eeef3ea6908e771b2b5ff47c6b31ca
Roi Martin <jroi.martin@gmail.com>
2022-07-01 00:59:03 +0200

    gremlin-go: add unit test for (*gremlinServerWSProtocol).close

--- gremlin-go/driver/protocol_test.go
+++ gremlin-go/driver/protocol_test.go
@@ -20,12 +20,15 @@ under the License.
 package gremlingo
 
 import (
+	"sync"
+	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/text/language"
-	"testing"
 )
 
-func Test(t *testing.T) {
+func TestProtocol(t *testing.T) {
 	t.Run("Test protocol connect error.", func(t *testing.T) {
 		connSettings := newDefaultConnectionSettings()
 		connSettings.authInfo, connSettings.tlsConfig = nil, nil
@@ -36,5 +39,28 @@ func Test(t *testing.T) {
 			nil, nil)
 		assert.NotNil(t, err)
 		assert.Nil(t, protocol)
+	})
+
+	t.Run("Test protocol close when closed", func(t *testing.T) {
+		wg := &sync.WaitGroup{}
+		protocol := &gremlinServerWSProtocol{
+			closed: true,
+			mutex:  sync.Mutex{},
+			wg:     wg,
+		}
+		wg.Add(1)
+
+		done := make(chan bool)
+
+		go func() {
+			protocol.close()
+			done <- true
+		}()
+
+		select {
+		case <-time.After(1 * time.Second):
+			t.Fatal("timeout")
+		case <-done:
+		}
 	})
 }
