c16da29f190d071e9005917497c36e77da24918d
Cole Greer <coleg@bitquilltech.com>
2023-01-04 17:50:37 -0800

    Add RequestOptions to Go GLV
    Go was previously not sending per-request options correctly to the server.
    Go was passing request options inside the gremlin script bindings map instead of
    within the args portion of the request message.
    
    This commit resolves this issue by encapsulating all per-request settings as well as
    script bindings into a new RequestOptions struct which has an accompanying RequestOptionsBuilder.
    
    This can now be passed in through new Client.SubmitWithOptions() and DriverRemoteConnection.SubmitWithOptions() methods.
    Both original Submit() methods are unchanged in their behavior so this will not break any users.

--- gremlin-go/driver/client_test.go
+++ gremlin-go/driver/client_test.go
@@ -37,13 +37,14 @@ func TestClient(t *testing.T) {
 	testNoAuthAuthInfo := &AuthInfo{}
 	testNoAuthTlsConfig := &tls.Config{}
 
-	t.Run("Test client.submit()", func(t *testing.T) {
+	t.Run("Test client.Submit()", func(t *testing.T) {
 		skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
 		client, err := NewClient(testNoAuthUrl,
 			func(settings *ClientSettings) {
 				settings.TlsConfig = testNoAuthTlsConfig
 				settings.AuthInfo = testNoAuthAuthInfo
 			})
+		defer client.Close()
 		assert.Nil(t, err)
 		assert.NotNil(t, client)
 		resultSet, err := client.Submit("g.V().count()")
@@ -53,7 +54,25 @@ func TestClient(t *testing.T) {
 		assert.Nil(t, err)
 		assert.True(t, ok)
 		assert.NotNil(t, result)
-		client.Close()
+	})
+
+	t.Run("Test client.SubmitWithOptions()", func(t *testing.T) {
+		skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
+		client, err := NewClient(testNoAuthUrl,
+			func(settings *ClientSettings) {
+				settings.TlsConfig = testNoAuthTlsConfig
+				settings.AuthInfo = testNoAuthAuthInfo
+			})
+		defer client.Close()
+		assert.Nil(t, err)
+		assert.NotNil(t, client)
+		resultSet, err := client.SubmitWithOptions("g.V().count()", *new(RequestOptions))
+		assert.Nil(t, err)
+		assert.NotNil(t, resultSet)
+		result, ok, err := resultSet.One()
+		assert.Nil(t, err)
+		assert.True(t, ok)
+		assert.NotNil(t, result)
 	})
 }
 
@@ -91,6 +110,12 @@ type SocketServerSettings struct {
 	 * during the web socket handshake.
 	 */
 	USER_AGENT_REQUEST_ID uuid.UUID `yaml:"USER_AGENT_REQUEST_ID"`
+	/**
+	 * If a request with this ID comes to the server, the server responds with a string containing all overridden
+	 * per request settings from the request message. String will be of the form
+	 * "requestId=19436d9e-f8fc-4b67-8a76-deec60918424 evaluationTimeout=1234, batchSize=12, userAgent=testUserAgent"
+	 */
+	PER_REQUEST_SETTINGS_REQUEST_ID uuid.UUID `yaml:"PER_REQUEST_SETTINGS_REQUEST_ID"`
 }
 
 func FromYaml(path string) *SocketServerSettings {
@@ -121,16 +146,17 @@ func TestClientAgainstSocketServer(t *testing.T) {
 	t.Run("Should get single vertex response from gremlin socket server", func(t *testing.T) {
 		skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
 		client, err := NewClient(testSocketServerUrl)
+		defer client.Close()
 		assert.Nil(t, err)
 		assert.NotNil(t, client)
-		resultSet, err := client.Submit("1", map[string]interface{}{"requestId": settings.SINGLE_VERTEX_REQUEST_ID})
+		resultSet, err := client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+			SetRequestId(settings.SINGLE_VERTEX_REQUEST_ID).Create())
 		assert.Nil(t, err)
 		assert.NotNil(t, resultSet)
 		result, ok, err := resultSet.One()
 		assert.Nil(t, err)
 		assert.True(t, ok)
 		assert.NotNil(t, result)
-		client.Close()
 	})
 
 	/**
@@ -140,10 +166,12 @@ func TestClientAgainstSocketServer(t *testing.T) {
 	t.Run("Should include user agent in handshake request", func(t *testing.T) {
 		skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
 		client, err := NewClient(testSocketServerUrl)
+		defer client.Close()
 		assert.Nil(t, err)
 		assert.NotNil(t, client)
 
-		resultSet, err := client.Submit("1", map[string]interface{}{"requestId": settings.USER_AGENT_REQUEST_ID})
+		resultSet, err := client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+			SetRequestId(settings.USER_AGENT_REQUEST_ID).Create())
 		assert.Nil(t, err)
 		assert.NotNil(t, resultSet)
 
@@ -154,8 +182,6 @@ func TestClientAgainstSocketServer(t *testing.T) {
 
 		userAgentResponse := result.GetString()
 		assert.Equal(t, userAgent, userAgentResponse)
-
-		client.Close()
 	})
 
 	/**
@@ -168,10 +194,12 @@ func TestClientAgainstSocketServer(t *testing.T) {
 			func(settings *ClientSettings) {
 				settings.EnableUserAgentOnConnect = false
 			})
+		defer client.Close()
 		assert.Nil(t, err)
 		assert.NotNil(t, client)
 
-		resultSet, err := client.Submit("1", map[string]interface{}{"requestId": settings.USER_AGENT_REQUEST_ID})
+		resultSet, err := client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+			SetRequestId(settings.USER_AGENT_REQUEST_ID).Create())
 		assert.Nil(t, err)
 		assert.NotNil(t, resultSet)
 
@@ -184,8 +212,33 @@ func TestClientAgainstSocketServer(t *testing.T) {
 		//If the gremlin user agent is disabled, the underlying web socket library reverts to sending its default user agent
 		//during connection requests.
 		assert.Contains(t, userAgentResponse, "Go-http-client/")
+	})
 
-		client.Close()
+	/**
+	 * Tests that client is correctly sending all overridable per request settings (requestId, batchSize,
+	 * evaluationTimeout, and userAgent) to the server.
+	 */
+	t.Run("Should Send Per Request Settings To Server", func(t *testing.T) {
+		skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
+		client, err := NewClient(testSocketServerUrl)
+		defer client.Close()
+		assert.Nil(t, err)
+		assert.NotNil(t, client)
+
+		resultSet, err := client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+			SetRequestId(settings.PER_REQUEST_SETTINGS_REQUEST_ID).
+			SetEvaluationTimeout(1234).
+			SetBatchSize(12).
+			SetUserAgent("helloWorld").
+			Create())
+		assert.Nil(t, err)
+		assert.NotNil(t, resultSet)
+		result, ok, err := resultSet.One()
+		assert.Nil(t, err)
+		assert.True(t, ok)
+		expectedResult := fmt.Sprintf("requestId=%v evaluationTimeout=%v, batchSize=%v, userAgent=%v",
+			settings.PER_REQUEST_SETTINGS_REQUEST_ID, 1234, 12, "helloWorld")
+		assert.Equal(t, expectedResult, result.Data)
 	})
 
 	/**
@@ -197,9 +250,11 @@ func TestClientAgainstSocketServer(t *testing.T) {
 		t.Run("Should try create new connection if closed by server", func(t *testing.T) {
 			skipTestsIfNotEnabled(t, integrationTestSuiteName, testNoAuthEnable)
 			client, err := NewClient(testSocketServerUrl)
+			defer client.Close()
 			assert.Nil(t, err)
 			assert.NotNil(t, client)
-			resultSet, err := client.Submit("1", map[string]interface{}{"requestId": settings.CLOSE_CONNECTION_REQUEST_ID})
+			resultSet, err := client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+				SetRequestId(settings.CLOSE_CONNECTION_REQUEST_ID).Create())
 			assert.Nil(t, err)
 			assert.NotNil(t, resultSet)
 
@@ -207,14 +262,14 @@ func TestClientAgainstSocketServer(t *testing.T) {
 
 			assert.EqualError(t, err, "websocket: close 1005 (no status)")
 
-			resultSet, err = client.Submit("1", map[string]interface{}{"requestId": settings.SINGLE_VERTEX_REQUEST_ID})
+			resultSet, err = client.SubmitWithOptions("1", new(RequestOptionsBuilder).
+				SetRequestId(settings.SINGLE_VERTEX_REQUEST_ID).Create())
 			assert.Nil(t, err)
 			assert.NotNil(t, resultSet)
 			result, ok, err = resultSet.One()
 			assert.Nil(t, err)
 			assert.True(t, ok)
 			assert.NotNil(t, result)
-			client.Close()
 		})
 	*/
 }
