8d654b78f947742c9b61ac3726f609c35403a5fd
Stephen Mallette <stepmall@amazon.com>
2023-04-27 12:26:48 -0400

    Removed deprecated getInstance() from grammar visitors CTR

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/language/grammar/TraversalFunctionVisitor.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/language/grammar/TraversalFunctionVisitor.java
@@ -40,14 +40,6 @@ public class TraversalFunctionVisitor extends DefaultGremlinBaseVisitor<Function
     }
 
     /**
-     * @deprecated As of release 3.5.2, replaced by {@link #instance()}.
-     */
-    @Deprecated
-    public static TraversalFunctionVisitor getInstance() {
-        return instance();
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
