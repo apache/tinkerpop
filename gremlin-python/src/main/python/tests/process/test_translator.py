f5139bc448bbf15838b42edd40e3ecf264083dde
Alex <90222413+hamilton-earthscope@users.noreply.github.com>
2023-06-05 16:23:11 -0600

    (new) (gremlin-python) improve translation (#2076)
    This commit improves the python Translator class in a few ways:
    
        handle TextP predicates in addition to P
        handle quoting for subclasses of str
        handle None -> null
    
    Tests have been added to verify the changes.
    
    * (fix) translate text predicates
    
    * (fix) translate str subclasses
    
    * (test) verify translation changes
    
    * (new) fixup str subclass
    
    * (test) verify translation of text predicate with str subclass
    
    * (fix) translate None -> null

--- gremlin-python/src/main/python/tests/process/test_translator.py
+++ gremlin-python/src/main/python/tests/process/test_translator.py
@@ -354,6 +354,29 @@ class TestTraversalStrategies(object):
         tests.append([g.withComputer().V().shortestPath().with_(ShortestPath.target, __.has('name','peter')),
                      "g.withStrategies(new VertexProgramStrategy()).V().shortestPath().with('~tinkerpop.shortestPath.target',__.has('name','peter'))"])
 
+        # 99
+        tests.append([g.V().has("p1", starting_with("foo")),
+                     "g.V().has('p1',startingWith('foo'))"])
+
+        # 100
+        tests.append([g.V().has("p1", ending_with("foo")),
+                     "g.V().has('p1',endingWith('foo'))"])
+
+        # 101
+        class SuperStr(str):
+            pass
+        tests.append([g.V(SuperStr("foo_id")),
+                     "g.V('foo_id')"])
+
+        # 102
+        tests.append([g.V().has("p1", containing(SuperStr("foo"))),
+                     "g.V().has('p1',containing('foo'))"])
+
+        # 103
+        tests.append([g.V().has("p1", None),
+                     "g.V().has('p1',null)"])
+
+
         tlr = Translator().of('g')
 
         for t in range(len(tests)):
