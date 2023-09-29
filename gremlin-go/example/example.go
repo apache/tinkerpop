fb398f07e52d09889b90a041811ba664d08915bf
Ryan Tan <ryanmtan5@gmail.com>
2023-09-29 03:34:19 -0700

    WIP Go example

--- gremlin-go/example/example.go
+++ gremlin-go/example/example.go
@@ -21,16 +21,24 @@ package main
 
 import (
 	"fmt"
-
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/driver"
+	"strconv"
 )
 
 // syntactic sugar
 var __ = gremlingo.T__
 var gt = gremlingo.P.Gt
 var order = gremlingo.Order
+var T = gremlingo.T
+var P = gremlingo.P
 
 func main() {
+	//connectionExample()
+	basicGremlinExample()
+	modernTraversalExample()
+}
+
+func connectionExample() {
 	// Creating the connection to the server
 	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("ws://localhost:8182/gremlin")
 
@@ -55,14 +63,80 @@ func main() {
 	// Creating the graph traversal
 	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
 	fmt.Println(g)
+}
 
-	// Perform traversal
-	result, err := g.V().HasLabel("person").Has("age", __.Is(gt(28))).Order().By("age", order.Desc).Values("name").ToList()
+func basicGremlinExample() {
+	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("ws://localhost:8182/gremlin")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, r := range result {
-		fmt.Println(r.GetString())
+	defer driverRemoteConnection.Close()
+	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
+
+	// Basic Gremlin: adding and retrieving data
+	v1, err := g.AddV("person").Property("name", "marko").Next()
+	v2, err := g.AddV("person").Property("name", "fwroigheorigq").Next()
+	v3, err := g.AddV("person").Property("name", "wqrgrgegqregre").Next()
+
+	// Be sure to use a terminating step like next() or iterate() so that the traversal "executes"
+	// Iterate() does not return any data and is used to just generate side-effects (i.e. write data to the database)
+	g.V(v1).AddE("knows").To(v2).Property("weight", 0.75).Iterate()
+	g.V(v1).AddE("knows").To(v3).Property("weight", 0.75).Iterate()
+
+	// Retrieve the data from the "marko" vertex
+	marko, err := g.V().Has("person", "name", "marko").Next()
+	fmt.Sprintln("name: ", marko)
+
+	// Find the "marko" vertex and then traverse to the people he "knows" and return their data
+	peopleMarkoKnows, err := g.V().Has("person", "name", "marko").Out("knows").ToList()
+	for _, person := range peopleMarkoKnows {
+		fmt.Sprintln("marko knows ", person)
 	}
+
+}
+
+func modernTraversalExample() {
+	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("ws://localhost:8182/gremlin")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer driverRemoteConnection.Close()
+	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
+
+	/*
+	  This example requires the Modern toy graph to be preloaded upon launching the Gremlin server.
+	  For details, see https://tinkerpop.apache.org/docs/current/reference/#gremlin-server-docker-image and use
+	  conf/gremlin-server-modern.yaml.
+	*/
+	e1, err := g.V(1).BothE().ToList()                             // (1)
+	e2, err := g.V(1).BothE().Where(__.OtherV().HasId(2)).ToList() // (2)
+	v1, err := g.V(1).Next()
+	v2, err := g.V(2).Next()
+	e3, err := g.V(v1).BothE().Where(__.OtherV().Is(v2)).ToList()               // (3)
+	e4, err := g.V(v1).OutE().Where(__.InV().Is(v2)).ToList()                   // (4)
+	e5, err := g.V(1).OutE().Where(__.InV().Has(T.Id, P.Within(2, 3))).ToList() // (5)
+	e6, err := g.V(1).Out().Where(__.In().HasId(6)).ToList()                    // (6)
+
+	fmt.Sprintf(strconv.Itoa(len(e1)))
+	fmt.Sprintf(strconv.Itoa(len(e2)))
+	fmt.Sprintf(strconv.Itoa(len(e3)))
+	fmt.Sprintf(strconv.Itoa(len(e4)))
+	fmt.Sprintf(strconv.Itoa(len(e5)))
+	fmt.Sprintf(strconv.Itoa(len(e6)))
+
+	/*
+	  1. There are three edges from the vertex with the identifier of "1".
+	  2. Filter those three edges using the where()-step using the identifier of the vertex returned by otherV() to
+	     ensure it matches on the vertex of concern, which is the one with an identifier of "2".
+	  3. Note that the same traversal will work if there are actual Vertex instances rather than just vertex
+	     identifiers.
+	  4. The vertex with identifier "1" has all outgoing edges, so it would also be acceptable to use the directional
+	     steps of outE() and inV() since the schema allows it.
+	  5. There is also no problem with filtering the terminating side of the traversal on multiple vertices, in this
+	     case, vertices with identifiers "2" and "3".
+	  6. There’s no reason why the same pattern of exclusion used for edges with where() can’t work for a vertex
+	     between two vertices.
+	*/
 }
