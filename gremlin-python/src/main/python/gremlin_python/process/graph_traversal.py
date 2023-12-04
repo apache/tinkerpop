6fd60080b6a6e6341ae03892295ff5fe17770c96
Ryan Tan <ryanmtan5@gmail.com>
2023-12-04 14:24:15 -0800

    Write feature tests

--- gremlin-python/src/main/python/gremlin_python/process/graph_traversal.py
+++ gremlin-python/src/main/python/gremlin_python/process/graph_traversal.py
@@ -438,6 +438,10 @@ class GraphTraversal(Traversal):
         self.bytecode.add_step("difference", *args)
         return self
 
+    def discard(self, *args):
+        self.bytecode.add_step("discard", *args)
+        return self
+
     def disjunct(self, *args):
         self.bytecode.add_step("disjunct", *args)
         return self
