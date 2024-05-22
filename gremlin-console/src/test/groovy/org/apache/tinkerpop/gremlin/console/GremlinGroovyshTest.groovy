06fcf77ce64714a22a5422eafc9136cb1e4f7b40
Valentyn Kahamlyk <Valentyn.Kahamlyk@improving.com>
2024-05-22 16:23:57 -0700

    added integration test

--- gremlin-console/src/test/groovy/org/apache/tinkerpop/gremlin/console/GremlinGroovyshTest.groovy
+++ gremlin-console/src/test/groovy/org/apache/tinkerpop/gremlin/console/GremlinGroovyshTest.groovy
@@ -18,9 +18,15 @@
  */
 package org.apache.tinkerpop.gremlin.console
 
-
 import org.apache.tinkerpop.gremlin.console.jsr223.AbstractGremlinServerIntegrationTest
+import org.apache.tinkerpop.gremlin.structure.io.Storage
+import org.apache.tinkerpop.gremlin.util.TestSupport
 import org.codehaus.groovy.tools.shell.IO
+import org.junit.Test
+
+import java.nio.file.Files
+
+import static org.junit.Assert.assertTrue;
 
 class GremlinGroovyshTest extends AbstractGremlinServerIntegrationTest {
     private IO testio
@@ -35,6 +41,33 @@ class GremlinGroovyshTest extends AbstractGremlinServerIntegrationTest {
         err = new ByteArrayOutputStream()
         testio = new IO(new ByteArrayInputStream(), out, err)
         shell = new GremlinGroovysh(new Mediator(null), testio)
+    }
+
+    @Test
+    void shouldGetResultFromRemote() {
+        final File configFile = TestSupport.generateTempFileFromResource(AbstractGremlinServerIntegrationTest.class, "remote.yaml", "")
+        final File file = File.createTempFile("remote-graph", ".properties")
+
+        try {
+            // create temporary config file
+            Files.deleteIfExists(file.toPath())
+            Files.createFile(file.toPath())
+            try (PrintStream out = new PrintStream(new FileOutputStream(file.toPath().toString()))) {
+                out.print("gremlin.remote.remoteConnectionClass=org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection\n")
+                out.print("gremlin.remote.driver.clusterFile=" + Storage.toPath(configFile))
+                out.print("\ngremlin.remote.driver.sourceName=g\n")
+            }
+
+            shell.execute("import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal")
+            shell.execute("g = traversal().with('" + Storage.toPath(file) + "')")
+            out.reset()
+            shell.execute("g.V().count().next()")
+
+            // 6 vertices in modern graph
+            assertTrue(out.toString().endsWith("6\r\n"))
+        } finally {
+            Files.deleteIfExists(file.toPath())
+        }
     }
 
     @Override
