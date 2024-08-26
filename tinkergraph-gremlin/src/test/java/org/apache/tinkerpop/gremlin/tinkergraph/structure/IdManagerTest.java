e5a2facd478cf030b4ad2efc6f7429ae98870142
Daniel Weber <danielcweber@users.noreply.github.com>
2024-08-26 12:53:19 +0200

    Add two tests for DefaultIdManager.STRING.

--- tinkergraph-gremlin/src/test/java/org/apache/tinkerpop/gremlin/tinkergraph/structure/IdManagerTest.java
+++ tinkergraph-gremlin/src/test/java/org/apache/tinkerpop/gremlin/tinkergraph/structure/IdManagerTest.java
@@ -78,4 +78,20 @@ public class IdManagerTest {
         final TinkerGraph.IdManager manager = TinkerGraph.DefaultIdManager.UUID;
         manager.convert(Double.NaN);
     }
+
+    @Test
+    public void shouldGenerateNiceErrorOnConversionOfJunkToString() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Expected an id that is convertible to");
+        final TinkerGraph.IdManager manager = TinkerGraph.DefaultIdManager.STRING;
+        manager.convert(Double.NaN);
+    }
+
+    @Test
+    public void shouldGenerateNiceErrorOnConversionOfUUIDToString() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Expected an id that is convertible to");
+        final TinkerGraph.IdManager manager = TinkerGraph.DefaultIdManager.STRING;
+        manager.convert(UUID.randomUUID());
+    }
 }
