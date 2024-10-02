69bf56b6da8079f23a137f876faf22759b7035db
Tomer Ohana <tomer.ohana@nsure.ai>
2024-10-02 15:23:49 +0100

    Javascript: fix memory leak on server error

--- gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
+++ gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
@@ -344,6 +344,7 @@ class Connection extends EventEmitter {
 
       return;
     } else if (response.status.code >= 400) {
+      this._clearHandler(response.requestId);
       // callback in error
       return handler.callback(
         // TINKERPOP-2285: keep the old server error message in case folks are parsing that - fix in a future breaking version
