40975e63b7a4af50f9d122194df261e176b3a0af
Cole-Greer <Cole.Greer@improving.com>
2024-10-09 19:35:45 -0700

    CTR Minor fixes on merge to 3.7
    Fixes for merge of https://github.com/apache/tinkerpop/pull/2807
    and https://github.com/apache/tinkerpop/pull/2813

--- gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
+++ gremlin-javascript/src/main/javascript/gremlin-javascript/lib/driver/connection.js
@@ -312,7 +312,7 @@ class Connection extends EventEmitter {
 
       return;
     } else if (response.status.code >= 400) {
-      this._clearHandler(response.requestId);
+      this.#clearHandler(response.requestId);
       // callback in error
       return handler.callback(
         // TINKERPOP-2285: keep the old server error message in case folks are parsing that - fix in a future breaking version
