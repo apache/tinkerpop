452c2c82d3283eb6b6805680c2a19a4d84e9cee8
Andrea Child <andrea.child@improving.com>
2025-03-05 19:56:45 -0800

    Tidy up httpTransporter by extracting smaller functions.

--- gremlin-go/driver/httpTransporter.go
+++ gremlin-go/driver/httpTransporter.go
@@ -31,6 +31,7 @@ import (
 
 // TODO decide channel size when chunked response handling is implemented - for now just set to 1
 const responseChannelSizeDefault = 1
+const contentTypeHeader = "content-type"
 
 // httpTransporter responsible for sending and receiving bytes to/from the server
 type httpTransporter struct {
@@ -58,70 +59,22 @@ func newHttpTransporter(url string, connSettings *connectionSettings, httpClient
 
 // write sends bytes to the server as a POST request and sends received response bytes to the responseChannel
 func (transporter *httpTransporter) write(data []byte) error {
-	transporter.logHandler.logf(Debug, creatingRequest)
-	req, err := http.NewRequest("POST", transporter.url, bytes.NewBuffer(data))
-	if err != nil {
-		transporter.logHandler.logf(Error, failedToSendRequest, err.Error())
-		return err
-	}
-	req.Header.Set("content-type", graphBinaryMimeType)
-	req.Header.Set("accept", graphBinaryMimeType)
-	if transporter.connSettings.enableUserAgentOnConnect {
-		req.Header.Set(userAgentHeader, userAgent)
-	}
-	if transporter.connSettings.enableCompression {
-		req.Header.Set("accept-encoding", "deflate")
-	}
-
-	transporter.logHandler.logf(Debug, writeRequest)
-	resp, err := transporter.httpClient.Do(req)
-	if err != nil {
-		transporter.logHandler.logf(Error, failedToSendRequest, err.Error())
-		return err
-	}
-
-	reader := resp.Body
-	if resp.Header.Get("content-encoding") == "deflate" {
-		reader, err = zlib.NewReader(resp.Body)
-		if err != nil {
-			transporter.logHandler.logf(Error, failedToReceiveResponse, err.Error())
-			return err
-		}
-	}
-
-	// TODO handle chunked encoding and send chunks to responseChannel
-	all, err := io.ReadAll(reader)
-	if err != nil {
-		transporter.logHandler.logf(Error, failedToReceiveResponse, err.Error())
-		return err
-	}
-	err = reader.Close()
+	resp, err := transporter.sendRequest(data)
 	if err != nil {
 		return err
 	}
 
-	transporter.logHandler.log(Debug, receivedResponse)
+	respBytes, err := transporter.readResponse(resp)
+	if err != nil {
+		return err
+	}
 
 	// possible to receive graph-binary or json error response bodies
-	contentType := resp.Header.Get("content-type")
-	if resp.StatusCode != 200 && contentType != graphBinaryMimeType {
-		if contentType == "application/json" {
-			var jsonMap map[string]interface{}
-			err = json.Unmarshal(all, &jsonMap)
-			if err != nil {
-				return err
-			}
-			message, exists := jsonMap["message"]
-			if exists {
-				return newError(err0502ResponseHandlerError, message, resp.StatusCode)
-			}
-			return newError(err0502ResponseHandlerError, "Response was not successful", resp.StatusCode)
-		}
-		// unexpected error content type
-		return newError(err0502ResponseHandlerError, "Response was not successful and of unexpected content-type: "+contentType, resp.StatusCode)
+	if resp.StatusCode != 200 && resp.Header.Get(contentTypeHeader) != graphBinaryMimeType {
+		return transporter.createResponseError(respBytes, resp)
 	}
 
-	transporter.responseChannel <- all
+	transporter.responseChannel <- respBytes
 	return nil
 }
 
@@ -142,4 +95,77 @@ func (transporter *httpTransporter) close() {
 		}
 		transporter.isClosed = true
 	}
+}
+
+func (transporter *httpTransporter) createResponseError(respBytes []byte, resp *http.Response) error {
+	contentType := resp.Header.Get(contentTypeHeader)
+	if contentType == "application/json" {
+		var jsonMap map[string]interface{}
+		err := json.Unmarshal(respBytes, &jsonMap)
+		if err != nil {
+			return err
+		}
+		message, exists := jsonMap["message"]
+		if exists {
+			return newError(err0502ResponseHandlerError, message, resp.StatusCode)
+		}
+		return newError(err0502ResponseHandlerError, "Response was not successful", resp.StatusCode)
+	}
+	// unexpected error content type
+	return newError(err0502ResponseHandlerError, "Response was not successful and of unexpected content-type: "+contentType, resp.StatusCode)
+}
+
+// reads bytes from the given response
+func (transporter *httpTransporter) readResponse(resp *http.Response) ([]byte, error) {
+	var reader io.ReadCloser
+	var err error
+
+	if resp.Header.Get("content-encoding") == "deflate" {
+		reader, err = zlib.NewReader(resp.Body)
+		if err != nil {
+			transporter.logHandler.logf(Error, failedToReceiveResponse, err.Error())
+			return nil, err
+		}
+	} else {
+		reader = resp.Body
+	}
+
+	// TODO handle chunked encoding and send chunks to responseChannel
+	all, err := io.ReadAll(reader)
+	if err != nil {
+		transporter.logHandler.logf(Error, failedToReceiveResponse, err.Error())
+		return nil, err
+	}
+	err = reader.Close()
+	if err != nil {
+		return nil, err
+	}
+	transporter.logHandler.log(Debug, receivedResponse)
+	return all, nil
+}
+
+// sends a POST request for the given byte content
+func (transporter *httpTransporter) sendRequest(data []byte) (*http.Response, error) {
+	transporter.logHandler.logf(Debug, creatingRequest)
+	req, err := http.NewRequest("POST", transporter.url, bytes.NewBuffer(data))
+	if err != nil {
+		transporter.logHandler.logf(Error, failedToSendRequest, err.Error())
+		return nil, err
+	}
+	req.Header.Set(contentTypeHeader, graphBinaryMimeType)
+	req.Header.Set("accept", graphBinaryMimeType)
+	if transporter.connSettings.enableUserAgentOnConnect {
+		req.Header.Set(userAgentHeader, userAgent)
+	}
+	if transporter.connSettings.enableCompression {
+		req.Header.Set("accept-encoding", "deflate")
+	}
+
+	transporter.logHandler.logf(Debug, writeRequest)
+	resp, err := transporter.httpClient.Do(req)
+	if err != nil {
+		transporter.logHandler.logf(Error, failedToSendRequest, err.Error())
+		return nil, err
+	}
+	return resp, nil
 }
