7364aa64c333ffd845a7d858b670102e0d8f3499
Cole-Greer <Cole.Greer@improving.com>
2025-09-26 12:21:58 -0700

    CTR avoid boxing property values as Traversals in AddPropertyStepPlaceholder

--- gremlin-core/src/test/java/org/apache/tinkerpop/gremlin/process/traversal/step/TraversalParentTest.java
+++ gremlin-core/src/test/java/org/apache/tinkerpop/gremlin/process/traversal/step/TraversalParentTest.java
@@ -291,7 +291,7 @@ public class TraversalParentTest {
                 {AddPropertyStepContract.class,
                         g.inject(1).property("key", "value"),
                         List.of(),
-                        List.of(new ConstantTraversal<>("value")), // constant Value's are internally boxed in Traversals
+                        List.of(),
                         null, null
                 },
                 {AddPropertyStepContract.class,
@@ -309,7 +309,7 @@ public class TraversalParentTest {
                 {AddPropertyStepContract.class,
                         g.addV().property("name", "value", "metaKey", "metaValue"),
                         List.of(),
-                        List.of(new ConstantTraversal<>("value")), // constant Value's are internally boxed in Traversals
+                        List.of(),
                         null, null
                 },
                 {AddPropertyStepContract.class,
@@ -327,7 +327,7 @@ public class TraversalParentTest {
                 {AddPropertyStepContract.class,
                         g.addV().property("name", "value", "metaKey1", "metaValue1", "metaKey2", "metaValue2"),
                         List.of(),
-                        List.of(new ConstantTraversal<>("value")), // constant Value's are internally boxed in Traversals
+                        List.of(),
                         null, null
                 },
                 {MergeStepContract.class,
@@ -982,23 +982,23 @@ public class TraversalParentTest {
 
     private void verifyExpectedParents(List<Traversal.Admin<?,?>> expectedGlobalChildren, List<Traversal.Admin<?,?>> expectedLocalChildren, String messageSuffix) {
         List<Step<?,?>> steps = TraversalHelper.getStepsOfAssignableClass(stepClass, traversal);
-        assertTrue(String.format("Expected a single step of class %s to test, found %d %s", stepClass.getName(), steps.size(), messageSuffix), steps.size() == 1);
-        assertTrue(String.format("Expected step to implement TraversalParent %s", messageSuffix), steps.get(0) instanceof TraversalParent);
+        assertTrue(String.format("Expected a single step of class %s to test, found %d for Traversal [%s] %s", stepClass.getName(), steps.size(), traversal, messageSuffix), steps.size() == 1);
+        assertTrue(String.format("Expected step to implement TraversalParent for Traversal [%s] %s", traversal, messageSuffix), steps.get(0) instanceof TraversalParent);
 
         TraversalParent testStep = (TraversalParent) steps.get(0);
-        assertThat(String.format("getGlobalChildren() did not produce the expected results %s", messageSuffix), testStep.getGlobalChildren(), containsInAnyOrder(expectedGlobalChildren.toArray()));
-        assertThat(String.format("getLocalChildren() did not produce the expected results %s", messageSuffix), testStep.getLocalChildren(), containsInAnyOrder(expectedLocalChildren.toArray()));
+        assertThat(String.format("getGlobalChildren() did not produce the expected results for Traversal [%s] %s", traversal, messageSuffix), testStep.getGlobalChildren(), containsInAnyOrder(expectedGlobalChildren.toArray()));
+        assertThat(String.format("getLocalChildren() did not produce the expected results for Traversal [%s] %s", traversal, messageSuffix), testStep.getLocalChildren(), containsInAnyOrder(expectedLocalChildren.toArray()));
 
         for (Traversal.Admin<?, ?> child : testStep.getGlobalChildren()) {
             if (!(child instanceof AbstractLambdaTraversal)) {
                 // Use reference equality to ensure parent points to the correct object. It's not enough that the parents contents match.
-                assertTrue(String.format("Expected child traversal %s to have correctly assigned parent %s", child.toString(), messageSuffix), testStep == child.getParent());
+                assertTrue(String.format("Expected child traversal %s to have correctly assigned parent for Traversal [%s] %s", child.toString(), traversal, messageSuffix), testStep == child.getParent());
             }
         }
         for (Traversal.Admin<?, ?> child : testStep.getLocalChildren()) {
             if (!(child instanceof AbstractLambdaTraversal)) {
                 // Use reference equality to ensure parent points to the correct object. It's not enough that the parents contents match.
-                assertTrue(String.format("Expected child traversal %s to have correctly assigned parent %s", child.toString(), messageSuffix), testStep == child.getParent());
+                assertTrue(String.format("Expected child traversal %s to have correctly assigned parent for Traversal [%s] %s", child.toString(), traversal, messageSuffix), testStep == child.getParent());
             }
         }
     }
