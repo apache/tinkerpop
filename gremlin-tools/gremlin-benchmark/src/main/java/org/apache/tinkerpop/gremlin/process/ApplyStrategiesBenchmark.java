608ea0ee63d597c19e40dfcb89df6cd304e49640
Andrea Child <andrea.child@improving.com>
2025-10-10 12:16:00 -0700

    Change back to use empty graph but manually add the default strategies to the traversal. Add check that at least one strategy should be applied otherwise the benchmark will fail.

--- gremlin-tools/gremlin-benchmark/src/main/java/org/apache/tinkerpop/gremlin/process/ApplyStrategiesBenchmark.java
+++ gremlin-tools/gremlin-benchmark/src/main/java/org/apache/tinkerpop/gremlin/process/ApplyStrategiesBenchmark.java
@@ -18,13 +18,16 @@
  */
 package org.apache.tinkerpop.gremlin.process;
 
+import java.util.List;
 import org.apache.tinkerpop.benchmark.util.AbstractBenchmarkBase;
 import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategies;
+import org.apache.tinkerpop.gremlin.process.traversal.TraversalStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -45,15 +48,21 @@ import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalS
 @State(Scope.Thread)
 public class ApplyStrategiesBenchmark extends AbstractBenchmarkBase {
 
-    private Graph graph = TinkerGraph.open();
-    private GraphTraversalSource g = traversal().withEmbedded(graph);
+    private Graph graph = EmptyGraph.instance();
+    private GraphTraversalSource g = traversal().withEmbedded(graph)
+           .withStrategies(TraversalStrategies.GlobalCache.getStrategies(Graph.class).toList().toArray(new TraversalStrategy[0]));
     private Traversal<?, ?> traversal;
     
     @Setup(Level.Trial)
     public void setUpTrial() {
-        // output the strategies that were applied for the benchmark
+        // ensure there are strategies registered
+        List<TraversalStrategy<?>> strategies = g.getStrategies().toList();
+        if (strategies.isEmpty()) {
+            throw new IllegalStateException("No traversal strategies are registered");
+        }
+        // output the strategies that were registered for the benchmark
         System.out.println();
-        System.out.println("Strategies: " + g.getStrategies().toList());
+        System.out.println("Strategies: " + strategies);
     }
 
     /**
