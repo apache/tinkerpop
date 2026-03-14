b2443ccf4adbaeedc04fe71040461d06f61acb12
Stephen Mallette <stepmall@amazon.com>
2026-03-14 16:02:29 -0400

    License file normalization CTR

--- gremlin-python/build/generate.groovy
+++ gremlin-python/build/generate.groovy
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -73,7 +73,7 @@ radishGremlinFile.withWriter('UTF-8') { Writer writer ->
             '# "License"); you may not use this file except in compliance\n' +
             '# with the License.  You may obtain a copy of the License at\n' +
             '# \n' +
-            '# http://www.apache.org/licenses/LICENSE-2.0\n' +
+            '#   http://www.apache.org/licenses/LICENSE-2.0\n' +
             '# \n' +
             '# Unless required by applicable law or agreed to in writing,\n' +
             '# software distributed under the License is distributed on an\n' +
