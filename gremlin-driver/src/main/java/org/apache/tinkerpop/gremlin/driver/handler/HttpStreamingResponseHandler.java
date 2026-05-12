ba1c20cde5df019594cb9127c3f5f8310146ffac
Cole Greer <cole@colegreer.ca>
2026-05-11 17:17:49 -0700

    fix tests

--- gremlin-driver/src/main/java/org/apache/tinkerpop/gremlin/driver/handler/HttpStreamingResponseHandler.java
+++ gremlin-driver/src/main/java/org/apache/tinkerpop/gremlin/driver/handler/HttpStreamingResponseHandler.java
@@ -133,7 +133,7 @@ public class HttpStreamingResponseHandler extends MessageToMessageDecoder<HttpOb
 
             if (maxResponseContentLength > 0 && bytesRead > maxResponseContentLength) {
                 // Don't signal here — exceptionCaught will handle cleanup
-                throw new TooLongFrameException("Response exceeded " + maxResponseContentLength + " bytes.");
+                throw new TooLongFrameException("Response entity too large");
             }
 
             if (!isGraphBinaryResponse()) {
